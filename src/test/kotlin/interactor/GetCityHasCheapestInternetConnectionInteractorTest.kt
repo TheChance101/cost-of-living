e9222645a5eb90fb93cf804ba7c822d869cdbf8e
HassanWasfy <wasfyelgamed@gmail.com>
2023-02-08 00:11:22 +0200

    Junit5 dependencies has been added

---
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -0,0 +1,12 @@
+package interactor
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class GetCityHasCheapestInternetConnectionInteractorTest {
+
+    @Test
+    fun execute() {
+    }
+}
