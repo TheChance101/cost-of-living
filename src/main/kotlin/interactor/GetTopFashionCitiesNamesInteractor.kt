6b683dd4b56f666f05caa70a34b2f0566351dca8
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-02-09 13:54:53 +0300

    prepare interactor

---
+++ src/main/kotlin/interactor/GetTopFashionCitiesNamesInteractor.kt
@@ -0,0 +1,11 @@
+package interactor
+
+class GetTopFashionCitiesNamesInteractor(
+    private val dataSource: CostOfLivingDataSource,
+) {
+
+    fun execute(limit: Int): List<String> {
+        throw Throwable("Not Implemented yet")
+    }
+
+}
