e2d2284b1be4b128dcc560c84f94a1c855573e32
Haidy <haidyabugom3a@gmail.com>
2023-02-09 23:47:23 +0200

    add get salaries and cities names interactor

---
+++ src/main/kotlin/interactor/GetSalaryAverageAndCitiesNamesInCountryInteractor.kt
@@ -0,0 +1,8 @@
+package interactor
+
+class GetSalaryAverageAndCitiesNamesInCountryInteractor {
+
+    fun execute(countryName:String):List<Pair<String,Float>>{
+        throw Throwable("Not Implemented yet")
+    }
+}
