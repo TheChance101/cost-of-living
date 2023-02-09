4e8a112a4c45b6863f7bd271d1b6541e5c608737
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-10 01:04:33 +0200

    add class GetCheapestBananaCitiesNamesInterActor

---
+++ src/main/kotlin/interactor/GetCheapestBananaCitiesNamesInterActor.kt
@@ -0,0 +1,7 @@
+package interactor
+
+class GetCheapestBananaCitiesNamesInterActor(private val dataSource: CostOfLivingDataSource) {
+    fun execute(vararg cities: String): List<String> {
+        return listOf()
+    }
+}
