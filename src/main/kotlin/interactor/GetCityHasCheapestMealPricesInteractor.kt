6482740293cc9d2644382e914b5c0a9b84cd29c0
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-02-10 00:53:58 +0200

    add GetCityHasCheapestMealPricesInteractor class

---
+++ src/main/kotlin/interactor/GetCityHasCheapestMealPricesInteractor.kt
@@ -0,0 +1,12 @@
+package interactor
+
+import model.CityEntity
+
+class GetCityHasCheapestMealPricesInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
+
+    fun execute(): CityEntity {
+        throw Throwable("Not Implemented yet")
+    }
+}
