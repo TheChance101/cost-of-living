98ca07349a54c3127c2711b5dc29eabe24946e14
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-02-09 19:01:32 +0300

    implement interactor

--- src/main/kotlin/interactor/GetTopFashionCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetTopFashionCitiesNamesInteractor.kt
@@ -1,11 +1,37 @@
 package interactor
 
+import model.CityEntity
+
 class GetTopFashionCitiesNamesInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
     fun execute(limit: Int): List<String> {
-        throw Throwable("Not Implemented yet")
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullPricesAndLowQualityData)
+            .sortedBy { it.getAveragePrice() }
+            .take(limit)
+            .map { it.cityName }
+    }
+
+    private fun excludeNullPricesAndLowQualityData(city: CityEntity): Boolean {
+        with(city.clothesPrices) {
+            return onePairOfJeansLevis50oneOrSimilar != null &&
+                    onePairOfMenLeatherBusinessShoes != null &&
+                    onePairOfNikeRunningShoesMidRange != null &&
+                    oneSummerDressInAChainStoreZaraHAndM != null &&
+                    city.dataQuality
+        }
+    }
+
+    private fun CityEntity.getAveragePrice(): Float {
+        with(clothesPrices) {
+            return (onePairOfJeansLevis50oneOrSimilar!! +
+                    onePairOfMenLeatherBusinessShoes!! +
+                    onePairOfNikeRunningShoesMidRange!! +
+                    oneSummerDressInAChainStoreZaraHAndM!!) / 4
+        }
     }
 
 }
