ccc60375d68d54864aded6ce11a871eb7b5f0635
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-02-09 15:48:47 +0200

    add implementation for cheapest banana price

--- src/main/kotlin/interactor/GetCitiesNamesWithSCheapestBananaPricesInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNamesWithSCheapestBananaPricesInteractor.kt
@@ -1,10 +1,53 @@
 package interactor
 
+import model.CityEntity
 
 class GetCitiesNamesWithSCheapestBananaPricesInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(vararg citiesNames: String): List<String> {
+        val dataEntities = dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullBananaPrice)
+
+        val dataNames = dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullBananaPrice)
+            .map { it.cityName }
+
+        if (
+            citiesNames.size == 1 &&
+            dataNames.contains(citiesNames.first())
+        ) {
+            return citiesNames.toList()
+        }
+        if (
+            citiesNames.size != 1
+            && dataNames.containsAll(citiesNames.toList())
+        ){
+            return dataEntities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }
+                .filter { it.cityName in citiesNames }
+                .map {
+                it.cityName
+            }
+        }
+
+        if (
+            citiesNames.size != 1
+            && citiesNames.any { it in dataNames }
+        ){
+            return dataEntities.sortedBy {
+                it.fruitAndVegetablesPrices.banana1kg
+            }
+                .filter { it.cityName in citiesNames }
+                .map {
+                it.cityName
+            }
+        }
         return emptyList()
+    }
+
+    private fun excludeNullBananaPrice(city: CityEntity): Boolean {
+        return city.fruitAndVegetablesPrices.banana1kg != null
     }
 }
