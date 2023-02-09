0f1eae05c1e6b59843f9241569712f5e153848ba
TarekIdrees <tarekdrias321@gmail.com>
2023-02-09 23:21:10 +0200

    first implementation of execute function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,20 @@ import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
 import interactor.CostOfLivingDataSource
 import interactor.GetCityHasCheapestInternetConnectionInteractor
+import interactor.GetCityThatMatchTheManagerExpectationsInteractor
 import interactor.GetHighestSalaryAverageCititesNamesInteractor
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
-    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
-    println(getHighestSalaryAverageCities.execute(limit = 10))
-    printSeparationLine()
+//    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
+//    println(getHighestSalaryAverageCities.execute(limit = 10))
+//    printSeparationLine()
 
-    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-    println(getCityHasCheapestInternetConnectionInteractor.execute())
+    val countries = listOf("United State", "Canada", "Mexico")
+    val getCityThatMatchTheManagerExpectationsInteractor = GetCityThatMatchTheManagerExpectationsInteractor(dataSource)
+    getCityThatMatchTheManagerExpectationsInteractor.execute(countries)
 
 }
 private fun printSeparationLine(){
--- src/main/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractor.kt
@@ -6,6 +6,20 @@ class GetCityThatMatchTheManagerExpectationsInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(countries: List<String>): CityEntity {
+
+         dataSource
+            .getAllCitiesData()
+             .filter{countries.contains(it.country)}
+             .filter (::excludeNullMealFor2PeopleMidRangeRestaurant)
+            .map {
+                print(it.mealsPrices.mealFor2PeopleMidRangeRestaurant)
+            }
         return dataSource.getAllCitiesData().random()
     }
+
+
+    private fun excludeNullMealFor2PeopleMidRangeRestaurant(city: CityEntity): Boolean {
+        return city.mealsPrices.mealFor2PeopleMidRangeRestaurant != null
+    }
+
 }
