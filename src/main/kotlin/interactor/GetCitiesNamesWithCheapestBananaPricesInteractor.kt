d965a429922a196c0beb9bf31e9445059c94d47f
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-10 23:30:27 +0200

    improve logic of feature

--- src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
@@ -4,17 +4,19 @@ import model.CityEntity
 
 class GetCitiesNamesWithCheapestBananaPricesInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(vararg citiesNames: String): List<String> {
-        val filteredCities = dataSource.getAllCitiesData().filter(::excludeNullBananaPrice)
-        val cityNames = filteredCities.map(CityEntity::cityName)
-
-        return if (citiesNames.any { it in cityNames }) {
-            filteredCities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }
-                .filter { it.cityName in citiesNames }.map(CityEntity::cityName)
-        } else emptyList()
-
+        return dataSource.getAllCitiesData()
+            .filter { excludeNullBananaPriceAndIncludedCitiesNames(it, *citiesNames) }
+            .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
+            .map(CityEntity::cityName)
     }
 
-    private fun excludeNullBananaPrice(city: CityEntity): Boolean {
+    private fun excludeNullBananaPriceAndIncludedCitiesNames(city: CityEntity, vararg citiesNames: String): Boolean {
         return city.fruitAndVegetablesPrices.banana1kg != null
+                && city.cityName.lowercase() in citiesNames.map(::toFormalCase)
     }
+
+    private fun toFormalCase(string: String): String {
+        return string.lowercase().trim()
+    }
+
 }
