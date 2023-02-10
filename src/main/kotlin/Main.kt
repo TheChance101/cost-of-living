beae5fbb5adf6b2b568cd9fbc017e3bd67138461
AndrewAboalhana <hana_hany6@yahoo.com>
2023-02-10 20:18:34 +0200

    add tests for city has the cheapest internet and add fake data

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,13 +7,18 @@ import interactor.GetHighestSalaryAverageCititesNamesInteractor
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
+    val data = dataSource.getAllCitiesData()
 
     val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
     println(getHighestSalaryAverageCities.execute(limit = 10))
     printSeparationLine()
 
-    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-    println(getCityHasCheapestInternetConnectionInteractor.execute())
+
+
+   val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor()
+    println(getCityHasCheapestInternetConnectionInteractor.execute(data))
+
+
 
 }
 private fun printSeparationLine(){
---
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -0,0 +1,51 @@
+package interactor
+
+import dataSource.FakeDataSource
+import model.CityEntity
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetCityHasCheapestInternetConnectionInteractorTest {
+
+    private lateinit var cityHasCheapestInternet: GetCityHasCheapestInternetConnectionInteractor
+    private lateinit var fakeDataSource: FakeDataSource
+
+    @BeforeEach
+    fun setup(){
+        fakeDataSource = FakeDataSource()
+        cityHasCheapestInternet = GetCityHasCheapestInternetConnectionInteractor()
+    }
+
+    @Test
+    fun should_ReturnTheOnlyCity_When_GivenOnly1CityIncluded(){
+        // given list of cityEntity
+        val cities = fakeDataSource.getAllCitiesData()[10]
+        // when check what is the cheapest city in list with giving only 1 city in list
+        val result = cityHasCheapestInternet.execute(listOf(cities))
+        // then check the result
+        assertEquals(cities.cityName,result?.cityName)
+    }
+
+    @Test
+    fun should_ReturnNull_When_givenEmptyList(){
+        // given Empty list
+        val cities = emptyList<CityEntity>()
+        // when check what is the cheapest city in list with giving emptyList
+        val result = cityHasCheapestInternet.execute(cities)
+        // then check the result
+        assertNull(result)
+    }
+
+    @Test
+    fun should_Return_CheapestCity_When_GivenListOfCities(){
+        // given list of cityEntity
+        val cities = fakeDataSource.getAllCitiesData()
+        // when check what is the cheapest city in the list
+        val result = cityHasCheapestInternet.execute(cities)
+        // then check the result
+        assertEquals(cities[17].cityName,result?.cityName)
+    }
+}
