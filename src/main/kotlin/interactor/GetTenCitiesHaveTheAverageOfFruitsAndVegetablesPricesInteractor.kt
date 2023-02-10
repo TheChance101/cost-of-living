a96ddd0f2fab09dfa2332c72948e9089509a2c3b
Menna Akram <mennaakram976@gmail.com>
2023-02-10 19:01:51 +0200

    Add class GetTenCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor

---
+++ src/main/kotlin/interactor/GetTenCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.kt
@@ -0,0 +1,9 @@
+package interactor
+
+class GetTenCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor
+    (private val dataSource: CostOfLivingDataSource){
+
+        fun execute(limit: Int): List<String> {
+            return emptyList()
+        }
+}
