9acd54a55754c13890ee300b96f3252e0ea1e77b
mohammed akkad <momoo362514@gmail.com>
2023-02-10 20:29:57 +0200

    improve function execute

--- src/main/kotlin/interactor/GetCityHasMoreSavingsPerMonthInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasMoreSavingsPerMonthInteractor.kt
@@ -1,11 +1,47 @@
 package interactor
 
+import model.CityEntity
+
 
 class GetCityHasMoreSavingsPerMonthInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(limit: Int) {
+    fun execute(limit: Int): List<String>? {
+        return if (limit > 0) dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullFoodPrices)
+            .filter(::excludeNullApartment)
+            .filter(::excludeTransportationsPrices)
+            .take(limit)
+            .map { it.cityName }
+        else if (limit == 0) null
+        else null
+
+    }
+
+    private fun excludeNullFoodPrices(city: CityEntity): Boolean {
+        return city.foodPrices.loafOfFreshWhiteBread500g != null
+                && city.foodPrices.localCheese1kg != null
+                && city.foodPrices.beefRound1kgOrEquivalentBackLegRedMeat != null
+                && city.foodPrices.chickenFillets1kg != null
+                && city.foodPrices.riceWhite1kg != null
+
+    }
+
+    private fun excludeNullApartment(city: CityEntity): Boolean {
+        return city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
+
+    }
+
+    private fun excludeTransportationsPrices(city: CityEntity): Boolean {
+        return city.transportationsPrices.gasolineOneLiter == null
+                && city.transportationsPrices.monthlyPassRegularPrice == null
+                && city.transportationsPrices.taxi1kmNormalTariff == null
+                && city.transportationsPrices.taxi1hourWaitingNormalTariff == null
+                && city.transportationsPrices.oneWayTicketLocalTransport == null
+                && city.transportationsPrices.taxiStartNormalTariff == null
 
 
     }
