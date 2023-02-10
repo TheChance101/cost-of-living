80ebd39f730dcd4e3c89537fa6a94ffbf319e56e
David samuel <davidsamuelramsis@gmail.com>
2023-02-10 23:26:05 +0200

    add implementation for cheapest city internet

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,10 +6,16 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity{
-        throw Throwable("Not Implemented yet")
-    }
+    fun execute(cities:List<CityEntity>): CityEntity?
+     = if (cities.isEmpty()) null else cities.filter {
+         excludeNullFromSalariesNullInternetPriceAndLowQualityData(it)
+    }.sortedBy { cityEntity -> (cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl?.
+    div(cityEntity.averageMonthlyNetSalaryAfterTax!!))?.times(100) }[0]
 
+    private fun excludeNullFromSalariesNullInternetPriceAndLowQualityData(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+                && city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null
+    }
 
 
 }
