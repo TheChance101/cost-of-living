9bec354bfe30f36fb86686d946df40b3035524f6
Haidy <haidyabugom3a@gmail.com>
2023-02-10 21:31:44 +0200

    add get salary average and citites functions

--- src/main/kotlin/interactor/GetSalaryAverageAndCitiesNamesInCountryInteractor.kt
+++ src/main/kotlin/interactor/GetSalaryAverageAndCitiesNamesInCountryInteractor.kt
@@ -1,12 +1,27 @@
 package interactor
 
-import dataSource.FakeDataSource
+import model.CityEntity
+import java.io.IOException
 
 class GetSalaryAverageAndCitiesNamesInCountryInteractor(
-    private val fakeDataSource: FakeDataSource
+    private val dataSource: CostOfLivingDataSource
     ) {
 
-    fun execute(countryName:String):List<Pair<String,Float>>{
-        throw Throwable("Not Implemented yet")
+    fun execute(countryName:String):List<Pair<String,Float?>> {
+        return if (checkIfCountryIsValid(countryName))
+            dataSource.getAllCitiesData()
+                .filter{excludeNullSalariesAndLowQualityData(it) && it.country.lowercase() == countryName.lowercase()}
+                .map{Pair(it.cityName,it.averageMonthlyNetSalaryAfterTax)}
+        else
+            throw IOException("Invalid country name")
+    }
+
+    private fun checkIfCountryIsValid(countryName: String):Boolean{
+        return countryName.lowercase() in dataSource.getAllCitiesData()
+            .map{it.country.lowercase()}
+    }
+
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
     }
 }
