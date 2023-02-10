05179978aea1facd782f56bc8334b53e548a4730
ameeramjed <pr.ameer98@gmail.com>
2023-02-10 21:24:10 +0300

    improve code

--- src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
@@ -4,15 +4,14 @@ import model.CityEntity
 
 class GetCitiesNamesWithCheapestBananaPricesInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(vararg citiesNames: String): List<String> {
-        val filteredCities = dataSource.getAllCitiesData().filter { city -> excludeNullBananaPrice(city) }
+        val filteredCities = dataSource.getAllCitiesData().filter(::excludeNullBananaPrice)
         val cityNames = filteredCities.map(CityEntity::cityName)
 
-        return when {
-            citiesNames.size == 1 && cityNames.contains(citiesNames.first()) -> citiesNames.toList()
-            citiesNames.size != 1 && cityNames.containsAll(citiesNames.toList()) -> filteredCities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }.map(CityEntity::cityName)
-            citiesNames.any { it in cityNames } -> filteredCities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }.filter { it.cityName in citiesNames }.map(CityEntity::cityName)
-            else -> emptyList()
-        }
+        return if (citiesNames.any { it in cityNames }) {
+            filteredCities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }
+                .filter { it.cityName in citiesNames }.map(CityEntity::cityName)
+        } else emptyList()
+
     }
 
     private fun excludeNullBananaPrice(city: CityEntity): Boolean {
