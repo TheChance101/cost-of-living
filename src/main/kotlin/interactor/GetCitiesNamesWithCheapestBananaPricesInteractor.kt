b61d6e1d1762ea3c893fa1ffd9245aecdb9adc21
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-10 03:45:56 +0200

    update version review

--- src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractor.kt
@@ -2,52 +2,46 @@ package interactor
 
 import model.CityEntity
 
-class GetCitiesNamesWithCheapestBananaPricesInteractor(
-    private val dataSource: CostOfLivingDataSource
-) {
+class GetCitiesNamesWithCheapestBananaPricesInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(vararg citiesNames: String): List<String> {
         val dataEntities = dataSource
             .getAllCitiesData()
-            .filter(::excludeNullBananaPrice)
+            .filter { cityEntity -> excludeNullBananaPrice(cityEntity) }
 
         val dataNames = dataSource
             .getAllCitiesData()
-            .filter(::excludeNullBananaPrice)
-            .map { it.cityName }
+            .filter { cityEntity -> excludeNullBananaPrice(cityEntity) }
+            .map(CityEntity::cityName)
 
-        if (
-            citiesNames.size == 1 &&
-            dataNames.contains(citiesNames.first())
-        ) {
-            return citiesNames.toList()
-        }
-        if (
-            citiesNames.size != 1
-            && dataNames.containsAll(citiesNames.toList())
-        ){
-            return dataEntities.sortedBy { it.fruitAndVegetablesPrices.banana1kg }
-                .filter { it.cityName in citiesNames }
-                .map {
-                it.cityName
+        when {
+            (citiesNames.size == 1) &&
+                    dataNames.contains(citiesNames.first())
+            -> return citiesNames.toList()
+
+            (citiesNames.size != 1)
+                    && dataNames.containsAll(citiesNames.toList())
+            -> return dataEntities.sortedBy { cityEntity -> cityEntity.fruitAndVegetablesPrices.banana1kg }
+                .filter { cityEntity -> cityEntity.cityName in citiesNames }
+                .map(CityEntity::cityName)
+
+            (citiesNames.size != 1)
+                    && citiesNames.any { it in dataNames }
+            -> return dataEntities.sortedBy { cityEntity ->
+                cityEntity.fruitAndVegetablesPrices.banana1kg
             }
+                .filter { cityEntity -> cityEntity.cityName in citiesNames }
+                .map(CityEntity::cityName)
+
+            else ->
+                return emptyList()
+
         }
 
-        if (
-            citiesNames.size != 1
-            && citiesNames.any { it in dataNames }
-        ){
-            return dataEntities.sortedBy {
-                it.fruitAndVegetablesPrices.banana1kg
-            }
-                .filter { it.cityName in citiesNames }
-                .map {
-                it.cityName
-            }
-        }
-        return emptyList()
     }
 
-    private fun excludeNullBananaPrice(city: CityEntity): Boolean {
-        return city.fruitAndVegetablesPrices.banana1kg != null
+    companion object {
+        private fun excludeNullBananaPrice(city: CityEntity): Boolean {
+            return city.fruitAndVegetablesPrices.banana1kg != null
+        }
     }
 }
--- src/test/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
-    lateinit var getCitiesNamesWithCheapestBananaPricesInteractor: GetCitiesNamesWithCheapestBananaPricesInteractor
-    lateinit var fakeDataSource: FakeDataSource
+    private lateinit var getCitiesNamesWithCheapestBananaPricesInteractor: GetCitiesNamesWithCheapestBananaPricesInteractor
+    private lateinit var fakeDataSource: FakeDataSource
 
     @BeforeAll
     fun setup() {
@@ -22,8 +22,8 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
 
     // region one city
     @Test
-    fun should_ReturnOneCity_when_TheInputIsOneCityIncluded() {
-        // given
+    fun should_ReturnOneCity_When_TheInputIsOneCityIncluded() {
+        // given one city included
         val city = "Santiago de Cuba"
 
         // when the output is list of one city
@@ -35,39 +35,39 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
 
     @Test
     fun should_ReturnEmptyList_when_TheInputIsOneCityNotIncluded() {
-        // given
+        // given one city is not included
         val city = "Galaxy"
 
-        // when the output is list of one city
+        // when the output is empty list of city
         val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
 
-        // then check
+        // then check 
         assertEquals(emptyList<String>(), result)
     }
     // endregion
 
     // region multi city
     @Test
-    fun should_ReturnSortedList_when_TheInputIsMultiCityAllIncluded() {
-        // given
+    fun should_ReturnSortedList_When_TheInputIsMultiCityAllIncluded() {
+        // given multi city all of this is included
         val cities = arrayOf("Santiago de Cuba", "Sancti Spiritus")
 
         // when the output is list of sorted cities by Banana Price
         val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(citiesNames = cities)
+            .execute(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
     }
 
     @Test
-    fun should_ReturnSortedList_when_TheInputIsMultiCitySomeIncluded() {
-        // given
+    fun should_ReturnSortedList_When_TheInputIsMultiCitySomeIncluded() {
+        // given multi city some of this is included
         val cities = arrayOf("Santiago de Cuba", "Sancti Spiritus", "Galaxy")
 
         // when the output is list of sorted cities by Banana Price
         val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(citiesNames = cities)
+            .execute(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
@@ -75,12 +75,12 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
 
     @Test
     fun should_ReturnSortedList_when_TheInputIsMultiCityAllNotIncluded() {
-        // given
+        // given multi city all of this is not included
         val cities = arrayOf("", "Farwala", "Galaxy")
 
         // when the output is empty list
         val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(citiesNames = cities)
+            .execute(*cities)
 
         // then check
         assertEquals(emptyList<String>(), result)
