c55319a466229f03ea18cdff8dc0f7ddb13f8cd3
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2023-02-10 02:30:47 +0200

    add GetTenCitiesNameToBuyDepartmentFaster interactor

---
+++ src/main/kotlin/interactor/GetTenCitiesNameToBuyDepartmentFaster.kt
@@ -0,0 +1,9 @@
+package interactor
+
+class GetTenCitiesNameToBuyDepartmentFaster(
+    private val dataSource: CostOfLivingDataSource,
+    ) {
+    fun execute(): List<Pair<String, Double>> {
+        throw Throwable("Not Implemented yet")
+    }
+}
