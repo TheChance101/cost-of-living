512cd0a0068eeb562264f81c15bfc9529e294453
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-02-10 11:14:29 +0200

    setup GetCheapestBananaPricesCitiesNamesInteractor class

--- src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
@@ -2,6 +2,11 @@ package interactor
 
 import model.CityEntity
 
-class GetCheapestBananaPricesCitiesNamesInteractor(){
+class GetCheapestBananaPricesCitiesNamesInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
 
+    fun execute(vararg cityEntities: CityEntity): List<String> {
+        throw Throwable("Not Implemented yet")
+    }
 }
