e080824c6eed35de03167c5b5b4b43ea9d43d4ce
jinan <jinanaqb@gmail.com>
2023-02-10 20:43:15 +0300

    implementation get cities and years to buy apartment

--- src/main/kotlin/interactor/GetCitiesAndYearsToBuyApartmentInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesAndYearsToBuyApartmentInteractor.kt
@@ -5,6 +5,29 @@ import model.CityEntity
 class GetCitiesAndYearsToBuyApartmentInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
-    fun execute(limit: Int): List<Pair<String?,String>> =  listOf(Pair("",""))
+
+    fun execute(limit: Int): List<Pair<String?,String>> =
+        dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullValueAndLowQualityData)
+            .sortedBy(::calculateYearsNeededToBuyApartment)
+            .take(limit)
+            .map { Pair(it.cityName, "${calculateYearsNeededToBuyApartment(it)} year") }
+
+
+    private fun excludeNullValueAndLowQualityData(city: CityEntity): Boolean =   //the value are Salaries , Prices Per SquareMeter and city name
+        city.let {
+            it.dataQuality &&
+            it.cityName != null &&
+            it.averageMonthlyNetSalaryAfterTax != null &&
+            it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null
+        }
+
+
+    private fun calculateYearsNeededToBuyApartment(city: CityEntity): Int =
+        city.let {
+            ( it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!! *100).toInt()  /
+                    (it.averageMonthlyNetSalaryAfterTax!! * 12).toInt()
+        }
 
 }
