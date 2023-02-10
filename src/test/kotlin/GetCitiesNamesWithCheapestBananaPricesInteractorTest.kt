56e0b5c4256bbdd7cb3a5c244eaabe2b18bfed30
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-10 23:28:20 +0200

    add two test case

--- src/test/kotlin/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
+++ src/test/kotlin/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
@@ -1,15 +1,13 @@
-package interactor
-
 import dataSource.utilis.FakeDataSource
-import model.*
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import interactor.GetCitiesNamesWithCheapestBananaPricesInteractor
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
+    // region init
     private lateinit var getCitiesNamesWithCheapestBananaPricesInteractor: GetCitiesNamesWithCheapestBananaPricesInteractor
     private lateinit var fakeDataSource: FakeDataSource
 
@@ -19,8 +17,9 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
         getCitiesNamesWithCheapestBananaPricesInteractor =
             GetCitiesNamesWithCheapestBananaPricesInteractor(fakeDataSource)
     }
+    // endregion
 
-    // region one city
+    // region one city cases
     @Test
     fun should_ReturnOneCity_When_TheInputIsOneCityIncluded() {
         // given one city included
@@ -44,9 +43,33 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
         // then check 
         assertEquals(emptyList<String>(), result)
     }
+
+    @Test
+    fun should_ReturnOneCity_When_TheInputIsOneCityIncludedInLowercase() {
+        // given one city included
+        val city = "santiago de cuba"
+
+        // when the output is list of one city
+        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+
+        // then check
+        assertEquals(listOf("Santiago de Cuba"), result)
+    }
+
+    @Test
+    fun should_ReturnOneCity_When_TheInputIsOneCityIncludedInLowercaseAndMoreOuterSpace() {
+        // given one city included
+        val city = " santiago de cuba "
+
+        // when the output is list of one city
+        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+
+        // then check
+        assertEquals(listOf("Santiago de Cuba"), result)
+    }
     // endregion
 
-    // region multi city
+    // region multi city cases
     @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCityAllIncluded() {
         // given multi city all of this is included
@@ -61,9 +84,35 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     }
 
     @Test
+    fun should_ReturnSortedList_When_TheInputIsMultiCityAllIncludedInDifferentCase() {
+        // given multi city all of this is included
+        val cities = arrayOf("SaNtiago de cuba", "sAncti spirItus")
+
+        // when the output is list of sorted cities by Banana Price
+        val result = getCitiesNamesWithCheapestBananaPricesInteractor
+            .execute(*cities)
+
+        // then check
+        assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
+    }
+
+    @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCitySomeIncluded() {
         // given multi city some of this is included
         val cities = arrayOf("Santiago de Cuba", "Sancti Spiritus", "Galaxy")
+
+        // when the output is list of sorted cities by Banana Price
+        val result = getCitiesNamesWithCheapestBananaPricesInteractor
+            .execute(*cities)
+
+        // then check
+        assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
+    }
+
+    @Test
+    fun should_ReturnSortedList_When_TheInputIsMultiCitySomeIncludedInDifferentCases() {
+        // given multi city some of this is included
+        val cities = arrayOf("santiago de cuba", "Sancti Spiritus", "Galaxy")
 
         // when the output is list of sorted cities by Banana Price
         val result = getCitiesNamesWithCheapestBananaPricesInteractor
