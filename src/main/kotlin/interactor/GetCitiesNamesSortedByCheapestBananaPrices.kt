d064c4ba31e4d4153e738a257c63fe685fcd68ac
Asia sama <sehunexo710@gmail.com>
2023-02-11 23:53:55 +0300

    add two test case for get  cities names sorted by cheapest banana prices

---
+++ src/main/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPrices.kt
@@ -0,0 +1,11 @@
+package interactor
+
+
+class GetCitiesNamesSortedByCheapestBananaPrices(
+    private val dataSource: CostOfLivingDataSource
+) {
+    fun execute(vararg cities: String): List<String> {
+        throw Throwable("Not Implemented yet")
+    }
+
+}
---
+++ src/test/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPricesTest.kt
@@ -0,0 +1,42 @@
+package interactor
+
+import FakeData
+import model.CityEntity
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.function.Executable
+import kotlin.test.assertEquals
+
+class GetCitiesNamesSortedByCheapestBananaPricesTest {
+
+    private lateinit var fakeData: FakeData
+    private lateinit var getCitiesNamesSortedByCheapestBananaPrices: GetCitiesNamesSortedByCheapestBananaPrices
+
+    @BeforeEach
+    fun setUp() {
+        fakeData = FakeData()
+        getCitiesNamesSortedByCheapestBananaPrices = GetCitiesNamesSortedByCheapestBananaPrices(fakeData)
+    }
+
+    @Test
+    fun should_return_citesName_when_enter_correct_list() {
+        //give list of cites with upper case
+        val cites = listOf("Caracas","Accra","Giza")
+        //when
+        val result = getCitiesNamesSortedByCheapestBananaPrices.execute(*cites.toTypedArray())
+        //Then
+        assertEquals(listOf("Giza", "Accra", "Caracas"),result)
+    }
+    @Test
+    fun should_return_citesName_when_enter_lowerCaseOrUpperCaseValue() {
+        //give list of cites with lower case
+        val cites = listOf("caracas","accra","giza")
+        //when
+        val result = getCitiesNamesSortedByCheapestBananaPrices.execute(*cites.toTypedArray())
+        //Then
+        assertEquals(listOf("Giza", "Accra", "Caracas"),result)
+    }
+
+
+}
