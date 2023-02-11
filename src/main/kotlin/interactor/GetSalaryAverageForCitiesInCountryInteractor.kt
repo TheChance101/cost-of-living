de54d9df9949371691200541d63e2ff70bfc5924
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-11 11:32:25 +0200

    1- add Class GetSalaryAverageForCitiesInCountryInteractor 2- add Class GetSalaryAverageForCitiesInCountryInteractorTest 3- Create FakeData

---
+++ src/main/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractor.kt
@@ -0,0 +1,19 @@
+package interactor
+
+import model.CityEntity
+
+/**
+ * Created by Mohamed Elgohary on 2/9/2023.
+ */
+
+class GetSalaryAverageForCitiesInCountryInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
+    fun execute(countryName: String): List<Pair<String, Float>> {
+        return listOf(Pair("", 1.0f))
+    }
+
+     fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+         return true
+    }
+}
