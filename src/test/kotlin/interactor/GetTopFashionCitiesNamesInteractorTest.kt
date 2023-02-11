c53cbb2353f646b63907ce44088873a4a5b354a2
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2023-02-12 02:20:31 +0300

    create more tests

--- src/test/kotlin/interactor/GetTopFashionCitiesNamesInteractorTest.kt
+++ src/test/kotlin/interactor/GetTopFashionCitiesNamesInteractorTest.kt
@@ -1,12 +1,10 @@
 package interactor
 
 import dataSource.FakeDataSource
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.function.Executable
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetTopFashionCitiesNamesInteractorTest {
@@ -20,34 +18,71 @@ class GetTopFashionCitiesNamesInteractorTest {
     }
 
     @Test
-    fun should_Return_Top_5_When_Limit_Is_Five() {
-        // given limit as 5
+    fun `should return only the required number of results`() {
+        // given limit
         val limit = 5
         // when get top 5 cities names
         val result = getTopFashionCitiesNamesInteractor.execute(limit)
-        // then result should be 5 cities names
-        assertEquals(5, result.size)
+        // then result should only contain the required number of cities
+        assertEquals(limit, result.size)
     }
 
     @Test
-    fun should_Return_Empty_List_When_Limit_Is_Zero() {
-        // given limit as 0
+    fun `should return ascending sorted list when limit is valid`() {
+        // given limit and expected result list
+        val limit = 5
+        val expected = listOf(
+            "Dhangadhi",
+            "Narayanganj",
+            "Banjul",
+            "Kasese",
+            "Sri Jayewardenepura Kotte"
+        )
+        // when get top fashion cities names
+        val result = getTopFashionCitiesNamesInteractor.execute(limit)
+        // then result should be equal to expected list
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return empty list when limit is zero`() {
+        // given limit
         val limit = 0
-        // when get top 5 cities names
+        // when get top fashion cities names
         val result = getTopFashionCitiesNamesInteractor.execute(limit)
         // then result should be empty list
         assertTrue(result.isEmpty())
     }
 
     @Test
-    fun should_Throw_Exception_When_Limit_Is_Negative() {
-        // given limit as negative
+    fun `should return empty list when limit is negative`() {
+        // given limit in negative
         val limit = -1
-        // when get top 5 cities names
-        val executable = Executable { getTopFashionCitiesNamesInteractor.execute(limit) }
-        // then exception should be thrown
-        assertThrows(IllegalArgumentException::class.java, executable)
+        // when get top fashion cities names
+        val result = getTopFashionCitiesNamesInteractor.execute(limit)
+        // then result should be empty list
+        assertTrue(result.isEmpty())
     }
 
+    @Test
+    fun `should return available list when limit is greater than available`() {
+        // given limit and expected result
+        val limit = 1000
+        val expected = 9
+        // when get top fashion cities names
+        val result = getTopFashionCitiesNamesInteractor.execute(limit)
+        // then result should be equal to a valid available list
+        assertEquals(expected, result.size)
+    }
 
+    @Test
+    fun `should return unique top fashion cities names when limit is valid`() {
+        //TODO: Adding more fake data to test this case
+        // given limit
+        val limit = 9
+        // when get top fashion cities names
+        val result = getTopFashionCitiesNamesInteractor.execute(limit)
+        // then result should not contain duplicates
+        assertEquals(result.toSet().size, result.size)
+    }
 }
