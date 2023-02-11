6011682048d9cc18418aec155a171162f3bc8ad1
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-02-11 15:33:40 +0200

    solve first test case

--- src/main/kotlin/interactor/GetCityNamesClothesPrices.kt
+++ src/main/kotlin/interactor/GetCityNamesClothesPrices.kt
@@ -4,10 +4,13 @@ import model.CityEntity
 import model.CityNameClothesPrices
 
 class GetCityNamesClothesPrices(
-    private val dataSource: CostOfLivingDataSource,
+    private val dataSource: List<CityEntity>,
 ) {
 
     fun execute( ): List<CityNameClothesPrices> {
+
+        if(dataSource.size == 0  ) throw  Exception (" the data you pass hasn't city ")
+
         return listOf()
     }
 }
