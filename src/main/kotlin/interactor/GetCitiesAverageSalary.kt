23b891d9df6815744f0a90ff29e7cd51a4d641db
Ali zainy <alizainy0123456789@gmail.com>
2023-02-12 02:14:52 +0300

    implement execute function for get cities average salary

--- src/main/kotlin/interactor/GetCitiesAverageSalary.kt
+++ src/main/kotlin/interactor/GetCitiesAverageSalary.kt
@@ -1,9 +1,17 @@
 package interactor
 
-class GetCitiesAverageSalary( private val dataSource: CostOfLivingDataSource)
-{
-    fun execute(country:String):List<Pair<String,Double>>
-    {
-        return emptyList()
+class GetCitiesAverageSalary(private val dataSource: CostOfLivingDataSource) {
+
+    fun execute(country: String): List<Pair<String, Double>> {
+        val countryData = dataSource.getAllCitiesData().filter {
+            it.country.equals(country, ignoreCase = true) && it.dataQuality
+        }
+
+        return if (countryData.isEmpty()) {
+            throw IllegalArgumentException("Invalid country name.")
+        } else {
+            countryData.map { Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!.toDouble()) }
+        }
     }
+
 }
