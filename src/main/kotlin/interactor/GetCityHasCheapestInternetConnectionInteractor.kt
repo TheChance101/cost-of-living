3595ca77bf5e8a4783dc4530547dc625f4df8c12
David samuel <davidsamuelramsis@gmail.com>
2023-02-11 12:56:14 +0200

    improve code to be readable

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,11 +6,17 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(cities:List<CityEntity>): CityEntity?
-     = if (cities.isEmpty()) null else cities.filter {
-         excludeNullFromSalariesNullInternetPriceAndLowQualityData(it)
-    }.sortedBy { cityEntity -> (cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl?.
-    div(cityEntity.averageMonthlyNetSalaryAfterTax!!))?.times(100) }[0]
+    fun execute(cities:List<CityEntity>): CityEntity? {
+         return if (cities.isEmpty()) {
+            null
+        }else {
+             cities.filter {
+                 excludeNullFromSalariesNullInternetPriceAndLowQualityData(it)
+             }.sortedBy { cityEntity -> (cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl?.div(cityEntity.averageMonthlyNetSalaryAfterTax!!))?.times(
+                 100)
+             }[0]
+         }
+    }
 
     private fun excludeNullFromSalariesNullInternetPriceAndLowQualityData(city: CityEntity): Boolean {
         return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
