ce9fae440c3f1ac78660bf412ce84ac950f13e03
Momen Mustafa Al_Hafidh <momen.myasar@gmail.com>
2023-02-11 11:58:09 +0300

    edit test class and move its file to the folder test

--- src/test/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaidTest.kt
+++ src/test/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaidTest.kt
@@ -159,7 +159,7 @@ internal class GetCitiesHasLowestFruitVegPricesComparingSalariesPaidTest {
 // all cities have same fruit-veg prices but different salaries
 // Iran and Egypt should not be in the new list after the test
 
-    val correctTestedList = list.filter { it.cityName != "Ciro" && it.cityName != "Tahran" }
+    val correctTestedList = list.filter { it.cityName != "Ciro" && it.cityName != "Tahran" }.sortedByDescending { it.averageMonthlyNetSalaryAfterTax }
     @BeforeAll
     fun setup(){
         getCities = GetCitiesHasLowestFruitVegPricesComparingSalariesPaid(list)
@@ -174,4 +174,13 @@ internal class GetCitiesHasLowestFruitVegPricesComparingSalariesPaidTest {
     //then
         assertEquals(correctTestedList,listToTest)
     }
+//    @Test
+//    fun shouldReturnFalseResultWhenLowNumberOfListIsGiven() {
+//        //given a list that has less than 10 objects of CityEntity
+//        val innerList = list
+//        // when find 10 cities that has lowest fruitVeg prices comparing to salaries paid there
+//        val listToTest = getCities.execute()
+//        //then
+//        assertEquals(correctTestedList,listToTest)
+//    }
 }
