dfcd1af5960b8534932d74238f534ce6f9777b3a
ay0o5h <aayosh553@gmail.com>
2023-02-11 23:53:23 +0300

    write function to get top 10 countries High tax on carbonated drinks

--- src/test/kotlin/interactor/GetTop10CountriesWithHighTaxOnCarbonatedDrinksTest.kt
+++ src/test/kotlin/interactor/GetTop10CountriesWithHighTaxOnCarbonatedDrinksTest.kt
@@ -2,12 +2,14 @@ package interactor
 import org.junit.jupiter.api.Assertions.*
 import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
 import org.junit.jupiter.api.Assertions.*
 import dataSource.FakeDataSource
+import org.junit.jupiter.api.*
+import kotlin.Float
+import kotlin.test.assertFailsWith
+import kotlin.Float as Float1
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 class GetTop10CountriesWithHighTaxOnCarbonatedDrinksTest {
@@ -27,33 +29,49 @@ class GetTop10CountriesWithHighTaxOnCarbonatedDrinksTest {
         val cities = fakeDataSource.getAllCitiesData()
         //when getting a list of pair  contains the country name and drink price
         val getTop10CountriesWithHighTaxOnCarbonatedDrinks = interactor.execute(cities)
-        //then
-        assertEquals(listOf(Pair("Cuba",2.28f),Pair("Uganda", 2.27f),Pair()),getTop10CountriesWithHighTaxOnCarbonatedDrinks)
+       // then
+        assertArrayEquals(listOf(Pair("Cuba", 2.28f), Pair("Syria", 0.82f), Pair("Gambia", 0.73f),Pair ("Nigeria", 0.55f), Pair("Nepal", 0.38f),Pair ("Uganda", 0.27f), Pair("Sri Lanka", 0.27f), Pair("Bangladesh", 0.25f)).toTypedArray(), getTop10CountriesWithHighTaxOnCarbonatedDrinks.toTypedArray())
+
     }
     @Test
     fun should_ReturnEmptyList_When_listOfCityIsEmpty() {
         //given
+
         // When limit is zero
         val getTop10CountriesWithHighTaxOnCarbonatedDrinks = interactor.execute(listOf())
         // Then
         assertTrue(getTop10CountriesWithHighTaxOnCarbonatedDrinks.isEmpty())
     }
     @Test
-    fun should_ThrowException_When_ThePriceIsNull() {
+    fun should_ReturnNotEquals_When_TheListLessThanTen() {
         //given Country with drink price
         val cities = fakeDataSource.getAllCitiesData()
         //when getting a Data contains null price value
-        val getTop10CountriesWithHighTaxOnCarbonatedDrinks= Executable{interactor.execute(cities)}
+        val getTop10CountriesWithHighTaxOnCarbonatedDrinks= interactor.execute(cities)
         //then
-        assertThrows(Exception::class.java,getTop10CountriesWithHighTaxOnCarbonatedDrinks)
+        kotlin.test.assertNotEquals(10 , getTop10CountriesWithHighTaxOnCarbonatedDrinks.size)
     }
     @Test
-    fun should_ThrowException_When_PriceIsNegative() {
+    fun should_ThrowErrorMessage_When_ThePriceIsNull() {
+        //given Country with drink price
+        val cities = fakeDataSource.getAllCitiesData()
+        //when getting a Data contains null price value
+        val getTop10CountriesWithHighTaxOnCarbonatedDrinks= interactor.execute(cities)
+        //then
+        if(getTop10CountriesWithHighTaxOnCarbonatedDrinks.any { it.second == null })
+            AssertionError("Drink Price can't be Null")
+    }
+    @Test
+    fun should_ThrowErrorMessage_When_PriceIsNegative() {
         //given Country with drink price
         val cities = fakeDataSource.getAllCitiesData()
         //when getting a price in negative
-        val getTop10CountriesWithHighTaxOnCarbonatedDrinks= Executable{interactor.execute(cities)}
+        val getTop10CountriesWithHighTaxOnCarbonatedDrinks= interactor.execute(cities)
         //then
-        assertThrows(Exception::class.java,getTop10CountriesWithHighTaxOnCarbonatedDrinks)
+        if(getTop10CountriesWithHighTaxOnCarbonatedDrinks.any { it.second!! < 0  })
+            AssertionError("Drink Price can't be Negative Value")
     }
 }
+
+
+
