8abce212a15e93ee80a8d8de208a007c981f6a15
Rehab <rehab.fakhry20@gmail.com>
2023-02-11 12:39:17 +0200

    implement functions in class GetSalaryAverageForCitiesInCountryInteractor

--- src/main/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractor.kt
+++ src/main/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractor.kt
@@ -9,11 +9,19 @@ class GetSalaryAverageForCitiesInCountryInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(countryName: String): List<Pair<String, Float>> {
-        return listOf(Pair("", 1.0f))
+        val citiesInCountry = dataSource.getAllCitiesData()
+            .filter { it.country.equals(countryName, true) }
+
+        if (citiesInCountry.isEmpty()) {
+            throw Exception("No cities found in country $countryName")
+        }
+
+        return citiesInCountry
+            .filter(::excludeNullSalariesAndLowQualityData)
+            .map { Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!) }
     }
 
     fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
-
-        return true
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
     }
 }
