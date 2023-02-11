b5f0f05b2961985175d51512dc58df6998f80577
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-11 15:31:06 +0200

    implement function excute in GetCheapestBananaCitiesNamesInteractor

--- src/main/kotlin/interactor/GetCheapestBananaCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestBananaCitiesNamesInteractor.kt
@@ -5,8 +5,16 @@ import model.CityEntity
 class GetCheapestBananaCitiesNamesInteractor(private val dataSource: CostOfLivingDataSource) {
 
     fun execute(vararg cities: String): List<String> {
-         TODO("return sorted list of cities by cheapest banana prices")
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullBananaPricesAndLowQualityData)
+            .filter { it.cityName in cities }
+            .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
+            .map { it.cityName }
     }
 
+    private fun excludeNullBananaPricesAndLowQualityData(city: CityEntity): Boolean {
+        return city.fruitAndVegetablesPrices.banana1kg != null && city.dataQuality
+    }
 
 }
