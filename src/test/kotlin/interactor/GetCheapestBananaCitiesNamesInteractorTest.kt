6bb35bebdccf0b74aa7e133c1b05ecb1f8e80d8b
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-11 14:05:47 +0200

    add test cases for function execute in GetCheapestBananaCitiesNamesInteractor

--- src/test/kotlin/interactor/GetCheapestBananaCitiesNamesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCheapestBananaCitiesNamesInteractorTest.kt
@@ -5,7 +5,9 @@ import dataSource.utils.CsvParser
 import io.mockk.clearAllMocks
 import io.mockk.unmockkAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetCheapestBananaCitiesNamesInteractorTest {
@@ -13,6 +15,7 @@ internal class GetCheapestBananaCitiesNamesInteractorTest {
     private lateinit var interactor: GetCheapestBananaCitiesNamesInteractor
     private val csvParser = CsvParser()
     private val dataSource = CsvDataSource(csvParser)
+//    private val entity: CityEntity = mockk()
 
     @BeforeAll
     fun setup() {
@@ -21,4 +24,93 @@ internal class GetCheapestBananaCitiesNamesInteractorTest {
         interactor = GetCheapestBananaCitiesNamesInteractor(dataSource)
     }
 
+    @Test
+    fun `should return entered single city when have correct city with correct banana value`() {
+        // given correct city with correct banana value
+        val city: String = "Alexandria"
+        // when get cities list
+        val cities = interactor.execute(city)
+        // then if getting entered city in a list
+        assertEquals(listOf("Alexandria"), cities)
+    }
+
+    @Test
+    fun `should return empty list when have single incorrect city`() {
+        // given Incorrect city
+        val city: String = "BlaBlaBla"
+        // when get cities list
+        val cities = interactor.execute(city)
+        // then if getting empty list
+        assertEquals(emptyList(), cities)
+    }
+
+    @Test
+    fun `should return empty list when have single correct city with incorrect banana value`() {
+        // given correct city with incorrect banana value
+        val city: String = "Tamale"
+        // when get cities list
+        val cities = interactor.execute(city)
+        // then if getting empty list
+        assertEquals(emptyList(), cities)
+    }
+
+    @Test
+    fun `should return empty list when have single correct city and it's dataQuality is false`() {
+        // given correct city with incorrect banana value
+        val city: String = "Tamale"
+        // when get cities list
+        val cities = interactor.execute(city)
+        // then if getting empty list
+        assertEquals(emptyList(), cities)
+    }
+
+    @Test
+    fun `should return sorted correct cities when have all correct cities data`() {
+        // given list of correct cities with true data quality and correct banana price
+        val citiesList = listOf("Havana", "Damascus", "Alexandria")
+        // when get sorted cities list by cheapest price
+        val cities = interactor.execute(citiesList.toString())
+        // then if getting correct list
+        assertEquals(listOf("Alexandria", "Havana", "Damascus"), cities)
+    }
+
+    @Test
+    fun `should return sorted correct cities when have list of correct cities with one or more city is not correct typing`() {
+        // given list of correct cities with true data quality and correct banana price but there is one incorrect typing
+        val citiesList = listOf("Havana", "Damascus", "Alexandria", "BlaBlaBla")
+        // when get sorted cities list by cheapest price
+        val cities = interactor.execute(citiesList.toString())
+        // then if getting correct list
+        assertEquals(listOf("Alexandria", "Havana", "Damascus"), cities)
+    }
+
+    @Test
+    fun `should return sorted correct cities when have list of correct cities with one or more city has incorrect dataQuality or banana price`() {
+        // given list of correct cities with true data quality and correct banana price but there is one or more city is not incorrect dataQuality or incorrect banana price
+        val citiesList = listOf("Havana", "Damascus", "Alexandria", "Moratuwa")
+        // when get sorted cities list by cheapest price
+        val cities = interactor.execute(citiesList.toString())
+        // then if getting correct list
+        assertEquals(listOf("Alexandria", "Havana", "Damascus"), cities)
+    }
+
+    @Test
+    fun `should return empty list when have all cities incorrect typing , incorrect dataQuality or incorrect banana price`() {
+        // given list of all Incorrect cities
+        val citiesList = listOf("BlaBlaBla", "Uyo", "Tamale", "Moratuwa")
+        // when get empty list
+        val cities = interactor.execute(citiesList.toString())
+        // then if getting empty list
+        assertEquals(listOf(), cities)
+    }
+
+    @Test
+    fun `should return the only correct city from many incorrect cities when  all given only one city satisfies conditions in all given cities`() {
+        // given list of all Incorrect cities except only one city satisfies all conditions
+        val citiesList = listOf("BlaBlaBla", "Uyo", "Tamale", "Moratuwa" , "Alexandria")
+        // when get sorted cities list by cheapest price
+        val cities = interactor.execute(citiesList.toString())
+        // then if the only correct city
+        assertEquals(listOf("Alexandria"), cities)
+    }
 }
