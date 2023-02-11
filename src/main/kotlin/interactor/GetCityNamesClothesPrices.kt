bb44736f57aa202be1500f9d02bdd9c2aee9ac13
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-02-11 15:52:50 +0200

    add code in class

--- src/main/kotlin/interactor/GetCityNamesClothesPrices.kt
+++ src/main/kotlin/interactor/GetCityNamesClothesPrices.kt
@@ -11,6 +11,17 @@ class GetCityNamesClothesPrices(
 
         if(dataSource.size == 0  ) throw  Exception (" the data you pass hasn't city ")
 
-        return listOf()
+        return dataSource.map (gitCityNamesClothesPrices )
+    }
+
+    private val gitCityNamesClothesPrices = { city : CityEntity ->
+        val cityNameClothesPrices = CityNameClothesPrices(
+            cityName = city.cityName ,
+            onePairOfJeansLevis50oneOrSimilar = city.clothesPrices.onePairOfJeansLevis50oneOrSimilar,
+            oneSummerDressInAChainStoreZaraHAndM =city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM,
+            onePairOfMenLeatherBusinessShoes= city.clothesPrices.onePairOfMenLeatherBusinessShoes,
+            onePairOfNikeRunningShoesMidRange = city.clothesPrices.onePairOfNikeRunningShoesMidRange
+        )
+        cityNameClothesPrices
     }
 }
