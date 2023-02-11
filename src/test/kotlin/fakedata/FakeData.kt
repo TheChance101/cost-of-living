d30b0aee7b0f3c7b0ae2db19c780c3dcac4e761b
kamel <kikokeko10@gmail.com>
2023-02-11 22:02:37 +0200

    active test package

---
+++ src/test/kotlin/fakedata/FakeData.kt
@@ -0,0 +1,22 @@
+package fakedata
+
+import dataSource.CsvDataSource
+import dataSource.utils.CsvParser
+import interactor.CostOfLivingDataSource
+import model.CityEntity
+
+class FakeData : CostOfLivingDataSource {
+    var fakeDataList: ArrayList<CityEntity> = ArrayList()
+
+    init {
+        val csvParser = CsvParser()
+        val dataSource = CsvDataSource(csvParser)
+        fakeDataList.addAll(
+            dataSource.getAllCitiesData().take(10)
+        )
+    }
+
+    override fun getAllCitiesData(): List<CityEntity> {
+        return fakeDataList
+    }
+}
