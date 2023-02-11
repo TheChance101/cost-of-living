6b1e28566426de5bc910b1117d2f6ce5a0baff08
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-02-11 20:44:15 +0200

    create class test and function test

---
+++ src/main/kotlin/interactor/GetCitiesNamesSortedCheapestBananPrices.kt
@@ -0,0 +1,12 @@
+package interactor
+
+import model.CityEntity
+
+class GetCitiesNamesSortedCheapestBananPrices {
+
+    fun excute(vararg cities:CityEntity):List<String>{
+        return emptyList()
+    }
+
+
+}
