abe89d40998f30d59dcba4a60af5cfdec5dc7698
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-11 20:10:04 +0200

    change place class Fake Data add test function

--- src/test/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractorTest.kt
+++ src/test/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractorTest.kt
@@ -60,7 +60,7 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest(
     @Test
     fun should_ReturnException_When_CountryNameIsEmpty() {
         val countryName = ""
-        val salary:Executable = Executable { getSalaryAverageForCitiesInCountryInteractor.execute(countryName) }
+        val salary: Executable = Executable { getSalaryAverageForCitiesInCountryInteractor.execute(countryName) }
         assertThrows(Exception::class.java, salary)
     }
 
@@ -141,6 +141,86 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest(
 
         // Then
         assertFalse(result)
+    }
+
+    @Test
+    fun should_ReturnTrue_When_SalariesNotEqualNullAndAvoidLowQualityData() {
+        // Given
+        val city = CityEntity(
+            cityName = "Tanta",
+            country = "Egypt",
+            mealsPrices = MealsPrices(
+                mealInexpensiveRestaurant = 4.07f,
+                mealFor2PeopleMidRangeRestaurant = 14.24f,
+                mealAtMcDonaldSOrEquivalent = 2.85f
+            ),
+            drinksPrices = DrinksPrices(
+                cappuccinoRegularInRestaurants = 1.15f,
+                cokePepsiAThirdOfLiterBottleInRestaurants = 0.27f,
+                waterAThirdOfLiterBottleInRestaurants = 0.16f,
+                milkRegularOneLiter = 0.73f,
+                waterOneAndHalfLiterBottleAtTheMarket = 0.23f
+            ),
+            fruitAndVegetablesPrices = FruitAndVegetablesPrices(
+                apples1kg = 0.75f,
+                banana1kg = 0.5f,
+                oranges1kg = 0.26f,
+                tomato1kg = 0.53f,
+                potato1kg = 0.37f,
+                onion1kg = 0.37f,
+                lettuceOneHead = 0.16f
+            ),
+            foodPrices = FoodPrices(
+                loafOfFreshWhiteBread500g = 0.33f,
+                riceWhite1kg = 0.5f,
+                eggsRegular12 = 1.08f,
+                localCheese1kg = 2.4f,
+                chickenFillets1kg = 2.65f,
+                beefRound1kgOrEquivalentBackLegRedMeat = 6.31f
+            ),
+            servicesPrices = ServicesPrices(
+                basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = 20.35f,
+                oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.01f,
+                internet60MbpsOrMoreUnlimitedDataCableAdsl = 9.77f,
+                fitnessClubMonthlyFeeForOneAdult = 10.17f,
+                tennisCourtRentOneHourOnWeekend = 5.6f,
+                cinemaInternationalReleaseOneSeat = 3.66f,
+                preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = 50.87f,
+                internationalPrimarySchoolYearlyForOneChild = 1078.49f
+            ),
+            clothesPrices = ClothesPrices(
+                onePairOfJeansLevis50oneOrSimilar = 14.24f,
+                oneSummerDressInAChainStoreZaraHAndM = 32.05f,
+                onePairOfNikeRunningShoesMidRange = 89.54f,
+                onePairOfMenLeatherBusinessShoes = 28.49f
+            ),
+            transportationsPrices = TransportationsPrices(
+                oneWayTicketLocalTransport = 0.12f,
+                monthlyPassRegularPrice = 1.02f,
+                taxiStartNormalTariff = 0.41f,
+                taxi1kmNormalTariff = 0.2f,
+                taxi1hourWaitingNormalTariff = 0.73f,
+                gasolineOneLiter = 0.38f
+            ),
+            carsPrices = CarsPrices(
+                volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = 20348.95f,
+                toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = 16228.29f
+            ),
+            realEstatesPrices = RealEstatesPrices(
+                apartmentOneBedroomInCityCentre = 156.01f,
+                apartmentOneBedroomOutsideOfCentre = 71.22f,
+                apartment3BedroomsInCityCentre = 196.71f,
+                pricePerSquareMeterToBuyApartmentInCityCentre = 335.76f
+            ),
+            averageMonthlyNetSalaryAfterTax = 101.74f,
+            dataQuality = true
+        )
+
+        // When
+        val result = getSalaryAverageForCitiesInCountryInteractor.excludeNullSalariesAndLowQualityData(city)
+
+        // Then
+        assertTrue(result)
     }
 
 
