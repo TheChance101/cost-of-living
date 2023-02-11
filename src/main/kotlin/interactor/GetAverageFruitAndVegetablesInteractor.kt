6606ac55abba546882401bb8db461c35963fcea4
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-02-11 23:09:22 +0100

    complete execute function

--- src/main/kotlin/interactor/GetAverageFruitAndVegetablesInteractor.kt
+++ src/main/kotlin/interactor/GetAverageFruitAndVegetablesInteractor.kt
@@ -1,9 +1,36 @@
 package interactor
 
+import model.CityEntity
+import model.FruitAndVegetablesPrices
+
 class GetAverageFruitAndVegetablesInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
     fun execute(): List<String> {
-        return emptyList()
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullAndNonNumberSalaries)
+            .sortedBy { it.fruitAndVegetablesPrices.getAverageFruitVegetablesPrice() / it.averageMonthlyNetSalaryAfterTax!! }
+            .take(10)
+            .map { it.cityName }
     }
+
+    private fun excludeNullAndNonNumberSalaries(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && !city.averageMonthlyNetSalaryAfterTax.isNaN()
+    }
+
+    private fun FruitAndVegetablesPrices.getAverageFruitVegetablesPrice(): Double {
+        return with(this) {
+            mutableListOf(
+                tomato1kg,
+                potato1kg,
+                lettuceOneHead,
+                oranges1kg,
+                onion1kg,
+                banana1kg,
+                apples1kg
+            ).filterNotNull().average()
+        }
+    }
+
 }
