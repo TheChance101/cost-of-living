b4c56680d41824a7db1c560a05b6937477a081c1
Momen Mustafa Al_Hafidh <momen.myasar@gmail.com>
2023-02-11 19:05:20 +0300

    finish the functionality of this class

--- src/main/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaid.kt
+++ src/main/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaid.kt
@@ -1,6 +1,7 @@
 package interactor
 
 import model.CityEntity
+import model.FruitAndVegetablesPrices
 
 class GetCitiesHasLowestFruitVegPricesComparingSalariesPaid
 {
@@ -15,9 +16,33 @@ class GetCitiesHasLowestFruitVegPricesComparingSalariesPaid
     }
 
     fun execute(): List<CityEntity> {
-
+        if(dataSource != null)
+            cityEntitylist = dataSource!!.getAllCitiesData()
 
         return cityEntitylist
+            .filter(::excludeNullSalaries)
+            .sortedBy { excludeNullFruitVegPrices(it.fruitAndVegetablesPrices).div(it.averageMonthlyNetSalaryAfterTax!!) }
+            .subList(0,10)
+    }
+    private fun excludeNullSalaries(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null
+    }
+    private fun excludeNullFruitVegPrices(fruitAndVegetablesPrices: FruitAndVegetablesPrices): Float {
+        return fruitAndVegetablesPrices.apples1kg?.let { apples ->
+                    fruitAndVegetablesPrices.banana1kg?.let { bananas ->
+                        fruitAndVegetablesPrices.lettuceOneHead?.let { lettuce ->
+                            fruitAndVegetablesPrices.onion1kg?.let { onions ->
+                                fruitAndVegetablesPrices.oranges1kg?.let { oranges ->
+                                    fruitAndVegetablesPrices.potato1kg?.let { potatoes ->
+                                        fruitAndVegetablesPrices.tomato1kg?.let { tomatoes ->
+                                            apples + bananas + lettuce + onions + oranges + potatoes + tomatoes
+                                        } ?: 0f
+                                    } ?: 0f
+                                } ?: 0f
+                            } ?: 0f
+                        } ?: 0f
+                    } ?: 0f
+            } ?: 0f
     }
 
 
