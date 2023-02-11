075827d00e29ceb67fe373b36e1e0ceb0906afcd
kareem eldeen esam <kareemesam52@yahoo.com>
2023-02-11 22:15:09 +0200

    implemented execute function

--- src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
+++ src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
@@ -1,17 +1,40 @@
 package interactor
 
 import model.CityEntity
-
-
-
+import model.DrinksPrices
 
 
 class GetHighestCarbonatedDrinksPricesInteractor(private val dataSource: CostOfLivingDataSource)
 {
-    fun execute () : List<Pair<String,Double>> {
+    fun execute () : List<Pair<String,Float>> {
 
-      var city : Pair<String,Double> = Pair(dataSource.getAllCitiesData().get(0).toString(),dataSource.getAllCitiesData().get(2).drinksPrices.toString().toDouble())
-        val listofcities : MutableList<Pair<String,Double>> = mutableListOf(city)
-        return listofcities
+      val city : List<CityEntity> = dataSource.getAllCitiesData().filter { nullDrinks(it) }
+          val listOfCities  = sortingDescending(city)
+        return pricesAvg(listOfCities)
     }
 }
+private fun nullDrinks (cityEntity: CityEntity):Boolean =
+            cityEntity.drinksPrices.cappuccinoRegularInRestaurants != null &&
+            cityEntity.drinksPrices.milkRegularOneLiter !=null &&
+            cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants !=null &&
+            cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants !=null &&
+            cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket !=null
+
+
+private fun sortingDescending(list: List<CityEntity>) =
+    list.sortedBy { avgPriceForASingleCity(it) }.take(10)
+
+
+
+private fun pricesAvg (list: List<CityEntity>) : List<Pair<String,Float>>{
+    val finalList : MutableList<Pair<String,Float>>
+    var avgPrice : Float
+    list.forEach {
+        avgPrice = avgPriceForASingleCity(it)
+        finalList.add(Pair(it.country,avgPrice))
+    }
+    return finalList
+}
+
+private fun avgPriceForASingleCity(cityEntity: CityEntity) = cityEntity.drinksPrices.milkRegularOneLiter!! + cityEntity.drinksPrices.cappuccinoRegularInRestaurants!! +
+        cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket!! + cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants!!+ cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants!!
