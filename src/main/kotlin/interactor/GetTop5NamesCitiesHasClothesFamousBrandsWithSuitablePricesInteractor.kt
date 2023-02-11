9bec0f44eb94574438c728bc8ab803615f061b5d
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-02-11 14:10:06 +0200

    add code in fun execute

--- src/main/kotlin/interactor/GetTop5NamesCitiesHasClothesFamousBrandsWithSuitablePricesInteractor.kt
+++ src/main/kotlin/interactor/GetTop5NamesCitiesHasClothesFamousBrandsWithSuitablePricesInteractor.kt
@@ -1,25 +1,32 @@
 package interactor
 
-import model.CityEntity
-import model.CityNameSumClothesPrices
 import model.NameCityClothesPrices
 
 class GetTop5NamesCitiesHasClothesFamousBrandsWithSuitablePricesInteractor (
     private val dataSource: List<NameCityClothesPrices>,
 ) {
 
+    private val numberOfReturnCityName = 5
     fun execute( ): List<String> {
 
-        if(dataSource.filter(::deletAllCityHasNotClothes).size < 5  ) throw  Exception ("the dataSource has less than 5 city ")
+        if(dataSource.filter(deletAllCityHasNotClothes).size < numberOfReturnCityName  ) throw  Exception ("the dataSource has less than 5 city ")
 
-        return listOf()
+        return dataSource
+            .filter(deletAllCityHasNotClothes)
+            .sortedBy(calculateSumClothesPrices)
+            .take(numberOfReturnCityName)
+            .map { it.cityName }
     }
 
 
-
-    private fun deletAllCityHasNotClothes(city: NameCityClothesPrices): Boolean {
-        return city.onePairOfJeansLevis50oneOrSimilar != null && city.onePairOfMenLeatherBusinessShoes != null  && city.onePairOfNikeRunningShoesMidRange != null && city.oneSummerDressInAChainStoreZaraHAndM != null
+    private val calculateSumClothesPrices = { city: NameCityClothesPrices ->
+        city.onePairOfMenLeatherBusinessShoes!! + city.onePairOfJeansLevis50oneOrSimilar!! + city.onePairOfNikeRunningShoesMidRange!! + city.oneSummerDressInAChainStoreZaraHAndM!!
     }
+
+    private val deletAllCityHasNotClothes = { city: NameCityClothesPrices ->
+        city.onePairOfJeansLevis50oneOrSimilar != null && city.onePairOfMenLeatherBusinessShoes != null  && city.onePairOfNikeRunningShoesMidRange != null && city.oneSummerDressInAChainStoreZaraHAndM != null
+    }
+
 }
 
 
