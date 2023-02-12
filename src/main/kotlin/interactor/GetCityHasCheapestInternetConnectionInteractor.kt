2dcd8876485f2e2d3d1152212913c7285fe4fff4
Ziad Abo Elela <ziaadaboelela@gmail.com>
2023-02-12 22:57:21 +0200

    add test cases

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,10 +6,16 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity{
+    fun execute(): String {
         throw Throwable("Not Implemented yet")
     }
 
+     fun excludeNullSalaries(city: CityEntity): Boolean {
+         throw Throwable("Not Implemented yet")
+    }
 
+    fun excludeNullPrice(city: CityEntity): Boolean {
+        throw Throwable("Not Implemented yet")
+    }
 
 }
--- src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -1,12 +1,88 @@
 package interactor
 
+import dataSource.CsvDataSource
+import fakeData.FackeDataSource
+import model.CityEntity
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+import java.util.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasCheapestInternetConnectionInteractorTest {
 
-    @Test
-    fun execute() {
+    private lateinit var getCity: GetCityHasCheapestInternetConnectionInteractor
+    private val dataSource: FackeDataSource by lazy { FackeDataSource() }
+
+
+    @BeforeEach
+    fun setUp() {
+        getCity = GetCityHasCheapestInternetConnectionInteractor(dataSource)
     }
+
+    @Test
+    fun Should_ReturnFalse_When_inputNullPrice(){
+        //given a list of FackDataSource
+        var city=  dataSource.getAllCitiesWithNullPrice()[0]
+
+        //when
+        val result = getCity.excludeNullPrice(city)
+        //then
+        assertFalse(result)
+    }
+
+    @Test
+    fun Should_ReturnFalse_When_InputNullSalary(){
+        //given a list of FackDataSource
+        var city=  dataSource.getAllCitiesWithNullSalary()[0]
+        //when
+        val result = getCity.excludeNullSalaries(city)
+        //then
+        assertFalse(result)
+    }
+
+
+    @Test
+    fun Should_ReturnTheCountryWithCheapestInternetPrice_When_SameSalaryAndDifferentPrice() {
+        //given a list of FackDataSource
+        dataSource.getAllCitiesWithSameSalaryAndDifferentPrice()
+        //when
+        val result = getCity.execute()
+        //then
+        assertEquals("Egypt", result)
+    }
+
+    @Test
+    fun Should_ReturnTheCountryWithCheapestInternetPrice_When_SamePriceAndDifferentSalary() {
+        //given a list of FackDataSource
+        dataSource.getAllCitiesWithSamePriceAndDifferentSalary()
+        //when
+        val result = getCity.execute()
+        //then
+        assertEquals("London", result)
+    }
+
+    @Test
+    fun Should_ReturnTheFirstCountryWithCheapestInternetPrice_When_SameAverage() {
+        //given a list of FackDataSource
+        dataSource.getAllCitiesWithSameAverage()
+        //when
+        val result = getCity.execute()
+        //then
+        assertEquals("Egypt", result)
+    }
+
+    @Test
+    fun Should_ReturnTheCountryWithCheapestInternetPrice_When_AllTypeOfData() {
+        //given a list of FackDataSource
+        dataSource.getAllCitiesData()
+        //when
+        val result = getCity.execute()
+        //then
+        assertEquals("Jordan", result)
+    }
+
 }
