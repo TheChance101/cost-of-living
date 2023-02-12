afd2a2f6a9a6bdc585fdf4629be8197a78b027da
Ahmed Refat <ahmedref21@gmail.com>
2023-02-12 20:46:17 +0200

    refactor the tests

--- src/test/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractorTest.kt
+++ src/test/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractorTest.kt
@@ -13,6 +13,7 @@ class GetCitiesNamesSortedByCheapestBananaPriceInteractorTest {
     @Test
     fun should_ignore_entries_with_null_city_name_or_banana_price() {
         val cities: List<CityEntity> = citiesWithNullBananaPrice()
+
         val sorted = getCitiesNamesSortedByCheapestBananaPriceInteractor.execute(*cities.toTypedArray())
 
         assertEquals(1, sorted.size)
@@ -24,7 +25,7 @@ class GetCitiesNamesSortedByCheapestBananaPriceInteractorTest {
 
         val sorted = getCitiesNamesSortedByCheapestBananaPriceInteractor.execute(*cities.toTypedArray())
 
-        val expected = listOf<String>("B", "A")
+        val expected = listOf("B", "A")
 
         assertEquals(expected, sorted)
     }
