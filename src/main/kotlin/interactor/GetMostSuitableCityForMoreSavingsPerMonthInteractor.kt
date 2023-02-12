d2bf7ca5e8eb35210af7bd1d9a635b6b772f331d
shehab <sheshox12@gmail.com>
2023-02-12 13:42:17 +0200

    create feature class

---
+++ src/main/kotlin/interactor/GetMostSuitableCityForMoreSavingsPerMonthInteractor.kt
@@ -0,0 +1,11 @@
+package interactor
+
+import model.CityEntity
+
+class GetMostSuitableCityForMoreSavingsPerMonthInteractor {
+
+    fun execute(): CityEntity {
+        throw Throwable("Not Implemented yet")
+    }
+
+}
