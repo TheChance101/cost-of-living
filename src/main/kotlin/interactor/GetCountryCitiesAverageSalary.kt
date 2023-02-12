08573f0210910bc68f4491220106f19a391ac5fa
Saif Alhaider <saifharith90@gmail.com>
2023-02-12 16:07:07 +0300

    added extension function for sentence formatting

--- src/main/kotlin/interactor/GetCountryCitiesAverageSalary.kt
+++ src/main/kotlin/interactor/GetCountryCitiesAverageSalary.kt
@@ -8,14 +8,13 @@ class GetCountryCitiesAverageSalaryInteractor(
 
     fun execute(country: String): List<Pair<String, Float>> {
         val citiesSalaries =  dataSource.getAllCitiesData()
-            .filter { it.country.lowercase() == country.lowercase().trim().replace(Regex(" +"), " ") && excludeNullSalariesAndLowQualityData(it) }
+            .filter { it.country.format_sentence() == country.format_sentence() && excludeNullSalariesAndLowQualityData(it) }
             .map { Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!) }
-
         if (citiesSalaries.isEmpty()) throw Exception("country was not found !")
 
         return citiesSalaries
     }
-
+    fun String.format_sentence() = this.lowercase().trim().replace(Regex(" +"), " ")
     private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
         return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
     }
