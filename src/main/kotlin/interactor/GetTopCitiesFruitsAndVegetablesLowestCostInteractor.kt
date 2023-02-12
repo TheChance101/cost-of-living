5b5f9533433f774ab73030b16a8adb3243755e8d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-12 22:15:45 +0200

    add function excludeNullFruitsAndVegetables

--- src/main/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractor.kt
+++ src/main/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractor.kt
@@ -2,11 +2,15 @@ package interactor
 
 import model.CityEntity
 
-class GetTop10CitiesFruitsAndVegetablesLowestCostInteractor(
+class GetTopCitiesFruitsAndVegetablesLowestCostInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
     fun execute(cityEntity: CityEntity): List<String> {
+        TODO("Not implemented yet")
+    }
+
+    fun excludeNullFruitsAndVegetables(city: CityEntity): Boolean {
         TODO("Not implemented yet")
     }
 
