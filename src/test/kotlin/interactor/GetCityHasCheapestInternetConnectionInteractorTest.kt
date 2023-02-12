109bebd3e80e8e8a97153ec62333be1db27bc613
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-02-12 14:17:46 +0200

    add test case

---
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -0,0 +1,39 @@
+package interactor
+
+import dataSource.CsvDataSource
+import dataSource.FakeCsvDataSource
+import dataSource.utils.CsvParser
+import model.CityEntity
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetCityHasCheapestInternetConnectionInteractorTest {
+
+    lateinit var csvParser :CsvParser
+    lateinit var dataSource: CostOfLivingDataSource
+    lateinit var getCityHasCheapestInternetConnectionInteractor: GetCityHasCheapestInternetConnectionInteractor
+
+    @BeforeAll
+    fun setup(){
+        csvParser= CsvParser()
+        dataSource  = FakeCsvDataSource(csvParser)
+        getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+    }
+
+    @Test
+    fun should_ReternTrue_when_ExecuteCityEntityWithHighDataQuality() {
+        // given
+
+        // when
+        val result = getCityHasCheapestInternetConnectionInteractor.execute()
+        // then
+        assertTrue(result.dataQuality)
+
+    }
+
+
+}
