35561275fab41241962d5a862604493066d4f501
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-12 22:07:53 +0200

    edit two test cases name and fake data

--- src/test/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractorTest.kt
+++ src/test/kotlin/interactor/GetSalaryAverageForCitiesInCountryInteractorTest.kt
@@ -29,8 +29,10 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     fun should_ReturnCitiesNameAndSalariesAverage_When_CountryNameIsUppercase() {
         // Given country name Uppercase
         val countryName = "CUBA"
+
         // When averageSalary not null and high dataQuality
         val actualPairList = interactor.execute(countryName)
+
         // Then return a list of pair of city name and average salary
         assertEquals(listOf(Pair("Santiago de Cuba", 18.0f)), actualPairList)
     }
@@ -39,9 +41,11 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     fun should_ReturnCitiesNameAndSalariesAverage_When_CountryNameIsLowercase() {
         // Given country name Lowercase
         val countryName = "egypt"
+
         // When averageSalary not null and high dataQuality
         val actualPairList = interactor.execute(countryName)
-         // Then return a list of pair of city name and average salary
+
+        // Then return a list of pair of city name and average salary
         assertEquals(listOf(Pair("Tanta", 101.74f)), actualPairList)
     }
 
@@ -49,8 +53,10 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     fun should_ReturnCitiesNameAndSalariesAverage_When_CountryNameIsMixed() {
         // Given country name Mix of Uppercase and Lowercase
         val countryName = "SyRia"
+
         // When averageSalary not null and high dataQuality
         val actualPairList = interactor.execute(countryName)
+
         // Then return a list of pair of city name and average salary
         assertEquals(listOf(Pair("Damascus", 50.24f)), actualPairList)
     }
@@ -59,8 +65,10 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     fun should_ReturnException_When_CountryNameIsEmpty() {
         // Given empty country name
         val countryName = ""
+
         // When country name is empty
         val salary = Executable { interactor.execute(countryName) }
+
         // Then return Throw Exception
         assertThrows(Exception::class.java, salary)
     }
@@ -68,77 +76,10 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     @Test
     fun should_ReturnFalse_When_SalariesNotEqualNullOrNotAvoidLowQualityData() {
         // Given cityEntity
-        val city = CityEntity(
-            cityName = "Santiago de Cuba",
-            country = "Cuba",
-            mealsPrices = MealsPrices(
-                mealInexpensiveRestaurant = 6.5f,
-                mealFor2PeopleMidRangeRestaurant = 15.0f,
-                mealAtMcDonaldSOrEquivalent = 5.0f
-            ),
-            drinksPrices = DrinksPrices(
-                cappuccinoRegularInRestaurants = 2.0f,
-                cokePepsiAThirdOfLiterBottleInRestaurants = 2.0f,
-                waterAThirdOfLiterBottleInRestaurants = 1.0f,
-                milkRegularOneLiter = 3.8f,
-                waterOneAndHalfLiterBottleAtTheMarket = 2.0f
-            ),
-            fruitAndVegetablesPrices = FruitAndVegetablesPrices(
-                apples1kg = 11.02f,
-                banana1kg = 0.44f,
-                oranges1kg = 1.1f,
-                tomato1kg = 0.88f,
-                potato1kg = 1.1f,
-                onion1kg = 2.2f,
-                lettuceOneHead = 0.25f
-            ),
-            foodPrices = FoodPrices(
-                loafOfFreshWhiteBread500g = 1.1f,
-                riceWhite1kg = 1.1f,
-                eggsRegular12 = 2.03f,
-                localCheese1kg = 9.0f,
-                chickenFillets1kg = 6.61f,
-                beefRound1kgOrEquivalentBackLegRedMeat = 6.61f
-            ),
-            servicesPrices = ServicesPrices(
-                basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
-                oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.1f,
-                internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
-                fitnessClubMonthlyFeeForOneAdult = null,
-                tennisCourtRentOneHourOnWeekend = null,
-                cinemaInternationalReleaseOneSeat = 1.0f,
-                preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
-                internationalPrimarySchoolYearlyForOneChild = null
-            ),
-            clothesPrices = ClothesPrices(
-                onePairOfJeansLevis50oneOrSimilar = 40.0f,
-                oneSummerDressInAChainStoreZaraHAndM = 40.0f,
-                onePairOfNikeRunningShoesMidRange = 60.0f,
-                onePairOfMenLeatherBusinessShoes = 50.0f
-            ),
-            transportationsPrices = TransportationsPrices(
-                oneWayTicketLocalTransport = null,
-                monthlyPassRegularPrice = null,
-                taxiStartNormalTariff = null,
-                taxi1kmNormalTariff = 0.7f,
-                taxi1hourWaitingNormalTariff = null,
-                gasolineOneLiter = null
-            ),
-            carsPrices = CarsPrices(
-                volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
-                toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = null
-            ),
-            realEstatesPrices = RealEstatesPrices(
-                apartmentOneBedroomInCityCentre = null,
-                apartmentOneBedroomOutsideOfCentre = null,
-                apartment3BedroomsInCityCentre = 600.0f,
-                pricePerSquareMeterToBuyApartmentInCityCentre = null
-            ),
-            averageMonthlyNetSalaryAfterTax = 18.0f,
-            dataQuality = false
-        )
+        val fakeCity = fakeData.getAllCitiesData()[3]
+
         // When averageSalary not null and low dataQuality
-        val result = interactor.excludeNullSalariesAndLowQualityData(city)
+        val result = interactor.excludeNullSalariesAndLowQualityData(fakeCity)
 
         // Then return false
         assertFalse(result)
@@ -147,78 +88,10 @@ internal class GetSalaryAverageForCitiesInCountryInteractorTest {
     @Test
     fun should_ReturnTrue_When_SalariesNotEqualNullAndAvoidLowQualityData() {
         // Given cityEntity
-        val city = CityEntity(
-            cityName = "Tanta",
-            country = "Egypt",
-            mealsPrices = MealsPrices(
-                mealInexpensiveRestaurant = 4.07f,
-                mealFor2PeopleMidRangeRestaurant = 14.24f,
-                mealAtMcDonaldSOrEquivalent = 2.85f
-            ),
-            drinksPrices = DrinksPrices(
-                cappuccinoRegularInRestaurants = 1.15f,
-                cokePepsiAThirdOfLiterBottleInRestaurants = 0.27f,
-                waterAThirdOfLiterBottleInRestaurants = 0.16f,
-                milkRegularOneLiter = 0.73f,
-                waterOneAndHalfLiterBottleAtTheMarket = 0.23f
-            ),
-            fruitAndVegetablesPrices = FruitAndVegetablesPrices(
-                apples1kg = 0.75f,
-                banana1kg = 0.5f,
-                oranges1kg = 0.26f,
-                tomato1kg = 0.53f,
-                potato1kg = 0.37f,
-                onion1kg = 0.37f,
-                lettuceOneHead = 0.16f
-            ),
-            foodPrices = FoodPrices(
-                loafOfFreshWhiteBread500g = 0.33f,
-                riceWhite1kg = 0.5f,
-                eggsRegular12 = 1.08f,
-                localCheese1kg = 2.4f,
-                chickenFillets1kg = 2.65f,
-                beefRound1kgOrEquivalentBackLegRedMeat = 6.31f
-            ),
-            servicesPrices = ServicesPrices(
-                basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = 20.35f,
-                oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.01f,
-                internet60MbpsOrMoreUnlimitedDataCableAdsl = 9.77f,
-                fitnessClubMonthlyFeeForOneAdult = 10.17f,
-                tennisCourtRentOneHourOnWeekend = 5.6f,
-                cinemaInternationalReleaseOneSeat = 3.66f,
-                preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = 50.87f,
-                internationalPrimarySchoolYearlyForOneChild = 1078.49f
-            ),
-            clothesPrices = ClothesPrices(
-                onePairOfJeansLevis50oneOrSimilar = 14.24f,
-                oneSummerDressInAChainStoreZaraHAndM = 32.05f,
-                onePairOfNikeRunningShoesMidRange = 89.54f,
-                onePairOfMenLeatherBusinessShoes = 28.49f
-            ),
-            transportationsPrices = TransportationsPrices(
-                oneWayTicketLocalTransport = 0.12f,
-                monthlyPassRegularPrice = 1.02f,
-                taxiStartNormalTariff = 0.41f,
-                taxi1kmNormalTariff = 0.2f,
-                taxi1hourWaitingNormalTariff = 0.73f,
-                gasolineOneLiter = 0.38f
-            ),
-            carsPrices = CarsPrices(
-                volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = 20348.95f,
-                toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = 16228.29f
-            ),
-            realEstatesPrices = RealEstatesPrices(
-                apartmentOneBedroomInCityCentre = 156.01f,
-                apartmentOneBedroomOutsideOfCentre = 71.22f,
-                apartment3BedroomsInCityCentre = 196.71f,
-                pricePerSquareMeterToBuyApartmentInCityCentre = 335.76f
-            ),
-            averageMonthlyNetSalaryAfterTax = 101.74f,
-            dataQuality = true
-        )
+        val fakeCity = fakeData.getAllCitiesData()[2]
 
         // When averageSalary not null and high dataQuality
-        val result = interactor.excludeNullSalariesAndLowQualityData(city)
+        val result = interactor.excludeNullSalariesAndLowQualityData(fakeCity)
 
         // Then return true
         assertTrue(result)
