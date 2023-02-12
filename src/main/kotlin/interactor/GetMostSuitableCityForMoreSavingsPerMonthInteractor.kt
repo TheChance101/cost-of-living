ce87733c8d033ead8dfe5c7b20e4c6251d126edf
shehab <sheshox12@gmail.com>
2023-02-12 16:33:56 +0200

    update feature class

--- src/main/kotlin/interactor/GetMostSuitableCityForMoreSavingsPerMonthInteractor.kt
+++ src/main/kotlin/interactor/GetMostSuitableCityForMoreSavingsPerMonthInteractor.kt
@@ -2,10 +2,13 @@ package interactor
 
 import model.CityEntity
 
-class GetMostSuitableCityForMoreSavingsPerMonthInteractor {
+class GetMostSuitableCityForMoreSavingsPerMonthInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
 
     fun execute(): CityEntity {
         throw Throwable("Not Implemented yet")
     }
+
 
 }
