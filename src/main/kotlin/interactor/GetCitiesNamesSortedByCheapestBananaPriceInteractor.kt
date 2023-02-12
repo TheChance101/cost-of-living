4b5ca5aa1189fc00cf57edac07bfe76754f5bce3
Ahmed Refat <ahmedref21@gmail.com>
2023-02-12 20:50:21 +0200

    refactor the code

--- src/main/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractor.kt
@@ -5,17 +5,14 @@ import model.CityEntity
 class GetCitiesNamesSortedByCheapestBananaPriceInteractor {
 
     fun execute(vararg cities: CityEntity): List<String> {
-
         return cities.asList()
             .filter(::isBananaPriceNonNull)
             .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
             .map(CityEntity::cityName)
-
     }
 
     private fun isBananaPriceNonNull(city: CityEntity): Boolean {
         return city.fruitAndVegetablesPrices.banana1kg != null
     }
-
 
 }
