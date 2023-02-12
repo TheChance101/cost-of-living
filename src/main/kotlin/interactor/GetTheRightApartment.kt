f0370c6f1a37c4c07e7e0e994c48d98e4a03b3ca
yassen ramadan <yassenyasooo50@gmail.com>
2023-02-13 00:09:54 +0200

    start  implementation of function

---
+++ src/main/kotlin/interactor/GetTheRightApartment.kt
@@ -0,0 +1,24 @@
+package interactor
+import model.CityEntity
+import model.DetailsOfApartment
+
+open class GetTheRightApartment(private val dataSource: CostOfLivingDataSource) {
+
+
+
+    private fun getTopTenCitesHasCanPayApartmentFaster(dataSource:List<CityEntity>) :List<CityEntity>{
+        return dataSource.sortedBy {
+            it.averageMonthlyNetSalaryAfterTax?.div(it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!.toInt())
+        }.take(10)
+
+    }
+    fun getListOfDetailsOfApartment(salary:Int):List<DetailsOfApartment>{
+        val dataSource=getTopTenCitesHasCanPayApartmentFaster(dataSource.getAllCitiesData())
+
+
+    }
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+    }
+
+}
