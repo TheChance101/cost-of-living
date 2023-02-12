f385547d8c47fefa31de10a67cdbc3941995bbbf
aliabuhussien <aliabuhussien371@gmail.com>
2023-02-12 18:51:17 +0200

    add code of task number four

--- src/main/kotlin/interactor/GetHighestDifferentPayForRent.kt
+++ src/main/kotlin/interactor/GetHighestDifferentPayForRent.kt
@@ -7,6 +7,22 @@ class  GetHighestDifferentPayForRent (
 
     ) {
     fun execute(): CityEntity{
-        throw Throwable("Not Implemented yet")
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeHighQulaityDataAndNullable)
+            .sortedByDescending (::sortedHighestDifferentPayForRent)
+            .first()
     }
+}
+private fun excludeHighQulaityDataAndNullable(city: CityEntity): Boolean {
+    val  apartmentPrices = city.realEstatesPrices
+    return city.dataQuality && apartmentPrices.apartmentOneBedroomOutsideOfCentre !=null &&apartmentPrices.apartment3BedroomsOutsideOfCentre !=null
+            && apartmentPrices.apartmentOneBedroomInCityCentre !=null &&apartmentPrices.apartment3BedroomsInCityCentre !=null
+}
+private  fun sortedHighestDifferentPayForRent(city: CityEntity):Float{
+    val  apartmentPrices=city.realEstatesPrices
+    val differentBetweenOneBadRoomPrices= apartmentPrices.apartmentOneBedroomInCityCentre !!- apartmentPrices.apartmentOneBedroomOutsideOfCentre !!
+    val differentBetweenThreeBadRoomPrices =apartmentPrices.apartment3BedroomsInCityCentre!!- apartmentPrices.apartment3BedroomsOutsideOfCentre !!
+    return  if (differentBetweenOneBadRoomPrices >=differentBetweenThreeBadRoomPrices) return differentBetweenOneBadRoomPrices
+    else  differentBetweenThreeBadRoomPrices
 }
