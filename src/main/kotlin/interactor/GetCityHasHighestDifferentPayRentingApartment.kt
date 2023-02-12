13639ddf75651b1062dc868c28036f3a5def1254
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-02-12 04:14:31 +0200

    create class get city has highest different pay rating apartment

--- src/main/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartment.kt
+++ src/main/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartment.kt
@@ -1,46 +1,4 @@
 package interactor
 
-import model.CityEntity
-
-class GetCityHasHighestDifferentPayRentingApartment (
-    private val dataSource: CostOfLivingDataSource,
-) {
-    fun execute_OneBedroomInCityCentre(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedByDescending { it.realEstatesPrices.apartmentOneBedroomInCityCentre }
-            .take(limit)
-            .map { it.cityName }
-    }
-    fun execute_OneBedroomOutsideOfCentree(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedByDescending { it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre }
-            .take(limit)
-            .map { it.cityName }
-    }
-    fun execute_O3BedroomsInCityCentre(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedByDescending { it.realEstatesPrices.apartment3BedroomsInCityCentre }
-            .take(limit)
-            .map { it.cityName }
-    }
-
-    fun execute_3BedroomsOutsideOfCentre(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedByDescending { it.realEstatesPrices.apartment3BedroomsOutsideOfCentre }
-            .take(limit)
-            .map { it.cityName }
-    }
-
-    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
-        return city.realEstatesPrices != null && city.dataQuality
-    }
+class GetCityHasHighestDifferentPayRentingApartment {
 }
-
