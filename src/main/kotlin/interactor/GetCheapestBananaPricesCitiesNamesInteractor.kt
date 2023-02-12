4fb827770bca2721c63b522520abd67ceb9686f1
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-02-12 19:19:29 +0200

    pass test case 2 ,3

--- src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
@@ -12,7 +12,23 @@ class  GetCheapestBananaPricesCitiesNamesInteractor(
     fun execute(vararg cityEntities: CityEntity): List<String> {
         return if (cityEntities.isEmpty())
             listOf("No Data is Entered !")
-       else  throw Throwable("Not Implemented yet")
+        else cityEntities
+            .filter(::excludeNullBananaPrices)
+            .sortingWithBananaPrices()
+            .map { it.cityName }
     }
 
+    private fun excludeNullBananaPrices(city: CityEntity): Boolean {
+        return city.fruitAndVegetablesPrices.banana1kg != null
+    }
+
+    /**
+     * Sort the list with the banana prices between CityEntities.
+     * @return sorted List<CityEntity>
+     */
+    private fun List<CityEntity>.sortingWithBananaPrices(): List<CityEntity> {
+        return this.sortedByDescending {
+            it.fruitAndVegetablesPrices.banana1kg
+        }
+    }
 }
