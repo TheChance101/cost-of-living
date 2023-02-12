49a21a98d65aabe84690ed6418315e3279c6f9ca
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-02-12 02:15:43 +0200

    create class data containt info city

--- src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
+++ src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
@@ -8,16 +8,23 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetCityHasHighestDifferentPayRentingApartmentTest {
-    val csvParser = CsvParser()
-    val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
-    lateinit var ratingHighest:GetCityHasHighestDifferentPayRentingApartment
-    @BeforeAll
-    fun setUp(){
-        ratingHighest = GetCityHasHighestDifferentPayRentingApartment(dataSource)
-    }
-
+//    val csvParser = CsvParser()
+//    val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
+//    lateinit var ratingHighest:GetCityHasHighestDifferentPayRentingApartment
+//    @BeforeAll
+//    fun setUp(){
+//        ratingHighest = GetCityHasHighestDifferentPayRentingApartment(dataSource)
+//    }
+    data class cityData(val name:String,val salaryInCenter:Double,val salaryOutCenter:Double,val value:Boolean)
+    // create method data city
+private val cities = listOf(
+    cityData("City1", 200.0, 100.0, true),
+        cityData("City2", 150.0, 75.0, true),
+        cityData("City3", 250.0, 125.0, false),
+        cityData("City4", 300.0, 150.0, true)
+)
     @Test
     fun should_Return_Correct_CityName_OneBedroomInCityCentre() {
         //given
