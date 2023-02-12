8d9f9715a8e89b216c9b804e821a6f5accf0fc34
Ahmed Refat <ahmedref21@gmail.com>
2023-02-12 20:20:45 +0200

    add failing test to deal with null bananaPrice

---
+++ src/main/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractor.kt
@@ -0,0 +1,13 @@
+package interactor
+
+import model.CityEntity
+
+class GetCitiesNamesSortedByCheapestBananaPriceInteractor {
+
+    fun execute(vararg cities: CityEntity): List<String> {
+        val f: Float? = null
+        return listOf()
+    }
+
+
+}
---
+++ src/test/kotlin/interactor/GetCitiesNamesSortedByCheapestBananaPriceInteractorTest.kt
@@ -0,0 +1,47 @@
+package interactor
+
+import model.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class GetCitiesNamesSortedByCheapestBananaPriceInteractorTest {
+
+    private val getCitiesNamesSortedByCheapestBananaPriceInteractor =
+        GetCitiesNamesSortedByCheapestBananaPriceInteractor()
+
+
+    @Test
+    fun should_ignore_entries_with_null_city_name_or_banana_price() {
+        val cities: List<CityEntity> = citiesWithNullBananaPrice()
+        val sorted = getCitiesNamesSortedByCheapestBananaPriceInteractor.execute(*cities.toTypedArray())
+
+        assertEquals(1, sorted.size)
+    }
+    private fun citiesWithNullBananaPrice(): List<CityEntity> {
+        return listOf(
+            cityEntity("A", null),
+            cityEntity("B", 10.25f)
+        )
+    }
+
+    private fun cityEntity(cityName: String, bananaPrice: Float?): CityEntity {
+        return CityEntity(
+            cityName,
+            "",
+            MealsPrices(null, null, null),
+            DrinksPrices(null, null, null, null, null),
+            FruitAndVegetablesPrices(null, bananaPrice, null, null, null, null, null),
+            FoodPrices(null, null, null, null, null, null),
+            ServicesPrices(null, null, null, null, null, null, null, null),
+            ClothesPrices(null, null, null, null),
+            TransportationsPrices(null, null, null, null, null, null),
+            CarsPrices(null, null),
+            RealEstatesPrices(null, null, null, null, null, null),
+            null,
+            true
+
+        )
+    }
+
+
+}
