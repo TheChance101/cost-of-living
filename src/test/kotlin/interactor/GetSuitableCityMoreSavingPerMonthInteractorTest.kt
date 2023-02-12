233bedc012e19b0e20652234f39396778cfad847
Aziza Helmy <aziza.helmy35@gmail.com>
2023-02-12 17:15:37 +0200

    add initial commit for first test case

--- src/test/kotlin/interactor/GetSuitableCityMoreSavingPerMonthInteractorTest.kt
+++ src/test/kotlin/interactor/GetSuitableCityMoreSavingPerMonthInteractorTest.kt
@@ -1,15 +1,27 @@
 package interactor
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 /**
  * Created by Aziza Helmy on 2/12/2023.
  */
 internal class GetSuitableCityMoreSavingPerMonthInteractorTest {
+    private lateinit var getSuitableCityMoreSavingPerMonth: GetSuitableCityMoreSavingPerMonthInteractor
+    private lateinit var fakedata: FakeData
+
+    @BeforeAll
+    fun setUp() {
+        fakedata = FakeData()
+        getSuitableCityMoreSavingPerMonth = GetSuitableCityMoreSavingPerMonthInteractor(fakedata)
+    }
 
     @Test
-    fun execute() {
+    fun should_ReturnTrue_When_CitiesHasAnAverageMonthlySalary(){
+        //Given
+        //When
+        //Then
+
     }
+
 }
