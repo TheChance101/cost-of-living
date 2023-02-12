19468b98922f12f58f15a7d147b47a95a0c37a7f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-12 20:19:01 +0200

    create Top10CitiesFruitsAndVegetablesLowestCostInteractor with a function execute not implemented yet

---
+++ src/main/kotlin/interactor/Top10CitiesFruitsAndVegetablesLowestCostInteractor.kt
@@ -0,0 +1,11 @@
+package interactor
+
+class Top10CitiesFruitsAndVegetablesLowestCostInteractor(
+    private val dataSource: CostOfLivingDataSource,
+) {
+
+    fun execute(limit: Int): List<String> {
+        TODO("Not implemented yet")
+    }
+
+}
