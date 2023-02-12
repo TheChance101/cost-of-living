9af48b3abbf73bc6f471a59fe41438b21145f154
shehab <sheshox12@gmail.com>
2023-02-12 16:34:13 +0200

    update test class

--- src/test/kotlin/interactor/GetMostSuitableCityForMoreSavingsPerMonthInteractorTest.kt
+++ src/test/kotlin/interactor/GetMostSuitableCityForMoreSavingsPerMonthInteractorTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetMostSuitableCityForMoreSavingsPerMonthInteractorTest {
@@ -15,16 +16,19 @@ internal class GetMostSuitableCityForMoreSavingsPerMonthInteractorTest {
     @BeforeAll
     fun setUp() {
         dataSource = HardCodedFakeDataSource()
-        getMostSuitableCity = GetMostSuitableCityForMoreSavingsPerMonthInteractor()
+        getMostSuitableCity = GetMostSuitableCityForMoreSavingsPerMonthInteractor(dataSource)
     }
 
     @Test
     fun should_ReturnCityEntity_When_EnterValidCityList() {
         // given
-
+        val expectedCity = dataSource.parisHighQuality
         // when
-
+        val cityEntity = getMostSuitableCity.run {
+            execute()
+        }
         // then
+        assertEquals(expectedCity,cityEntity)
     }
 
 
