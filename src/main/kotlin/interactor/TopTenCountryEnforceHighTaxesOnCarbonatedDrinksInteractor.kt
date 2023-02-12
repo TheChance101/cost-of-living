adf64e9a76b446e9d69c38dcb0f110665ff966f9
Zeinab Abdien <suzanabdien15@gmail.com>
2023-02-12 18:08:14 +0200

    add top ten class and test class

---
+++ src/main/kotlin/interactor/TopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor.kt
@@ -0,0 +1,6 @@
+package interactor
+
+class TopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor {
+
+
+}
---
+++ src/test/kotlin/interactor/TopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractorTest.kt
@@ -0,0 +1,8 @@
+package interactor
+
+import org.junit.jupiter.api.Assertions.*
+
+class TopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractorTest{
+
+
+}
