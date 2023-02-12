805ff2afa30533ffd80ec45c5e5e97da56313eaa
Aziza Helmy <aziza.helmy35@gmail.com>
2023-02-12 16:28:05 +0200

    add GetSuitableCityMoreSavingPerMonthInteractor class

---
+++ src/main/kotlin/interactor/GetSuitableCityMoreSavingPerMonthInteractor.kt
@@ -0,0 +1,13 @@
+package interactor
+
+/**
+ * Created by Aziza Helmy on 2/11/2023.
+ */
+class GetSuitableCityMoreSavingPerMonthInteractor(private val dataSource: CostOfLivingDataSource) {
+
+
+    fun execute(): String {
+        return ""
+    }
+
+}
