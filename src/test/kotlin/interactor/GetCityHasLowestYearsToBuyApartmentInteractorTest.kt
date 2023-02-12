0ee3a455fcc5fded3cb58ecc4b01a5b0f31615a0
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-02-12 02:00:21 +0200

    add set up for test and first true test case

--- src/test/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractorTest.kt
@@ -1,24 +1,39 @@
 package interactor
 
+import FakeData.FakeData
+import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
 import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
 import io.mockk.unmockkAll
+import model.CityEntity
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasLowestYearsToBuyApartmentInteractorTest {
-    private lateinit var sut :GetCityHasLowestYearsToBuyApartmentInteractor
-    private val csvParser =CsvParser()
 
-    @BeforeAll
-    fun setUp(){
-        clearAllMocks()
-        unmockkAll()
-       // sut = GetCityHasLowestYearsToBuyApartmentInteractor( )
-    }
+    private lateinit var  fakeData : FakeData
+    private lateinit var converter :GetCityHasLowestYearsToBuyApartmentInteractor
+        @BeforeAll
+        fun setUp (){
+            fakeData = FakeData()
+            converter = GetCityHasLowestYearsToBuyApartmentInteractor(fakeData)
+
+        }
     @Test
-    fun execute() {
+    fun should_ReturnCorrectListOfCitiesAndNumberOfYearsToBuyApartment_when_EnterFullTimeSalary() {
+        //given
+        val fullTimeSalary = 100
+        //when
+        val resultList = converter.execute(fullTimeSalary)
+        //then
+         assertEquals(listOf(Pair("",0.0f)),resultList)
     }
+
+
 }
