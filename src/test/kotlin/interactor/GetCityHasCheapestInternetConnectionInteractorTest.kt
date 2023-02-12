78921f13038a55be64dd426863264e35fc61dad1
Mohamed Naser <mahmmedn19@gmail.com>
2023-02-12 22:10:20 +0200

    add test return null if no cities have internet price

--- src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import io.mockk.mockk
 import model.*
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class GetCityHasCheapestInternetConnectionInteractorTest {
 
@@ -20,14 +21,29 @@ class GetCityHasCheapestInternetConnectionInteractorTest {
             createCity("City 3", 5000f, 200f)
         )
         //when(dataSource.getAllCitiesData()).thenRetu(citiesData)
-        // every { dataSource.getAllCitiesData() } returns (citiesData)
+        every { dataSource.getAllCitiesData() } returns (citiesData)
         // when
         val result = interactor.execute()
 
         // then
         assertEquals("City 1", result?.cityName)
     }
+    @Test
+    fun `should return null if no cities have internet price`() {
+        // given
+        val citiesData = listOf(
+            createCity("City 1", 1000f, null),
+            createCity("City 2", 2000f, null),
+            createCity("City 3", 5000f, null)
+        )
+        every { dataSource.getAllCitiesData() } returns (citiesData)
 
+        // when
+        val result = interactor.execute()
+
+        // then
+        assertNull(result)
+    }
 
     private fun createCity(cityName: String, salary: Float, internetPrice: Float?) =
         CityEntity(
@@ -36,7 +52,8 @@ class GetCityHasCheapestInternetConnectionInteractorTest {
             FruitAndVegetablesPrices(null, null, null, null, null, null, null),
             FoodPrices(null, null, null, null, null, null),
             ServicesPrices(null, null, internetPrice, null, null, null, null, null),
-            ClothesPrices(null, null, null, null), TransportationsPrices(null, null, null, null, null, null),
+            ClothesPrices(null, null, null, null),
+            TransportationsPrices(null, null, null, null, null, null),
             CarsPrices(null, null),
             RealEstatesPrices(null, null, null, null, null, null),
             salary, true
