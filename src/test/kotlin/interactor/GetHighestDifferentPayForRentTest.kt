1a07d17e4f0859565f1696379a4fb1aff1a3892d
aliabuhussien <aliabuhussien371@gmail.com>
2023-02-12 18:39:20 +0200

    add first test case

---
+++ src/test/kotlin/interactor/GetHighestDifferentPayForRentTest.kt
@@ -0,0 +1,31 @@
+package interactor
+
+import dataSource.CsvDataSource
+import dataSource.FakeCsvDataSource
+import dataSource.utils.CsvParser
+import model.CityEntity
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetHighestDifferentPayForRentTest {
+    lateinit var csvParser : CsvParser
+    lateinit var dataSource: CostOfLivingDataSource
+    lateinit var getHighestDifferentPayForRent:GetHighestDifferentPayForRent
+    @BeforeAll
+    fun setup(){
+        csvParser= CsvParser()
+        dataSource  = FakeCsvDataSource(csvParser)
+        getHighestDifferentPayForRent = GetHighestDifferentPayForRent(dataSource)
+    }
+   @Test
+   fun should_ReturnapartmentOneBedroomOutsideOfCentreNotEqualNull(){
+       //given
+       //when
+       val result=getHighestDifferentPayForRent.execute()
+       //then not return null
+       assertNotEquals(null , result.realEstatesPrices.apartmentOneBedroomOutsideOfCentre)
+   }
+}
