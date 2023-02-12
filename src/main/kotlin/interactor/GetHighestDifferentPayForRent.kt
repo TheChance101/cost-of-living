49f1f10a1b6ad901caa748b0b27776171b13310c
aliabuhussien <aliabuhussien371@gmail.com>
2023-02-12 18:26:35 +0200

    add GetHighestDifferentPayForRent kotlin file

---
+++ src/main/kotlin/interactor/GetHighestDifferentPayForRent.kt
@@ -0,0 +1,13 @@
+package interactor
+
+import model.CityEntity
+
+class  GetHighestDifferentPayForRent (
+    private val dataSource: CostOfLivingDataSource,
+
+    ) {
+    fun execute(): CityEntity{
+        throw Throwable("Not Implemented yet")
+    }
+
+}
