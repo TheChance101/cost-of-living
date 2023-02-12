dbd5ae7fb15d289bd0261636b63bdbeb9bf53475
Narges-Nagy <nargesnagy21@gmail.com>
2023-02-13 01:41:44 +0200

    add implementaion to isCityHasAverageMealPrice function

--- src/main/kotlin/interactor/GetCityHasCheapestMealPricesInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestMealPricesInteractor.kt
@@ -17,6 +17,12 @@ class GetCityHasCheapestMealPricesInteractor(
         throw Throwable("Not Implemented yet")
     }
 
+    fun isCityHasAverageMealPrice(city: CityEntity): Boolean {
+        return !((city.mealsPrices.mealFor2PeopleMidRangeRestaurant == null || city.mealsPrices.mealFor2PeopleMidRangeRestaurant == 0.0F)
+                && ((city.mealsPrices.mealInexpensiveRestaurant == null || city.mealsPrices.mealInexpensiveRestaurant == 0.0F)
+                || (city.mealsPrices.mealAtMcDonaldSOrEquivalent == null || city.mealsPrices.mealAtMcDonaldSOrEquivalent == 0.0F)))
+    }
+
     fun citiesInUSACanadaAndMexico(city: CityEntity): Boolean {
         return city.country == ThreeSpecificCountries.USA.nameOFCountry
                 || city.country == ThreeSpecificCountries.CANADA.nameOFCountry
