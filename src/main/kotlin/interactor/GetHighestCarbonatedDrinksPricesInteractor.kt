aa630885970ba7d857d936f0bfc0a1783e13fa56
kareem eldeen esam <kareemesam52@yahoo.com>
2023-02-12 16:39:46 +0200

    enhancement

--- src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
+++ src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
@@ -1,5 +1,4 @@
 package interactor
-
 import model.CityEntity
 import model.DrinksPrices
 
@@ -8,22 +7,27 @@ class GetHighestCarbonatedDrinksPricesInteractor(private val dataSource: CostOfL
 {
     fun execute () : List<Pair<String,Float>> {
 
-      val city : List<CityEntity> = dataSource.getAllCitiesData().filter { nullDrinks(it) }
-          val listOfCities  = sortingDescending(city)
-        return pricesAvg(listOfCities)
+      val city : List<CityEntity> = dataSource.getAllCitiesData()
+          .filter { nullAndLowQualityDrinks(it) }
+          .sortedByDescending { avgPriceForASingleCity(it) }
+          .take(10)
+
+
+        return if
+            (city.isEmpty())
+        { emptyList()
+        }// listOf(Pair("no countries Found",0.0f))
+      else {pricesAvg(city)}
     }
 
-private fun nullDrinks (cityEntity: CityEntity):Boolean =
+private fun nullAndLowQualityDrinks (cityEntity: CityEntity):Boolean =
+
             cityEntity.drinksPrices.cappuccinoRegularInRestaurants != null &&
             cityEntity.drinksPrices.milkRegularOneLiter !=null &&
             cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants !=null &&
             cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants !=null &&
-            cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket !=null
-
-
-private fun sortingDescending(list: List<CityEntity>) =
-    list.sortedBy { avgPriceForASingleCity(it) }.take(10)
-
+            cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket !=null &&
+            cityEntity.dataQuality
 
 
 private fun pricesAvg (list: List<CityEntity>) : List<Pair<String,Float>>{
