a8c427e576f1d5b0139f87c1db7e2228e8c726e2
Momen Mustafa Al_Hafidh <momen.myasar@gmail.com>
2023-02-12 08:52:42 +0300

    edit the private function excludeNullFruitVegPrices

--- src/main/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaid.kt
+++ src/main/kotlin/interactor/GetCitiesHasLowestFruitVegPricesComparingSalariesPaid.kt
@@ -18,20 +18,14 @@ class GetCitiesHasLowestFruitVegPricesComparingSalariesPaid(private val dataSour
         return city.averageMonthlyNetSalaryAfterTax != null
     }
     private fun excludeNullFruitVegPrices(fruitAndVegetablesPrices: FruitAndVegetablesPrices): Float {
-        return fruitAndVegetablesPrices.apples1kg?.let { apples ->
-                    fruitAndVegetablesPrices.banana1kg?.let { bananas ->
-                        fruitAndVegetablesPrices.lettuceOneHead?.let { lettuce ->
-                            fruitAndVegetablesPrices.onion1kg?.let { onions ->
-                                fruitAndVegetablesPrices.oranges1kg?.let { oranges ->
-                                    fruitAndVegetablesPrices.potato1kg?.let { potatoes ->
-                                        fruitAndVegetablesPrices.tomato1kg?.let { tomatoes ->
-                                            apples + bananas + lettuce + onions + oranges + potatoes + tomatoes
-                                        } ?: 0f
-                                    } ?: 0f
-                                } ?: 0f
-                            } ?: 0f
-                        } ?: 0f
-                    } ?: 0f
-            } ?: 0f
+        var total = 0f
+        fruitAndVegetablesPrices.apples1kg?.let { total += it }
+        fruitAndVegetablesPrices.banana1kg?.let { total += it }
+        fruitAndVegetablesPrices.lettuceOneHead?.let { total += it }
+        fruitAndVegetablesPrices.onion1kg?.let { total += it }
+        fruitAndVegetablesPrices.oranges1kg?.let { total += it }
+        fruitAndVegetablesPrices.potato1kg?.let { total += it }
+        fruitAndVegetablesPrices.tomato1kg?.let { total += it }
+        return total
     }
 }
