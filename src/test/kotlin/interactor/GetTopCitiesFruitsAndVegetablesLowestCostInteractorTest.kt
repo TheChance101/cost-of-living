3763b387ed78b47770663c73f5e4da72a479f2c1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-12 22:12:07 +0200

    add 2 test cases for excludeNullFruitsAndVegetables

---
+++ src/test/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest.kt
@@ -0,0 +1,40 @@
+package interactor
+
+import fake.FakeData
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest {
+
+    private lateinit var interactor: GetTopCitiesFruitsAndVegetablesLowestCostInteractor
+    private val fakeData = FakeData()
+
+    @BeforeAll
+    fun setup() {
+        interactor = GetTopCitiesFruitsAndVegetablesLowestCostInteractor(fakeData)
+    }
+
+    @Test
+    fun `should return true when cities fruits and vegetables is not null`(){
+        // given
+        val fakeCity = fakeData.getAllCitiesData()[0]
+        // when
+        val result = interactor.excludeNullFruitsAndVegetables(fakeCity)
+        // then
+        assertTrue(result)
+    }
+    @Test
+    fun `should return false when all cities and vegetables is null`(){
+        // given
+        val fakeCity = fakeData.getAllCitiesData()[1]
+        // when
+        val result = interactor.excludeNullFruitsAndVegetables(fakeCity)
+        // then
+        assertFalse(result)
+    }
+
+}
