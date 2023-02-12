a99d602a2e8974889eb5bc1d5d253715d6a62bbd
hawraa <hawraa@email.com>
2023-02-13 00:22:30 +0300

    implement GetHighestDifferentInApartmentRent functionality

--- src/main/kotlin/interactor/GetHighestDifferentInApartmentRent.kt
+++ src/main/kotlin/interactor/GetHighestDifferentInApartmentRent.kt
@@ -6,6 +6,29 @@ class GetHighestDifferentInApartmentRent (  private val dataSource: CostOfLiving
 ) {
 
     fun execute(cities: List<CityEntity>): String? {
-        return cities.toString()
+        return if (cities.isNotEmpty()) cities
+            .filter(::excludeNullSalariesAndLowQualityData)
+            .sortedByDescending { it.findHighestDifferentInCitiesRent() }
+            .take(1)
+            .map { it.cityName }[0]
+        else null
+
+    }
+
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+                && city.dataQuality
+    }
+
+    private fun CityEntity.findHighestDifferentInCitiesRent(): Float {
+        return (realEstatesPrices.apartmentOneBedroomInCityCentre!!
+            .minus(realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!))
+            .plus(
+                (realEstatesPrices.apartment3BedroomsInCityCentre!! - realEstatesPrices.apartment3BedroomsOutsideOfCentre!!)
+            )
+
     }
 }
