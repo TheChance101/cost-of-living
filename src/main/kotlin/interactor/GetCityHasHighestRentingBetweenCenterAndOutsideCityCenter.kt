b3174a73d1c3fbd8c6c29a164aef734ccc476c86
hamsa <hamsaalsaadie@gmail.com>
2023-02-12 19:46:30 +0300

    change execution function

--- src/main/kotlin/interactor/GetCityHasHighestRentingBetweenCenterAndOutsideCityCenter.kt
+++ src/main/kotlin/interactor/GetCityHasHighestRentingBetweenCenterAndOutsideCityCenter.kt
@@ -6,7 +6,7 @@ class GetHighestApartmentPriceDifferenceCityInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
 
-    fun execute(cities: List<CityEntity>): CityEntity? {
+    fun execute(): CityEntity? {
         return dataSource
             .getAllCitiesData()
             .filter(::excludeNullPricesAndLowQualityData)
@@ -20,16 +20,14 @@ class GetHighestApartmentPriceDifferenceCityInteractor(
                     apartmentOneBedroomOutsideOfCentre != null &&
                     apartment3BedroomsInCityCentre != null &&
                     apartment3BedroomsOutsideOfCentre != null &&
-                    pricePerSquareMeterToBuyApartmentInCityCentre != null &&
-                    pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&
                     city.dataQuality
         }
     }
     private fun RealEstatesPrices.getApartmentPriceDifference(): Float {
         return( ((apartmentOneBedroomOutsideOfCentre?.let { apartmentOneBedroomInCityCentre?.minus(it) } )!!.toFloat() +
-                (apartment3BedroomsInCityCentre?.let { apartment3BedroomsOutsideOfCentre?.minus(it) } )!!.toFloat()+
-                (pricePerSquareMeterToBuyApartmentInCityCentre?.let { pricePerSquareMeterToBuyApartmentOutsideOfCentre?.minus(it) } )!!.toFloat())/3)
+                (apartment3BedroomsInCityCentre?.let { apartment3BedroomsOutsideOfCentre?.minus(it) } )!!.toFloat())/2)
     }
+
 
 }
 
