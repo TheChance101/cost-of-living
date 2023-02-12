2897c2f1b9e88de2367d548e21f6e775e744b337
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-12 12:01:23 +0200

    make some changes at execute function in GetCheapestBananaCitiesNamesInteractor

--- src/main/kotlin/interactor/GetCheapestBananaCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestBananaCitiesNamesInteractor.kt
@@ -4,17 +4,12 @@ import model.CityEntity
 
 class GetCheapestBananaCitiesNamesInteractor(private val dataSource: CostOfLivingDataSource) {
 
-    fun execute(vararg cities: String): List<String> {
+    fun execute(vararg cities: CityEntity): List<String> {
         return dataSource
             .getAllCitiesData()
-            .filter(::excludeNullBananaPricesAndLowQualityData)
-            .filter { it.cityName in cities }
+            .filter { it.fruitAndVegetablesPrices.banana1kg != null && it in cities }
             .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
             .map { it.cityName }
-    }
-
-    private fun excludeNullBananaPricesAndLowQualityData(city: CityEntity): Boolean {
-        return city.fruitAndVegetablesPrices.banana1kg != null && city.dataQuality
     }
 
 }
