e67e6fcb648601b628ded7156559c25758e33110
Abdurrahman-Salah-ad-Din <abdosalahas.01128@gmail.com>
2023-02-12 19:23:12 +0200

    add excludeNullSalaries method. add excludeLowQualityData method. add excludeNullApartmentPrice method.

--- src/main/kotlin/interactor/GetFastest10CitiesToBuyAPTInteractor.kt
+++ src/main/kotlin/interactor/GetFastest10CitiesToBuyAPTInteractor.kt
@@ -1,9 +1,37 @@
 package interactor
 
+import model.CityEntity
+
 class GetFastest10CitiesToBuyAPTInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(): List<Pair<String, Double>> {
         throw Throwable("Not Implemented yet")
     }
+
+    /**
+     * Returns `true` if the value of `averageMonthlyNetSalaryAfterTax` is not `null`
+     * and `false` if the `averageMonthlyNetSalaryAfterTax` is `null`.
+     * @param cityEntity represents a city.
+     * @return  if the salary is not equal to `null`.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeNullSalaries(cityEntity: CityEntity) = false
+
+    /**
+     * Returns `true` for high quality data and `false` for low quality data.
+     * @param cityEntity represents a city.
+     * @return the value of `DataQuality`.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeLowQualityData(cityEntity: CityEntity) = false
+
+    /**
+     * Returns `true` if the value of `pricePerSquareMeterToBuyApartmentOutsideOfCentre` is not `null` and
+     * `false` if the `pricePerSquareMeterToBuyApartmentOutsideOfCentre` is `null`.
+     * @param cityEntity represents a city.
+     * @return  if the `pricePerSquareMeterToBuyApartmentOutsideOfCentre` is not equal to `null`.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeNullApartmentPrice(cityEntity: CityEntity) = false
 }
