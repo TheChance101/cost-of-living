c449bbb8049c9c060f352d0fae3b1fc8546279cc
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-02-12 22:22:11 +0200

    enhance all test cases of citiesInUSACanadaAndMexico function

--- src/test/kotlin/interactor/GetCityHasCheapestMealPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestMealPricesInteractorTest.kt
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasCheapestMealPricesInteractorTest {
@@ -21,44 +23,54 @@ class GetCityHasCheapestMealPricesInteractorTest {
 
     @BeforeAll
     fun setUp() {
+        // clear and cancel all objects mock
         clearAllMocks()
         unmockkAll()
+        // sut: system under test
         sut = GetCityHasCheapestMealPricesInteractor(dataSource)
     }
 
     @Test
     fun `should return true when the city is in USA`() {
+        // given fake city entity data
         val city: CityEntity = mockk()
+        // when return the country of the city is USA
         every { city.country } returns ThreeSpecificCountries.USA.nameOFCountry
         val result = sut.citiesInUSACanadaAndMexico(city)
-
-        assertEquals(true, result)
+        // then
+        assertTrue(result)
     }
 
     @Test
     fun `should return true when the city is in Canada`() {
+        // given fake city entity data
         val city: CityEntity = mockk()
+        // when return the country of the city is Canada
         every { city.country } returns ThreeSpecificCountries.CANADA.nameOFCountry
         val result = sut.citiesInUSACanadaAndMexico(city)
-
-        assertEquals(true, result)
+        // then
+        assertTrue(result)
     }
 
     @Test
     fun `should return true when the city is in Mexico`() {
+        // given fake city entity data
         val city: CityEntity = mockk()
+        // when return the country of the city is Mexico
         every { city.country } returns ThreeSpecificCountries.MEXICO.nameOFCountry
         val result = sut.citiesInUSACanadaAndMexico(city)
-
-        assertEquals(true, result)
+        // then
+        assertTrue(result)
     }
 
     @Test
     fun `should return false when the city is in Egypt`() {
+        // given fake city entity data
         val city: CityEntity = mockk()
+        // when return the country of the city is Egypt
         every { city.country } returns ThreeSpecificCountries.EGYPT.nameOFCountry
         val result = sut.citiesInUSACanadaAndMexico(city)
-
-        assertEquals(false, result)
+        // then
+        assertFalse(result)
     }
 }
