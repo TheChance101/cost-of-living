3836fe029d584cf552d0fcbe19410761de9626ad
Garfend <71102453+Garfend@users.noreply.github.com>
2023-02-13 22:12:40 +0200

    Added Interactor function

--- src/main/kotlin/interactor/GetAvgFruitNVeggieInteractor.kt
+++ src/main/kotlin/interactor/GetAvgFruitNVeggieInteractor.kt
@@ -6,7 +6,44 @@ class GetAvgFruitNVeggieInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
     fun execute(): List<CityEntity>? {
-        return null
+        val list = dataSource.getAllCitiesData()
+        if (list.isEmpty()) return null
+        return dataSource.getAllCitiesData()
+
+            .filter(::excludeNullSalaries)
+            .filter(::excludeNullFruitsVegPrices)
+            .sortedBy {
+                FruitsAndVegetablesPrice(it.fruitAndVegetablesPrices, it.averageMonthlyNetSalaryAfterTax!!)
+            }
+            .take(10)
     }
+
+
+
+    private fun excludeNullSalaries(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null
+    }
+
+    private fun excludeNullFruitsVegPrices(city: CityEntity): Boolean {
+        with(city.fruitAndVegetablesPrices) {
+            return apples1kg != null &&
+                    banana1kg != null &&
+                    oranges1kg != null &&
+                    tomato1kg != null &&
+                    potato1kg != null &&
+                    onion1kg != null &&
+                    lettuceOneHead != null
+        }
+    }
+        fun FruitsAndVegetablesPrice(
+            fruitAndVegetablesPrices: FruitAndVegetablesPrices,
+            averageMonthlyNetSalaryAfterTax: Float?): Float {
+            fruitAndVegetablesPrices.apply{
+                return ((apples1kg!! + banana1kg!! +  oranges1kg!! + tomato1kg!! + potato1kg!! + onion1kg!! + lettuceOneHead!!)
+                        .div(7)).div(averageMonthlyNetSalaryAfterTax!!)
+
+            }
+        }
+
 
 }
--- src/test/kotlin/interactor/GetAvgFruitNVeggieInteractorTest.kt
+++ src/test/kotlin/interactor/GetAvgFruitNVeggieInteractorTest.kt
@@ -12,34 +12,34 @@ class GetAvgFruitNVeggieInteractorTest {
     fun should_ReturnLowest10CityPricesComparedToSalaries_When_TheListNotEmptyAndNotNullAndTheAttributesNotNull(){
         //given list of cities prices of fruit and vegetables and salaries
         val csvParser = CsvParser()
-        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, GetAvgFruitNVeggieInteractorTest.SUB_FILE_NAME) // CSV : comma separated values
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, SUB_FILE_NAME) // CSV : comma separated values
         cheapestFruitNVeggie=GetAvgFruitNVeggieInteractor(dataSource)
         val list=dataSource.getAllCitiesData()
         //when average prices of fruits and vegetables are the lowest compared to the city salaries
         val cheapestCityInPrices = cheapestFruitNVeggie.execute()
 
         //then find the lowest 10 cities fruits and vegetables compared to salaries
-            assertEquals(list.subList(0, 9), cheapestCityInPrices)
+            assertEquals(list.subList(0, 10), cheapestCityInPrices)
     }
     @Test
     fun should_ReturnFirst10City_When_just10CityProvided() {
         //given List of 10 cities prices of fruit and vegetables and salaries
         val csvParser = CsvParser()
-        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, GetAvgFruitNVeggieInteractorTest.TEN_CITY_FILE)
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, TEN_CITY_FILE)
 
         cheapestFruitNVeggie=GetAvgFruitNVeggieInteractor(dataSource)
         val list=dataSource.getAllCitiesData()
         //when average prices of fruits and vegetables are the lowest compared to the city salaries
         val cheapestCityInPrices = cheapestFruitNVeggie.execute()
         //then find the lowest 10 cities fruits and vegetables compared to salaries
-        assertEquals(list.subList(0, 9), cheapestCityInPrices)
+        assertEquals(list.subList(0, 10), cheapestCityInPrices)
     }
 
     @Test
     fun should_ReturnNull_When_TheFileIsEmpty(){
         //given
         val csvParser = CsvParser()
-        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, GetAvgFruitNVeggieInteractorTest.EMPTY_FILE)
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, EMPTY_FILE)
         cheapestFruitNVeggie=GetAvgFruitNVeggieInteractor(dataSource)
 
         //when
@@ -51,7 +51,7 @@ class GetAvgFruitNVeggieInteractorTest {
     @Test
     fun should_ReturnNull_When_AllTheSalariesAreNull(){
         val csvParser = CsvParser()
-        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, GetAvgFruitNVeggieInteractorTest.NULL_SALARIES_FILE)
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, NULL_SALARIES_FILE)
         cheapestFruitNVeggie=GetAvgFruitNVeggieInteractor(dataSource)
 
         //when
@@ -64,7 +64,7 @@ class GetAvgFruitNVeggieInteractorTest {
     fun should_ReturnNull_When_AllTheFruitsAndVegetablesPricesAreNull(){
         //given
        val csvParser = CsvParser()
-        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, GetAvgFruitNVeggieInteractorTest.NULL_FRUITNVEGGIE_COST_FILE)
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, NULL_FRUITNVEGGIE_COST_FILE)
         cheapestFruitNVeggie=GetAvgFruitNVeggieInteractor(dataSource)
 
         //when
@@ -72,7 +72,8 @@ class GetAvgFruitNVeggieInteractorTest {
         //then
         assertNull(cheapestCityInPrices)
 
-   }
+
+    }
 
     companion object{
         private const val SUB_FILE_NAME = "csvFiles/subListOfCostOfLiving20City.csv"
@@ -80,6 +81,7 @@ class GetAvgFruitNVeggieInteractorTest {
         private const val NULL_SALARIES_FILE = "csvFiles/null10Salaries.csv"
         private const val NULL_FRUITNVEGGIE_COST_FILE = "csvFiles/null10Salaries.csv"
         private const val TEN_CITY_FILE = "csvFiles/tenCityFile.csv"
+
 
     }
 }
