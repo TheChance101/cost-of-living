18ab912b6ac69abba1fb7d5d26a0c2e31dfc9e58
ahmed faris <ahmadfaris080@gmail.com>
2023-02-14 00:06:23 +0300

    add implementation of function

--- src/main/kotlin/interactor/GetDifferenBetweenApartmentRenting.kt
+++ src/main/kotlin/interactor/GetDifferenBetweenApartmentRenting.kt
@@ -5,10 +5,27 @@ import model.CityEntity
 class GetDifferentBetweenApartmentRenting(private val dataSource:CostOfLivingDataSource) {
 
     fun getCityWithHighestApartmentRentDifference(dataSource: CostOfLivingDataSource): CityEntity? {
-       return null
+        return dataSource.getAllCitiesData().filter { it.dataQuality }
+            .maxByOrNull { city ->
+                val realEstatesPrices = city.realEstatesPrices
+                val oneBedroomDiff = getApartmentRentDifference(
+                    realEstatesPrices.apartmentOneBedroomInCityCentre,
+                    realEstatesPrices.apartmentOneBedroomOutsideOfCentre
+                )
+                val threeBedroomDiff = getApartmentRentDifference(
+                    realEstatesPrices.apartment3BedroomsInCityCentre,
+                    realEstatesPrices.apartment3BedroomsOutsideOfCentre
+                )
+                maxOf(oneBedroomDiff, threeBedroomDiff)
+            }
+
     }
 
      fun getApartmentRentDifference(centerPrice: Float?, outsidePrice: Float?): Float {
-      return 0f
+
+         if (centerPrice == null || outsidePrice == null) {
+             return 0f
+         }
+         return centerPrice - outsidePrice
     }
 }
