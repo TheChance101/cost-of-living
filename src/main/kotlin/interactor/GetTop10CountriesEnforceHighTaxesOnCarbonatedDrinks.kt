2690e7a97f1f6ad2bdeaf245d0c3fbe758168eae
Yaseen Zaqut <yz456359@gmail.com>
2023-02-13 10:19:08 +0200

    شdd the required task

--- src/main/kotlin/interactor/GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks.kt
+++ src/main/kotlin/interactor/GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks.kt
@@ -4,8 +4,37 @@ import model.CityEntity
 
 class GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks (
     private val dataSource: CostOfLivingDataSource,
-) {
-    fun excute(): List<String>? {
+){
+    fun excute():List<String>?{
+        val list = dataSource.getAllCitiesData()
+
+        if (list.isEmpty()||allPriceIsNull(list)||allDataIsLow(list)) return null
+
+        return dataSource.getAllCitiesData()
+            .filter(::excludeNullPricesAndLowQualityData)
+            .sortedByDescending{it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants}
+            .take(10).map { it.cityName +" : " + it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants}
 
     }
+    private fun excludeNullPricesAndLowQualityData(city: CityEntity): Boolean {
+        return city.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants != null
+                && city.dataQuality
+    }
+    private fun allPriceIsNull(list: List<CityEntity>):Boolean{
+        var count: Int = 0
+        for (element in list)
+            if (element.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants ==null)
+                count++
+
+        return count==list.size
+    }
+    private fun allDataIsLow(list: List<CityEntity>):Boolean{
+        var count: Int = 0
+        for (element in list)
+            if (!element.dataQuality)
+                count++
+
+        return count==list.size
+    }
+
 }
