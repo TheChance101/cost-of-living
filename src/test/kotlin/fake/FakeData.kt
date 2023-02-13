58dbc0abf35700da255c34f8be635b508634f59d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-13 13:10:07 +0200

    add some objects to fake data

--- src/test/kotlin/fake/FakeData.kt
+++ src/test/kotlin/fake/FakeData.kt
@@ -79,79 +79,7 @@ class FakeData : CostOfLivingDataSource {
                 averageMonthlyNetSalaryAfterTax = 18f,
                 dataQuality = true
             ),
-            CityEntity(
-                cityName = "Santiago de Cuba",
-                country = "Cuba",
-                mealsPrices = MealsPrices(
-                    mealInexpensiveRestaurant = 6.5f,
-                    mealFor2PeopleMidRangeRestaurant = 15.0f,
-                    mealAtMcDonaldSOrEquivalent = 5.0f
-                ),
-                drinksPrices = DrinksPrices(
-                    cappuccinoRegularInRestaurants = 2.0f,
-                    cokePepsiAThirdOfLiterBottleInRestaurants = 2.0f,
-                    waterAThirdOfLiterBottleInRestaurants = 1.0f,
-                    milkRegularOneLiter = 3.8f,
-                    waterOneAndHalfLiterBottleAtTheMarket = 2.0f
-                ),
-                fruitAndVegetablesPrices = FruitAndVegetablesPrices(
-                    apples1kg = null,
-                    banana1kg = null,
-                    oranges1kg = null,
-                    tomato1kg = null,
-                    potato1kg = null,
-                    onion1kg = null,
-                    lettuceOneHead = null
-                ),
-                foodPrices = FoodPrices(
-                    loafOfFreshWhiteBread500g = 1.1f,
-                    riceWhite1kg = 1.1f,
-                    eggsRegular12 = 2.03f,
-                    localCheese1kg = 9.0f,
-                    chickenFillets1kg = 6.61f,
-                    beefRound1kgOrEquivalentBackLegRedMeat = 6.61f
-                ),
-                servicesPrices = ServicesPrices(
-                    basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
-                    oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.1f,
-                    internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
-                    fitnessClubMonthlyFeeForOneAdult = null,
-                    tennisCourtRentOneHourOnWeekend = null,
-                    cinemaInternationalReleaseOneSeat = 1.0f,
-                    preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
-                    internationalPrimarySchoolYearlyForOneChild = null
-                ),
-                clothesPrices = ClothesPrices(
-                    onePairOfJeansLevis50oneOrSimilar = 40.0f,
-                    oneSummerDressInAChainStoreZaraHAndM = 40.0f,
-                    onePairOfNikeRunningShoesMidRange = 60.0f,
-                    onePairOfMenLeatherBusinessShoes = 50.0f
-                ),
-                transportationsPrices = TransportationsPrices(
-                    oneWayTicketLocalTransport = null,
-                    monthlyPassRegularPrice = null,
-                    taxiStartNormalTariff = null,
-                    taxi1kmNormalTariff = 0.7f,
-                    taxi1hourWaitingNormalTariff = null,
-                    gasolineOneLiter = null
-                ),
-                carsPrices = CarsPrices(
-                    volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
-                    toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = null
-                ),
-                realEstatesPrices = RealEstatesPrices(
-                    apartmentOneBedroomInCityCentre = 200.0f,
-                    apartmentOneBedroomOutsideOfCentre =100.0f ,
-                    //100
-                    apartment3BedroomsInCityCentre = 600.0f,
-                    apartment3BedroomsOutsideOfCentre = 100f,
-                    //500
-                    pricePerSquareMeterToBuyApartmentInCityCentre = null,
-                    pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
-                ),
-                averageMonthlyNetSalaryAfterTax =null,
-                dataQuality = true
-            ),
+
             CityEntity(
                 cityName = "Sancti Spiritus",
                 country = "Cuba",
@@ -583,6 +511,148 @@ class FakeData : CostOfLivingDataSource {
                     pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
                 ),
                 averageMonthlyNetSalaryAfterTax = 18f,
+                dataQuality = true
+            ),
+            CityEntity(
+                cityName = "Santiago de Cuba",
+                country = "Argentina",
+                mealsPrices = MealsPrices(
+                    mealInexpensiveRestaurant = 6.5f,
+                    mealFor2PeopleMidRangeRestaurant = 15.0f,
+                    mealAtMcDonaldSOrEquivalent = 5.0f
+                ),
+                drinksPrices = DrinksPrices(
+                    cappuccinoRegularInRestaurants = 2.0f,
+                    cokePepsiAThirdOfLiterBottleInRestaurants = 2.0f,
+                    waterAThirdOfLiterBottleInRestaurants = 1.0f,
+                    milkRegularOneLiter = 3.8f,
+                    waterOneAndHalfLiterBottleAtTheMarket = 2.0f
+                ),
+                fruitAndVegetablesPrices = FruitAndVegetablesPrices(
+                    apples1kg = null,
+                    banana1kg = null,
+                    oranges1kg = null,
+                    tomato1kg = null,
+                    potato1kg = null,
+                    onion1kg = null,
+                    lettuceOneHead = null
+                ),
+                foodPrices = FoodPrices(
+                    loafOfFreshWhiteBread500g = 1.1f,
+                    riceWhite1kg = 1.1f,
+                    eggsRegular12 = 2.03f,
+                    localCheese1kg = 9.0f,
+                    chickenFillets1kg = 6.61f,
+                    beefRound1kgOrEquivalentBackLegRedMeat = 6.61f
+                ),
+                servicesPrices = ServicesPrices(
+                    basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
+                    oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.1f,
+                    internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
+                    fitnessClubMonthlyFeeForOneAdult = null,
+                    tennisCourtRentOneHourOnWeekend = null,
+                    cinemaInternationalReleaseOneSeat = 1.0f,
+                    preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
+                    internationalPrimarySchoolYearlyForOneChild = null
+                ),
+                clothesPrices = ClothesPrices(
+                    onePairOfJeansLevis50oneOrSimilar = 40.0f,
+                    oneSummerDressInAChainStoreZaraHAndM = 40.0f,
+                    onePairOfNikeRunningShoesMidRange = 60.0f,
+                    onePairOfMenLeatherBusinessShoes = 50.0f
+                ),
+                transportationsPrices = TransportationsPrices(
+                    oneWayTicketLocalTransport = null,
+                    monthlyPassRegularPrice = null,
+                    taxiStartNormalTariff = null,
+                    taxi1kmNormalTariff = 0.7f,
+                    taxi1hourWaitingNormalTariff = null,
+                    gasolineOneLiter = null
+                ),
+                carsPrices = CarsPrices(
+                    volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
+                    toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = null
+                ),
+                realEstatesPrices = RealEstatesPrices(
+                    apartmentOneBedroomInCityCentre = 200.0f,
+                    apartmentOneBedroomOutsideOfCentre =100.0f ,
+                    apartment3BedroomsInCityCentre = 600.0f,
+                    apartment3BedroomsOutsideOfCentre = 100f,
+                    pricePerSquareMeterToBuyApartmentInCityCentre = null,
+                    pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
+                ),
+                averageMonthlyNetSalaryAfterTax = null,
+                dataQuality = true
+            ),
+            CityEntity(
+                cityName = "cairo blaza",
+                country = "Loliput",
+                mealsPrices = MealsPrices(
+                    mealInexpensiveRestaurant = 6.5f,
+                    mealFor2PeopleMidRangeRestaurant = 15.0f,
+                    mealAtMcDonaldSOrEquivalent = 5.0f
+                ),
+                drinksPrices = DrinksPrices(
+                    cappuccinoRegularInRestaurants = 2.0f,
+                    cokePepsiAThirdOfLiterBottleInRestaurants = 2.0f,
+                    waterAThirdOfLiterBottleInRestaurants = 1.0f,
+                    milkRegularOneLiter = 3.8f,
+                    waterOneAndHalfLiterBottleAtTheMarket = 2.0f
+                ),
+                fruitAndVegetablesPrices = FruitAndVegetablesPrices(
+                    apples1kg = 11.02f,
+                    banana1kg = null,
+                    oranges1kg = 1.1f,
+                    tomato1kg = null,
+                    potato1kg = 1.13f,
+                    onion1kg = 2.21f,
+                    lettuceOneHead = 0.25f
+                ),
+                foodPrices = FoodPrices(
+                    loafOfFreshWhiteBread500g = 1.1f,
+                    riceWhite1kg = 1.1f,
+                    eggsRegular12 = 2.03f,
+                    localCheese1kg = 9.0f,
+                    chickenFillets1kg = 6.61f,
+                    beefRound1kgOrEquivalentBackLegRedMeat = 6.61f
+                ),
+                servicesPrices = ServicesPrices(
+                    basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
+                    oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.1f,
+                    internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
+                    fitnessClubMonthlyFeeForOneAdult = null,
+                    tennisCourtRentOneHourOnWeekend = null,
+                    cinemaInternationalReleaseOneSeat = 1.0f,
+                    preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
+                    internationalPrimarySchoolYearlyForOneChild = null
+                ),
+                clothesPrices = ClothesPrices(
+                    onePairOfJeansLevis50oneOrSimilar = 40.0f,
+                    oneSummerDressInAChainStoreZaraHAndM = 40.0f,
+                    onePairOfNikeRunningShoesMidRange = 60.0f,
+                    onePairOfMenLeatherBusinessShoes = 50.0f
+                ),
+                transportationsPrices = TransportationsPrices(
+                    oneWayTicketLocalTransport = null,
+                    monthlyPassRegularPrice = null,
+                    taxiStartNormalTariff = null,
+                    taxi1kmNormalTariff = 0.7f,
+                    taxi1hourWaitingNormalTariff = null,
+                    gasolineOneLiter = null
+                ),
+                carsPrices = CarsPrices(
+                    volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
+                    toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = null
+                ),
+                realEstatesPrices = RealEstatesPrices(
+                    apartmentOneBedroomInCityCentre = 200.0f,
+                    apartmentOneBedroomOutsideOfCentre =100.0f ,
+                    apartment3BedroomsInCityCentre = 600.0f,
+                    apartment3BedroomsOutsideOfCentre = 100f,
+                    pricePerSquareMeterToBuyApartmentInCityCentre = null,
+                    pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
+                ),
+                averageMonthlyNetSalaryAfterTax = null,
                 dataQuality = true
             ),
         )
