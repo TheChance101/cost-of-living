8341253eb1f589e85b17e485ba2d3e9d33ec8cd0
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-02-13 19:36:28 +0200

    create fake data source

--- src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
+++ src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
@@ -6,7 +6,12 @@ import dataSource.utils.CsvParser
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class TestGetCityWithHighestRentalPriceDifferenceInteractor{
+    private lateinit var getCityWithHighestRentalPriceDifference: GetCityWithHighestRentalPriceDifferenceInteractor
+    private lateinit var dataSource: FakeDataSourceForRentalPrice
 
 
 //internal class TestGetHighestDifferenceBetweenCityCenterAndOutsideCityCenter {
