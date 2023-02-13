6bccdc5fdd0fbedf58fcecca1036fb4b4b8ba789
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-02-13 08:21:54 +0300

    add test case returnTrueWhenNorthAmericaCounty

--- src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
@@ -6,6 +6,26 @@ class GetCityMatchManagerExpectationsInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute() {}
+    private val northAmericaCountries: List<String> =
+        listOf(
+            "United States",
+            "Guatemala",
+            "Belize",
+            "El Salvador",
+            "Honduras",
+            "Nicaragua",
+            "Costa Rica",
+            "Panama",
+            "Canada",
+            "Mexico"
+        )
+
+    private fun filterOnlyNorthAmericaCities(cities: List<CityEntity>): List<CityEntity> =
+        cities.filter { it.country in northAmericaCountries }
+
+
+    fun execute(cities: List<CityEntity>): String {
+        return "${filterOnlyNorthAmericaCities(cities).size}"
+    }
 
 }
--- src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
@@ -1,24 +1,54 @@
 package interactor
 
-import dataSource.FakeDataSource
+import dataSource.CsvDataSource
+import dataSource.utils.CsvParser
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 
 class GetCityMatchManagerExpectationsInteractorTest {
 
+    /// TODO: uncomment this when completing the test
+//    private lateinit var dataSource: FakeDataSource
     private lateinit var interactor: GetCityMatchManagerExpectationsInteractor
-    private lateinit var dataSource: FakeDataSource
+
+    /// TODO: remove this code when completing the test
+    private lateinit var dataSource: CsvDataSource
+    private lateinit var csvParser: CsvParser
+
 
     @BeforeEach
     fun setup() {
-        dataSource = FakeDataSource()
+        /// TODO: remove this code when completing the test
+        csvParser = CsvParser()
+        dataSource = CsvDataSource(csvParser)
+
         interactor = GetCityMatchManagerExpectationsInteractor(dataSource)
+
+        /// TODO: uncomment this when completing the test
+//        dataSource = FakeDataSource()
     }
 
     @Test
-    fun execute() {
+    fun `should return true when country is in one of north america countries`() {
+        // given a list of countries that are in north america
+        val northAmericaCountries: List<String> =
+            listOf(
+                "United States",
+                "Guatemala",
+                "Belize",
+                "El Salvador",
+                "Honduras",
+                "Nicaragua",
+                "Costa Rica",
+                "Panama",
+                "Canada",
+                "Mexico"
+            )
+        // when we check if the countries are in north america
+        val result = interactor.execute(dataSource.getAllCitiesData())
+        // then we should get true
+        assertTrue(northAmericaCountries.contains(result))
     }
 }
