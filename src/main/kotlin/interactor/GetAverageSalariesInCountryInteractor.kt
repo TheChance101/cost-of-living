a585cf59662f7e71780d8b665c7f8c803e8bccf7
Nahla <nahla@email.com>
2023-02-13 20:13:42 +0200

    update execute function in GetAverageSalariesInCountryInteractor class

--- src/main/kotlin/interactor/GetAverageSalariesInCountryInteractor.kt
+++ src/main/kotlin/interactor/GetAverageSalariesInCountryInteractor.kt
@@ -6,7 +6,17 @@ import java.util.*
 class GetAverageSalariesInCountryInteractor (
     private val dataSource: CostOfLivingDataSource,) {
     fun execute(countryName: String): List<Pair<String,Float?>> {
-        return emptyList()
+        val citiesData = dataSource
+            .getAllCitiesData()
+            .filter (::excludeLowQualityData )
+            .filter(::excludeNullSalaries)
+            .filter { (it.country.lowercase(Locale.getDefault())) == countryName.lowercase(Locale.getDefault()) }
+
+        if (citiesData.isEmpty()) {
+            throw IllegalArgumentException("Country name not found")
+        }
+
+        return citiesData.map { Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax) }
     }
     fun excludeLowQualityData(city: CityEntity): Boolean {
         return city.dataQuality
