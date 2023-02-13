db4e51548ecbca7e985134a1092b80307d90bd56
Asia sama <sehunexo710@gmail.com>
2023-02-13 15:50:30 +0300

    add calculate clothes prices methode  && refactor code && add unnecessary files to gitignore

--- src/main/kotlin/interactor/GetTopFashionCitiesInteractor.kt
+++ src/main/kotlin/interactor/GetTopFashionCitiesInteractor.kt
@@ -5,26 +5,27 @@ import model.CityEntity
 class GetTopFashionCitiesInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
-    fun execute(limit: Int): List<String>{
+    fun execute(limit: Int): List<String> {
         return dataSource.getAllCitiesData()
-            .filter (::execludeNullPrices)
-            .sortedBy {
-
-                        it.clothesPrices.onePairOfJeansLevis50oneOrSimilar!! +
-                        it.clothesPrices.oneSummerDressInAChainStoreZaraHAndM!! +
-                        it.clothesPrices.onePairOfNikeRunningShoesMidRange!!
-            }
+            .filter(::excludeNullPrices)
+            .sortedBy(::calculateClothesPrices)
             .take(limit)
-            .map {
-                it.cityName
-            }
+            .map { it.cityName }
     }
 
-    private fun execludeNullPrices(city: CityEntity): Boolean{
+    private fun excludeNullPrices(city: CityEntity): Boolean {
         return with(city.clothesPrices) {
-                this.onePairOfJeansLevis50oneOrSimilar != null &&
-                this.onePairOfNikeRunningShoesMidRange != null &&
-                this.oneSummerDressInAChainStoreZaraHAndM != null
+            this.onePairOfJeansLevis50oneOrSimilar != null &&
+                    this.onePairOfNikeRunningShoesMidRange != null &&
+                    this.oneSummerDressInAChainStoreZaraHAndM != null
+        }
+    }
+
+    private fun calculateClothesPrices(city: CityEntity): Float {
+        return with(city.clothesPrices) {
+            onePairOfJeansLevis50oneOrSimilar!! +
+                    oneSummerDressInAChainStoreZaraHAndM!! +
+                    onePairOfNikeRunningShoesMidRange!!
         }
     }
 }
