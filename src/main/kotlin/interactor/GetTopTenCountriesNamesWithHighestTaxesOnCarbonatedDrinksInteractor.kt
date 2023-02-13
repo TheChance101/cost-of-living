bfc3df7e5ffdb44ce201482dadb7e94ba34d5b21
Abdurrahman-Salah-ad-Din <abdosalahas.01128@gmail.com>
2023-02-13 18:02:59 +0200

    add 3 method and documentation for them 1-excludeInvalidDrinksPrice method 2-excludeLowQualityData method 3-excludeInvalidCountries methods

--- src/main/kotlin/interactor/GetTopTenCountriesNamesWithHighestTaxesOnCarbonatedDrinksInteractor.kt
+++ src/main/kotlin/interactor/GetTopTenCountriesNamesWithHighestTaxesOnCarbonatedDrinksInteractor.kt
@@ -15,5 +15,28 @@ class GetTopTenCountriesNamesWithHighestTaxesOnCarbonatedDrinksInteractor(
         throw Exception("Not Implemented yet")
     }
 
+    /**
+     * Returns `true` if the `cokePepsiAThirdOfLiterBottleInRestaurants` value
+     * is valid
+     * @param cityEntity represents a city.
+     * @return `true` if the value of drink price not `null` and positive.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeInvalidDrinksPrice(cityEntity: CityEntity) = Exception("Not implemented yet")
 
+    /**
+     * Returns `true` for high quality data and `false` for low quality data.
+     * @param cityEntity represents a city.
+     * @return the value of `DataQuality`.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeLowQualityData(cityEntity: CityEntity) = Exception("Not implemented yet")
+
+    /**
+     * Returns `true` if the value of `country` is invalid.
+     * @param cityEntity represents a city.
+     * @return `true` if the value of `countryName` is not empty or `null`.
+     * @author Abdurrahman Salah ad-Din
+     */
+    fun excludeInvalidCountries(cityEntity: CityEntity) = Exception("Not implemented yet")
 }
