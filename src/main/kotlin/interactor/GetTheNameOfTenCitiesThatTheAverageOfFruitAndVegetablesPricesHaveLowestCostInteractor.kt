8aec067a0339420f852c7ebd8e6225bd209f688d
Mahmoud Badway <mahmoudbadawy406@gmail.com>
2023-02-13 21:01:50 +0200

    Add GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor kotlin file

---
+++ src/main/kotlin/interactor/GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor.kt
@@ -0,0 +1,12 @@
+package interactor
+
+class GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor (
+  private val dataSource: CostOfLivingDataSource,) {
+
+  fun execute(): List<String>
+  {
+    throw Throwable("Not Implemented yet")
+  }
+
+
+}
