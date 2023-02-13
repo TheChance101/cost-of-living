2d8e77e8d6d2330db5da4fdbd3fc9d82021ea245
Mohamed Esam <moesam138@gmail.com>
2023-02-14 01:56:13 +0200

    add first update for function

--- src/main/kotlin/interactor/GetSuitableClothesPriceCitiesNameInteractor.kt
+++ src/main/kotlin/interactor/GetSuitableClothesPriceCitiesNameInteractor.kt
@@ -1,9 +1,39 @@
 package interactor
 
+import model.CityEntity
+
 class GetSuitableClothesPriceCitiesNameInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
     fun execute(limit: Int): List<String> {
-        return listOf("")
+        return  dataSource
+            .getAllCitiesData()
+            .sortedBy(::getAveragePriceForClothes)
+            .take(limit)
+            .map { it.cityName }
     }
+    /**
+     * this function get average price for clothes in this city
+     * @return a float represent average price for famous clothes in city
+     */
+
+    private fun getAveragePriceForClothes(cityEntity: CityEntity):Float
+    {
+        var totalPrice:Float =0.0F
+        if(cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM !=null) totalPrice+=cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM
+        if(cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange !=null) totalPrice+=cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange
+        if (cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes !=null) totalPrice+=cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes
+        if(cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar !=null) totalPrice+=cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar
+
+        return totalPrice / getNumberOfBrand(cityEntity)
+    }
+    private fun getNumberOfBrand(cityEntity: CityEntity):Int{
+        var totalBrand =0
+        if(cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM != null) totalBrand++
+        if(cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange != null) totalBrand++
+        if(cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes != null) totalBrand++
+        if(cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar != null) totalBrand++
+        return totalBrand
+    }
+
 }
