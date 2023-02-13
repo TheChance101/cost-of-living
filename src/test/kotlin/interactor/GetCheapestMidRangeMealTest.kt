b5adfab05ee5ebe61a9e842a19aff24c0c7b7f9b
Bassam-Alaa-Yassin <bassamouf@std.mans.edu.eg>
2023-02-13 19:47:11 +0200

    first-test passed

---
+++ src/test/kotlin/interactor/GetCheapestMidRangeMealTest.kt
@@ -0,0 +1,36 @@
+package interactor
+
+import dataSource.FakeCsvDataSource
+import dataSource.utils.CsvParser
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetCheapestMidRangeMealTest {
+
+    lateinit var csvParser : CsvParser
+    lateinit var dataSource: CostOfLivingDataSource
+    lateinit var getCheapestMidRangeMeal: GetCheapestMidRangeMeal
+
+    @BeforeAll
+    fun setup(){
+        csvParser= CsvParser()
+        dataSource  = FakeCsvDataSource(csvParser)
+        getCheapestMidRangeMeal = GetCheapestMidRangeMeal(dataSource)
+    }
+
+    @Test
+    fun should_ReturnCityNotNull_when_Execute() {
+        // given
+        //costOfLiving.csv
+        // when
+        val result = getCheapestMidRangeMeal.execute()
+        // then
+        Assertions.assertNotEquals(null, result)
+
+    }
+
+}
