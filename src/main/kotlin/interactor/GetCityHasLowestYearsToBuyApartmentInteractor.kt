264fb0b6305a6d94f933719c788d73b1734857eb
Hagar Gomaa <https://github.com/Hanaa-Hany/FinalProject.git>
2023-02-13 14:54:03 +0200

    add throw exception in execute function

--- src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
@@ -6,14 +6,16 @@ class GetCityHasLowestYearsToBuyApartmentInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
     fun execute(limit: Int, fullTimeSalary: Int): List<Pair<String, Float>> {
-
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedBy { getNumberOfYearsToBuyApartment(fullTimeSalary, it) }
-            .take(limit)
-            .map { Pair(it.cityName, getNumberOfYearsToBuyApartment(fullTimeSalary, it)) }
-    }
+        if(fullTimeSalary<=0 || limit<0) {
+            throw Exception("Not valid full time salary")
+        }else {
+            return dataSource
+                .getAllCitiesData()
+                .filter(::excludeNullSalariesAndLowQualityData)
+                .sortedBy { getNumberOfYearsToBuyApartment(fullTimeSalary, it) }
+                .take(limit)
+                .map { Pair(it.cityName, getNumberOfYearsToBuyApartment(fullTimeSalary, it)) }
+        } }
 
      fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
         return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null && city.dataQuality
@@ -21,7 +23,7 @@ class GetCityHasLowestYearsToBuyApartmentInteractor(
     }
 
      fun getNumberOfYearsToBuyApartment(fullTimeSalary: Int, city: CityEntity): Float {
-         if(fullTimeSalary<=0){
+         if(fullTimeSalary<=0) {
              throw Exception("Not valid full time salary")
          }
          val numberOfYears =  city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!/(fullTimeSalary*12)
