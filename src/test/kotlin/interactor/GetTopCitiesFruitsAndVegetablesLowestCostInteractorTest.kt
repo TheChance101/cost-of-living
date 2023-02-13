0361196665ff50437955a00fdc22b976e9833626
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-13 13:18:13 +0200

    edit and add some test cases for helper functions

--- src/test/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest.kt
+++ src/test/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest.kt
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,42 +22,76 @@ internal class GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest {
         interactor = GetTopCitiesFruitsAndVegetablesLowestCostInteractor(fakeData)
     }
 
+    // Start testing for getAverageFruitAndVegetablePricesForCity
     @Test
-    fun `should return true when cities fruits and vegetables is not null`(){
+    fun `should return null when sum of all fruit and vegetables prices less than and not equal zero`(){
+        // given
+        val entity = fakeData.getAllCitiesData()[7]
+        // when
+        val result = interactor.getAverageFruitAndVegetablePricesForCity(entity)
+        // then
+        assertNull(result)
+    }
+    @Test
+    fun `should return correct value when all fruit and vegetable is not null`(){
+        // given
+        val entity = fakeData.getAllCitiesData()[0]
+        // when
+        val result = interactor.getAverageFruitAndVegetablePricesForCity(entity)
+        // then
+        assertEquals( 2.427143f , result)
+    }
+    @Test
+    fun `should return correct value when there is at least one value is null`(){
+        // given
+        val entity = fakeData.getAllCitiesData()[1]
+        // when
+        val result = interactor.getAverageFruitAndVegetablePricesForCity(entity)
+        // then
+        assertEquals( 1.5f , result)
+    }
+    // End testing for getAverageFruitAndVegetablePricesForCity
+
+    // Start testing for excludeNullFruitAndVegetablePricesOrNullSalaries
+    @Test
+    fun `should return true when cities fruits and vegetables and average salaries is not null`(){
         // given
         val fakeCity = fakeData.getAllCitiesData()[0]
         // when
-        val result = interactor.excludeNullFruitsAndVegetables(fakeCity)
+        val result = interactor.excludeNullFruitAndVegetablePricesOrNullSalaries(fakeCity)
         // then
         assertTrue(result)
     }
     @Test
-    fun `should return false when all cities and vegetables is null`(){
+    fun `should return false when all cities fruits and vegetables and average salaries is null`(){
         // given
-        val fakeCity = fakeData.getAllCitiesData()[1]
+        val fakeCity = fakeData.getAllCitiesData()[7]
         // when
-        val result = interactor.excludeNullFruitsAndVegetables(fakeCity)
+        val result = interactor.excludeNullFruitAndVegetablePricesOrNullSalaries(fakeCity)
         // then
         assertFalse(result)
     }
     @Test
-    fun `should return true when salaries is not null`(){
+    fun `should return false when cities fruits and vegetables is not null but average salary is null`(){
         // given
-        val fakeCity = fakeData.getAllCitiesData()[0]
+        val fakeCity = fakeData.getAllCitiesData()[8]
         // when
-        val result = interactor.excludeNullSalaries(fakeCity)
-        // then
-        assertTrue(result)
-    }
-    @Test
-    fun `should return false when all salaries is null`(){
-        // given
-        val fakeCity = fakeData.getAllCitiesData()[1]
-        // when
-        val result = interactor.excludeNullSalaries(fakeCity)
+        val result = interactor.excludeNullFruitAndVegetablePricesOrNullSalaries(fakeCity)
         // then
         assertFalse(result)
     }
+    @Test
+    fun `should return false when cities fruits and vegetables is null but average salary is not null`(){
+        // given
+        val fakeCity = fakeData.getAllCitiesData()[1]
+        // when
+        val result = interactor.excludeNullFruitAndVegetablePricesOrNullSalaries(fakeCity)
+        // then
+        assertFalse(result)
+    }
+    // End testing for excludeNullFruitAndVegetablePricesOrNullSalaries
+
+    // Start testing for execute function
     @Test
     fun `should throws exception when limit is negative`(){
         // given
@@ -65,5 +101,6 @@ internal class GetTopCitiesFruitsAndVegetablesLowestCostInteractorTest {
         // then
         assertThrows(Exception::class.java , result)
     }
+    // End testing for execute function
 
 }
