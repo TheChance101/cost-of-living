8b6f92cfde6ff1407d012b2bf62d8f6d313eaa79
kareem-01 <kareemesam52@yahoo.com>
2023-02-13 16:59:57 +0200

    added function for countries

--- src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
+++ src/main/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractor.kt
@@ -1,4 +1,5 @@
 package interactor
+
 import model.CityEntity
 import model.DrinksPrices
 
@@ -19,30 +20,47 @@ class GetHighestCarbonatedDrinksPricesInteractor(private val dataSource: CostOfL
         else {pricesAvg(city)}
     }
 
-    private fun nullAndLowQualityDrinks (cityEntity: CityEntity):Boolean =
+    private fun nullAndLowQualityDrinks(cityEntity: CityEntity): Boolean =
 
         cityEntity.drinksPrices.cappuccinoRegularInRestaurants != null &&
-                cityEntity.drinksPrices.milkRegularOneLiter !=null &&
-                cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants !=null &&
-                cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants !=null &&
-                cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket !=null &&
+                cityEntity.drinksPrices.milkRegularOneLiter != null &&
+                cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants != null &&
+                cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants != null &&
+                cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket != null &&
                 cityEntity.dataQuality
 
 
-    private fun pricesAvg (list: List<CityEntity>) : List<Pair<String,Float>>{
-        val finalList = mutableListOf<Pair<String,Float>>()
-        var avgPrice : Float
+    private fun pricesAvg(list: List<CityEntity>): List<Pair<String, Float>> {
+        val finalList = mutableListOf<Pair<String, Float>>()
+        val setofcountries = mutableSetOf<Pair<String, Float>>()
+        var avgPrice: Float
         list.forEach {
             avgPrice = avgPriceForASingleCity(it)
-            finalList.add(Pair(it.country,avgPrice))
+            setofcountries.add(Pair(it.country, avgPrice))
         }
-        return finalList
+        return setofcountries.toList()
     }
 
+    // function to return the average prices for only one city
     private fun avgPriceForASingleCity(cityEntity: CityEntity) = (cityEntity.drinksPrices.milkRegularOneLiter!! +
             cityEntity.drinksPrices.cappuccinoRegularInRestaurants!! +
             cityEntity.drinksPrices.waterOneAndHalfLiterBottleAtTheMarket!! +
-            cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants!!+
-            cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants!!)/5
+            cityEntity.drinksPrices.waterAThirdOfLiterBottleInRestaurants!! +
+            cityEntity.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants!!) / 5
 
+    //a function to return the average prices for a whole country
+    private fun avgpriceforacountry(list: List<CityEntity>): Float {
+        var listofprices = mutableListOf<Float>()
+        var finalvalue : Float = 0f
+        list.forEach {
+            listofprices.add(avgPriceForASingleCity(it))
+        }
+        listofprices.forEach{
+            finalvalue += it
+        }
+    return finalvalue/listofprices.size
+    }
+
+    //
+    private fun pricesforacoutry
 }
