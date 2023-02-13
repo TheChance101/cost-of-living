52340df4d3148f299881b27d376fe2ac78cc4b89
Anwaar Raafat <raafatanwaar@gmail.com>
2023-02-13 21:54:45 +0200

    add implementation of methods

--- src/main/kotlin/interactor/GetTopFiveCitesNameInClothesFromFamousBrandsInteractor.kt
+++ src/main/kotlin/interactor/GetTopFiveCitesNameInClothesFromFamousBrandsInteractor.kt
@@ -3,21 +3,33 @@ package interactor
 import model.CityEntity
 import model.ClothesPrices
 
-class GetTopFiveCitesNameInClothesFromFamousBrandsInteractor (
-    private val dataSource: CostOfLivingDataSource,
-    ) {
+class GetTopFiveCitesNameInClothesFromFamousBrandsInteractor(
 
+    private val dataSource: CostOfLivingDataSource,
+) {
+    val limit=5
     fun execute(): List<String> {
 
-            return listOf("null")
-        }
-        fun excludeNullClothesPrices(city: CityEntity): Boolean {
-
-            return false}
-
-    }
-    fun avgPrice(clothesPrices: ClothesPrices): Float {
-
-        return 0f
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullClothesPrices)
+            .sortedByDescending { avgPrice(it.clothesPrices) }
+            .take(limit)
+            .map { it.cityName }
 
     }
+
+    fun excludeNullClothesPrices(city: CityEntity): Boolean {
+        return city.clothesPrices.onePairOfJeansLevis50oneOrSimilar != null && city.clothesPrices.onePairOfMenLeatherBusinessShoes != null
+                && city.clothesPrices.onePairOfNikeRunningShoesMidRange != null && city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM != null
+
+    }
+
+}
+
+fun avgPrice(clothesPrices: ClothesPrices): Float {
+    var price = (clothesPrices.onePairOfJeansLevis50oneOrSimilar!! + clothesPrices.onePairOfMenLeatherBusinessShoes!! +
+            clothesPrices.onePairOfNikeRunningShoesMidRange!! + clothesPrices.oneSummerDressInAChainStoreZaraHAndM!!) / 4
+    return price
+
+}
