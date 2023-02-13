98fef9df337bd3311885c06fcdb6ab34ebff6e7c
أســـــاور <xyzas.7510@gmail.com>
2023-02-13 18:34:14 +0300

    add logic

--- src/main/kotlin/interactor/GetTopTenCountriesTaxesInteractor.kt
+++ src/main/kotlin/interactor/GetTopTenCountriesTaxesInteractor.kt
@@ -3,8 +3,19 @@ package interactor
 
 class GetTopTenCountriesTaxesInteractor(private val dataSource : CostOfLivingDataSource) {
     fun execute() : List<Pair<String, Float>> {
-        return emptyList()
+        val drinksPrices = dataSource.getAllCitiesData()
+        val filteredDrinksPrices =
+            drinksPrices.filter { it.dataQuality && it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants != null }
+        val topTenCountries = filteredDrinksPrices
+            .groupBy { it.country }
+            .mapValues { (_, prices) -> prices.sumOf { it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants!!.toDouble() } / prices.size }
+            .toList()
+            .sortedByDescending { (_, avgPrice) -> avgPrice }
+            .take(10)
+        return topTenCountries.map { (country, avgPrice) -> Pair(country, avgPrice.toFloat()) }
     }
+
+
 }
 
 
