715b33e61e6d97fa28450ce629eff438b7b3b0d1
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-02-13 22:25:42 +0300

    add comments

--- src/test/kotlin/interactor/getTheAverageSalaryForEachCityInTheCountryInteractorTest.kt
+++ src/test/kotlin/interactor/getTheAverageSalaryForEachCityInTheCountryInteractorTest.kt
@@ -1,4 +1,5 @@
 package interactor
+
 import fakedata.FakeData
 import org.junit.jupiter.api.Assertions.*
 
@@ -11,61 +12,67 @@ import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class getTheAverageSalaryForEachCityInTheCountryInteractorTest {
-    lateinit var fakeData :FakeData
+    lateinit var fakeData: FakeData
+
     @BeforeAll
-    fun setup(){
-        fakeData= FakeData()
+    fun setup() {
+        fakeData = FakeData()
     }
+
     @Test
-    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsLowerCase (){
+    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsLowerCase() {
         //given country name in lower case
         val countryName = "cuba"
         //when find the city name and salary average
-         val cityAndSalaryAverage = getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData).execute(countryName)
+        val cityAndSalaryAverage = getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData).execute(countryName)
         //then check return a List of pairs each pair have city name and salary average for this city
-        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(),cityAndSalaryAverage.toString())
+        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(), cityAndSalaryAverage.toString())
 
     }
+
     @Test
-    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsUpperCase (){
+    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsUpperCase() {
         //given country name in upper case
         val countryName = "CUBA"
         //when find the city name and salary average
         val cityAndSalaryAverage = getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData).execute(countryName)
         //then check return a List of pairs each pair have city name and salary average for this city
-        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(),cityAndSalaryAverage.toString())
+        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(), cityAndSalaryAverage.toString())
     }
+
     @Test
-    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsMixedCase (){
+    fun should_ReturnListOfPairsEachPairContainCityNameAndSalaryAverage_whenCountryNameIsMixedCase() {
         //given country name in mix case
         val countryName = "Cuba"
         //when  the city name and salary average
         val cityAndSalaryAverage = getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData).execute(countryName)
         //then check return a List of pairs each pair have city name and salary average for this city
-        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(),cityAndSalaryAverage.toString())
+        assertEquals(listOf<Any>(listOf("Havana", 35.75)).toString(), cityAndSalaryAverage.toString())
     }
+
     @Test
-    fun should_throwAnException_whenCountryNameWasEmpty (){
+    fun should_throwAnException_whenCountryNameWasEmpty() {
         //given empty country name
-        val countryName=""
+        val countryName = ""
         //when  the city name and salary average
-        val cityAndSalaryAverageExecutable:Executable =Executable{
+        val cityAndSalaryAverageExecutable: Executable = Executable {
             getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData)
                 .execute(countryName)
         }
         //then check exception
-        assertThrows(Exception::class.java,cityAndSalaryAverageExecutable)
+        assertThrows(Exception::class.java, cityAndSalaryAverageExecutable)
     }
+
     @Test
-    fun should_throwAnException_whenCountryNameWasWrong (){
+    fun should_throwAnException_whenCountryNameWasWrong() {
         //given wrong country name
-        val countryName="test wrong name"
+        val countryName = "test wrong name"
         //when  the city name and salary average
-        val cityAndSalaryAverageExecutable:Executable =Executable{
+        val cityAndSalaryAverageExecutable: Executable = Executable {
             getTheAverageSalaryForEachCityInTheCountryInteractor(fakeData)
                 .execute(countryName)
         }
         //then check exception
-        assertThrows(Exception::class.java,cityAndSalaryAverageExecutable)
+        assertThrows(Exception::class.java, cityAndSalaryAverageExecutable)
     }
 }
