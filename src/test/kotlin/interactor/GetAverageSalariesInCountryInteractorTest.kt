eb89f89d72db418fe8ff8e2f8e3e467e27f421b1
Nahla <nahla@email.com>
2023-02-13 08:03:37 +0200

    add setUp function in GetAverageSalariesInCountryInteractorTest class

--- src/test/kotlin/interactor/GetAverageSalariesInCountryInteractorTest.kt
+++ src/test/kotlin/interactor/GetAverageSalariesInCountryInteractorTest.kt
@@ -1,10 +1,18 @@
 package interactor
 
+import fakeDataSource.FakeDataSource
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
 
 class GetAverageSalariesInCountryInteractorTest {
 
+    lateinit var getAverageSalariesInCountryInteractor: GetAverageSalariesInCountryInteractor
 
+    @BeforeAll
+    fun setUp() {
+        var fake = FakeDataSource()
+        getAverageSalariesInCountryInteractor = GetAverageSalariesInCountryInteractor(fake)
+    }
 }
