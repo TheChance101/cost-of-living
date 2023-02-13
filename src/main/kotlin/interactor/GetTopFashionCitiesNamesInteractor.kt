29ec9d99dd45985da9b982bc24dda19917572cfc
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2023-02-13 22:28:22 +0300

    fix interactor null exception

--- src/main/kotlin/interactor/GetTopFashionCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetTopFashionCitiesNamesInteractor.kt
@@ -8,16 +8,11 @@ class GetTopFashionCitiesNamesInteractor(
 
     fun execute(limit: Int): List<String> {
         if (limit <= 0) return emptyList()
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullPricesAndLowQualityData)
-            .sortedBy { it.getClothesAveragePrice() }
-            .take(limit)
-            .map { it.cityName }
+        return dataSource.getAllCitiesData().filter(::excludeNullPricesAndLowQualityData)
+            .sortedBy { it.getClothesAveragePrice() }.take(limit).map { it.cityName }
     }
 
-    private fun excludeNullPricesAndLowQualityData(city: CityEntity) =
-        city.hasNoNullClothesPrices() && city.dataQuality
+    private fun excludeNullPricesAndLowQualityData(city: CityEntity) = city.hasNoNullClothesPrices() && city.dataQuality
 
     private fun CityEntity.hasNoNullClothesPrices(): Boolean {
         val fields = clothesPrices::class.java.declaredFields
@@ -28,16 +23,12 @@ class GetTopFashionCitiesNamesInteractor(
     }
 
     private fun CityEntity.getClothesAveragePrice(): Float {
-        return clothesPrices::class.java.declaredFields
-            .map {
-                it.isAccessible = true
-                it.get(clothesPrices) as Float
-            }
-            .average()
-            .toFloat()
+        return clothesPrices::class.java.declaredFields.mapNotNull {
+            it.isAccessible = true
+            val value = it.get(clothesPrices)
+            if (value is Float) value else null
+        }.average().toFloat()
     }
-
-
 
 
 }
