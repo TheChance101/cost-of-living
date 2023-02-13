b50584d318c01bee1c8e29eecfac18f8b5bd03f5
muhamedsultan21 <sultanmedo49@gmail.com>
2023-02-14 01:34:49 +0200

    add feature implementation

--- src/main/kotlin/interactor/FindTheHighestDifferenceInRentalPricesInteractor.kt
+++ src/main/kotlin/interactor/FindTheHighestDifferenceInRentalPricesInteractor.kt
@@ -2,8 +2,26 @@ package interactor
 
 import model.CityEntity
 
-class FindTheHighestDifferenceInRentalPricesInteractor(dataSource: CostOfLivingDataSource) {
-    fun execute(): CityEntity? {
-        return null
+class FindTheHighestDifferenceInRentalPricesInteractor(private val dataSource: CostOfLivingDataSource) {
+    fun execute(): CityEntity {
+        val highestDifference = dataSource.getAllCitiesData()
+            .filter(::isDataQuality)
+            .maxBy {
+                val oneBedroomDifference =
+                    it.realEstatesPrices.apartmentOneBedroomInCityCentre!! -
+                            it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
+
+                val threeBedroomsDifference =
+                    it.realEstatesPrices.apartment3BedroomsInCityCentre!! -
+                            it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
+                maxOf(
+                    oneBedroomDifference, threeBedroomsDifference,
+                )
+            }
+        return highestDifference
+    }
+
+    private fun isDataQuality(city: CityEntity): Boolean {
+        return city.dataQuality
     }
 }
