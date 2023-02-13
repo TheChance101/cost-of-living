11b579dd04b90c6fc7d4d95dfffecc56aadb56ff
hamsa <hamsaalsaadie@gmail.com>
2023-02-14 00:35:23 +0300

    improve code

--- src/main/kotlin/interactor/GetCityHasHighestRentingBetweenCenterAndOutsideCityCenter.kt
+++ src/main/kotlin/interactor/GetCityHasHighestRentingBetweenCenterAndOutsideCityCenter.kt
@@ -1,33 +1,41 @@
 package interactor
 
 import model.CityEntity
-import model.RealEstatesPrices
+import kotlin.math.abs
 
-class GetCityWithHighestCostOfLivingDifferenceInteractor(
-    private val dataSource: CostOfLivingDataSource,
+class GetCityHasHighestDifferenceRentBetweenCityCenterAndOutside(
+    private val dataSource: CostOfLivingDataSource
 ) {
-
-    fun execute(): CityEntity {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullPricesAndLowQualityData)
-            .maxByOrNull {
-                getLivingCostDifference(it.realEstatesPrices)
-            }!!
+    fun execute(type: typeOfApartments): CityEntity? {
+        return dataSource.getAllCitiesData()
+            .filter(::excludeNullRentAndLowQualityData)
+            .maxByOrNull { city ->
+                when (type) {
+                    typeOfApartments.ONE_BEDROOM -> abs(
+                        city.realEstatesPrices.apartmentOneBedroomInCityCentre!!
+                                - city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
+                    )
+                    typeOfApartments.THREE_BEDROOMS -> abs(
+                        city.realEstatesPrices.apartment3BedroomsInCityCentre!!
+                                - city.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
+                    )
+                }
+            }
     }
 
-    private fun excludeNullPricesAndLowQualityData(city: CityEntity): Boolean {
-        return city.realEstatesPrices.apartmentOneBedroomInCityCentre != null &&
-                city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null &&
-                city.realEstatesPrices.apartment3BedroomsInCityCentre != null &&
-                city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null &&
-                city.dataQuality
+    private fun excludeNullRentAndLowQualityData(city: CityEntity): Boolean {
+        return city.dataQuality
+                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+                && city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
     }
 
-    private fun getLivingCostDifference(prices: RealEstatesPrices): Float {
-        return (prices.apartmentOneBedroomInCityCentre?.minus(prices.apartmentOneBedroomOutsideOfCentre!!))?.plus((prices.apartment3BedroomsInCityCentre?.minus(prices.apartment3BedroomsOutsideOfCentre!!)!!)) ?: 0f
-    }
 }
 
+enum class typeOfApartments {
+    ONE_BEDROOM, THREE_BEDROOMS
+
+}
 
 
