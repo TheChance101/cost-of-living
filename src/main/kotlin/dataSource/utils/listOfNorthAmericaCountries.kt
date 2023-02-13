6e48b01b01a97630fa60c9d030e9eab3fdb66de9
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2023-02-13 10:12:54 +0300

    create listOfNorthAmericaCountries

---
+++ src/main/kotlin/dataSource/utils/listOfNorthAmericaCountries.kt
@@ -0,0 +1,17 @@
+package dataSource.utils
+
+object NorthAmericaCountries {
+    val list =
+        listOf(
+            "United States",
+            "Guatemala",
+            "Belize",
+            "El Salvador",
+            "Honduras",
+            "Nicaragua",
+            "Costa Rica",
+            "Panama",
+            "Canada",
+            "Mexico"
+        )
+}
