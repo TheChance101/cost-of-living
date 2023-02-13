54b234339956f02764266167c31cd61f068f763e
Abdallah Sayes <abdallahsayed3355@gmail.com>
2023-02-14 01:55:49 +0200

    add some tests and make some changes on tests

--- src/test/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountryTest.kt
+++ src/test/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountryTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
 
 val csvParser = CsvParser()
 val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
@@ -26,6 +27,52 @@ class GetCitiesNamesAndAverageSalariesInCountryTest {
         //when get the cities name and average salary in country
         val ListOfCitiesAndSalaries=getResult.execute(country)
         //then check the result
-        assertEquals(listOf(Pair("Tanta",101.74.toFloat()),Pair("Alexandria",163.76.toFloat()), Pair("Giza", 171.51.toFloat()),Pair("Cairo", 200.4.toFloat())),ListOfCitiesAndSalaries)
+        val list=listOf(Pair("Tanta",101.74),Pair("Alexandria",163.76), Pair("Giza", 171.51),Pair("Cairo", 200.4))
+        assertEquals(list,ListOfCitiesAndSalaries)
+    }
+
+    @Test
+    fun should_ReturnNamesOfCitiesAndAverageSalary_when_EnterCountryNameInTheUpperCase() {
+        //given name of the country
+        val country="EGYPT"
+        //when get the cities name and average salary in country
+        val ListOfCitiesAndSalaries=getResult.execute(country)
+        //then check the result
+        val list=listOf(Pair("Tanta",101.74),Pair("Alexandria",163.76), Pair("Giza", 171.51),Pair("Cairo", 200.4))
+        assertEquals(list,ListOfCitiesAndSalaries)
+    }
+
+    @Test
+    fun should_ReturnNamesOfCitiesAndAverageSalary_when_EnterCountryNameInTheLowerCase() {
+        //given name of the country
+        val country="egypt"
+        //when get the cities name and average salary in country
+        val ListOfCitiesAndSalaries=getResult.execute(country)
+        //then check the result
+        val list=listOf(Pair("Tanta",101.74),Pair("Alexandria",163.76), Pair("Giza", 171.51),Pair("Cairo", 200.4))
+        assertEquals(list,ListOfCitiesAndSalaries)
+    }
+
+
+    @Test
+    fun should_ReturnNamesOfCitiesAndAverageSalary_when_EnterCountryNameInMixtureOfUppercaseAndLowercase() {
+        //given name of the country
+        val country="eGyPt"
+        //when get the cities name and average salary in country
+        val ListOfCitiesAndSalaries=getResult.execute(country)
+        //then check the result
+        val list=listOf(Pair("Tanta",101.74),Pair("Alexandria",163.76), Pair("Giza", 171.51),Pair("Cairo", 200.4))
+        assertEquals(list,ListOfCitiesAndSalaries)
+    }
+
+
+    @Test
+    fun should_ThrowException_when_EnterWrongName() {
+        //given name of the country
+        val country="sd"
+        //when get the cities name and average salary in country
+        // then check the result
+        assertThrows<Exception> {getResult.execute(country)}
+
     }
 }
