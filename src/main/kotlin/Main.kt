52dc388fb470198e4db8ecd86f9d44db0d2334ce
Hagar Gomaa <https://github.com/Hanaa-Hany/FinalProject.git>
2023-02-13 05:04:45 +0200

    call function execute in main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,18 +3,20 @@ import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
 import interactor.CostOfLivingDataSource
 import interactor.GetCityHasCheapestInternetConnectionInteractor
+import interactor.GetCityHasLowestYearsToBuyApartmentInteractor
 import interactor.GetHighestSalaryAverageCititesNamesInteractor
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
-    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
-    println(getHighestSalaryAverageCities.execute(limit = 10))
+    val getCityHasLowestYearsToBuyApartmentInteractor = GetCityHasLowestYearsToBuyApartmentInteractor(dataSource)
+    println(getCityHasLowestYearsToBuyApartmentInteractor.execute(10,55))
     printSeparationLine()
-
-    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-    println(getCityHasCheapestInternetConnectionInteractor.execute())
+    println(getCityHasLowestYearsToBuyApartmentInteractor.execute(7,100))
+    printSeparationLine()
+    println(getCityHasLowestYearsToBuyApartmentInteractor.execute(0,55))
+    printSeparationLine()
 
 }
 private fun printSeparationLine(){
