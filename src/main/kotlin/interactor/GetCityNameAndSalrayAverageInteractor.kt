8e7207b2a1ca335b8d789e4c938337f0ed8c4525
nahedshehata <109394234+nahedshehata@users.noreply.github.com>
2023-02-14 01:17:32 +0200

    add implemention function

--- src/main/kotlin/interactor/GetCityNameAndSalrayAverageInteractor.kt
+++ src/main/kotlin/interactor/GetCityNameAndSalrayAverageInteractor.kt
@@ -6,10 +6,28 @@ import java.lang.reflect.Executable
 class GetCityNameAndSalrayAverageInteractor(private val dataSource: CostOfLivingDataSource) {
 
 
-   fun execute(country:String):List<Pair<String,Float>>
-   {
-
-       return emptyList()
-   }
+    fun execute(country: String): List<Pair<String,Float>>
+    {
+        val list = dataSource
+            .getAllCitiesData()
+            .filter { (it.country == country.convert()) && (it.dataQuality) }.getSalary()
+        return list
+    }
+    private fun List<CityEntity>.getSalary(): List<Pair<String, Float>> {
+        val newList = mutableListOf<Pair<String, Float>>()
+        var salary: Float
+        this.forEach{
+            salary =
+                it.averageMonthlyNetSalaryAfterTax!!
+            newList.add(Pair(it.cityName, salary))
+        }
+        return newList
+    }
+    private fun String.convert(): String {
+        return capitalize(lowercase())!!
+    }
+    private fun capitalize(str: String?): String? {
+        return str?.capitalize() ?: str
+    }
 
 }
