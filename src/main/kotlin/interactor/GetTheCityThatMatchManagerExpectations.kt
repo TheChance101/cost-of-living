78748a536fdfbf5d3cc3b01fccf289a9e1161a28
Saif Alhaider <saifharith90@gmail.com>
2023-02-14 02:06:29 +0300

    finished execute function

--- src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectations.kt
+++ src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectations.kt
@@ -2,12 +2,20 @@ package interactor
 
 import model.CityEntity
 import dataSource.CsvDataSource
-class GetTheCityThatMatchManagerExpectations(dataSource: CostOfLivingDataSource) {
-    fun excute():CityEntity?{
-        return null
-    }
-    fun searchCity(): CityEntity? {
-        return null
+import java.lang.Math.floor
+
+class GetTheCityThatMatchManagerExpectations(private val dataSource: CostOfLivingDataSource) {
+    fun execute(): CityEntity {
+        val northAmericaCountries = filterByCountryAndNoneNullable(dataSource.getAllCitiesData())
+            .sortedBy { it.mealsPrices.mealFor2PeopleMidRangeRestaurant }
+        return northAmericaCountries.get(getMedianValue(northAmericaCountries.size))
     }
 
+    fun filterByCountryAndNoneNullable(countries: List<CityEntity>): List<CityEntity> =
+        countries.filter {
+            (it.country == "United States" || it.country == "Mexico" || it.country == "Canada")
+                    && it.mealsPrices.mealFor2PeopleMidRangeRestaurant != null
+        }
+
+    fun getMedianValue(number: Int): Int = kotlin.math.floor(number / 2.0).toInt()
 }
