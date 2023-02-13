64d2220343ecb28fb999a7e1d65dd7b5784ed6fe
yassen ramadan <yassenyasooo50@gmail.com>
2023-02-13 21:36:21 +0200

    add some code in functions and fix test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,21 @@ import dataSource.utils.CsvParser
 import interactor.CostOfLivingDataSource
 import interactor.GetCityHasCheapestInternetConnectionInteractor
 import interactor.GetHighestSalaryAverageCititesNamesInteractor
+import interactor.GetTheRightApartment
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
-    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
-    println(getHighestSalaryAverageCities.execute(limit = 10))
-    printSeparationLine()
-
-    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-    println(getCityHasCheapestInternetConnectionInteractor.execute())
+//    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
+//    println(getHighestSalaryAverageCities.execute(limit = 10))
+//    printSeparationLine()
+//
+//    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+//    println(getCityHasCheapestInternetConnectionInteractor.execute())
+//
+    val getTheRightApartment = GetTheRightApartment(dataSource)
+    println(getTheRightApartment.getListOfDetailsOfApartment(1000))
 
 }
 private fun printSeparationLine(){
