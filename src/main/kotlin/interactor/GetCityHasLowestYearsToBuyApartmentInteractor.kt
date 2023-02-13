f0e3d7d8119b8a06c8e2151438b4e94b1ac5d215
Hagar Gomaa <https://github.com/Hanaa-Hany/FinalProject.git>
2023-02-13 02:59:35 +0200

    add fun get number of years to buy apartment and add implementation in fun execute

--- src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
@@ -5,12 +5,23 @@ import model.CityEntity
 class GetCityHasLowestYearsToBuyApartmentInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
-    fun execute(limit:Int,fullTimeSalary : Int): List<Pair<String,Float>> {
+    fun execute(limit: Int, fullTimeSalary: Int): List<Pair<String, Float>> {
 
-        TODO(" Not implemented yet ")
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullSalariesAndLowQualityData)
+            .sortedBy { getNumberOfYearsToBuyApartment(fullTimeSalary, it) }
+            .take(limit)
+            .map { Pair(it.cityName, getNumberOfYearsToBuyApartment(fullTimeSalary, it)) }
+    }
+
+     fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null && city.dataQuality
 
     }
-    fun excludeNullPriceOfApartmentAndLowQualityData(city: CityEntity): Boolean {
-        return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&  city.dataQuality
+
+     fun getNumberOfYearsToBuyApartment(fullTimeSalary: Int, city: CityEntity): Float {
+        TODO(" Not implemented yet ")
+
     }
 }
