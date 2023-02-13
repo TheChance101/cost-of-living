fced7b3338f0492820303ec538a0dd0f6ba205eb
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-02-13 19:30:48 +0200

    create class get highest price different

--- src/main/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartment.kt
+++ src/main/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartment.kt
@@ -3,36 +3,88 @@ package interactor
 import model.CityEntity
 import model.RealEstatesPrices
 
-class GetHighestDifferenceBetweenCityCenterAndOutsideCityCenter(
-    private val dataSource:CostOfLivingDataSource
-)  {
 
-    fun execute_DifferentPayRentingBetweenIntCityCenter_OutCitycenter(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullDataAndLowQuality)
-            .sortedByDescending { it.realEstatesPrices.apartmentOneBedroomInCityCentre!! - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!! }
-            .take(limit)
-            .map { it.cityName }
-    }
-    fun execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter( limit: Int ): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullDataAndLowQuality)
-            .sortedByDescending { it.realEstatesPrices.apartment3BedroomsInCityCentre!! - it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!! }
-            .take(limit)
-            .map { it.cityName }
-    }
-    fun execute_DifferentPayRentingBetween_pricePerSquareMeterToBuy_IntCityCenter_OutCitycenter( limit: Int ): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullDataAndLowQuality)
-            .sortedByDescending { it.realEstatesPrices.pricePerSquareMeterToBuyApartmentInCityCentre!! - it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!! }
-            .take(limit)
-            .map { it.cityName }
+class GetCityWithHighestRentalPriceDifferenceInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
+
+    fun execute(): String {
+//        val listOfData = mutableListOf<CityByPriceDifference>()
+//
+//        dataSource.getAllCitiesData().filter(::excludeNullValues)
+//            .forEach {
+//                val priceDifference =
+//                    it.realEstatesPrices.pricePerSquareMeterToBuyApartmentInCityCentre!! - it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
+//                listOfData.add(CityByPriceDifference(it.cityName, priceDifference.toDouble()))
+//            }
+//
+//
+//        listOfData.sortByDescending { it.priceDifference }
+//
+//        return listOfData[0].nameCity
     }
 
-    private fun excludeNullDataAndLowQuality(city: CityEntity): Boolean {
-        return city.realEstatesPrices != null && city.realEstatesPrices != null && city.dataQuality
+    fun excludeNullValues(city: CityEntity): Boolean {
+        return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentInCityCentre != null && city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null
     }
+
+}
+
+data class CityByPriceDifference(var nameCity: String, var priceDifference: Double)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class GetHighestDifferenceBetweenCityCenterAndOutsideCityCenter(
+//    private val dataSource:CostOfLivingDataSource
+//)  {
+
+
+
+
+//    fun execute_DifferentPayRentingBetweenIntCityCenter_OutCitycenter(limit: Int): List<String> {
+//        return dataSource
+//            .getAllCitiesData()
+//            .filter(::excludeNullDataAndLowQuality)
+//            .sortedByDescending { it.realEstatesPrices.apartmentOneBedroomInCityCentre!! - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!! }
+//            .take(limit)
+//            .map { it.cityName }
+//    }
+//    fun execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter( limit: Int ): List<String> {
+//        return dataSource
+//            .getAllCitiesData()
+//            .filter(::excludeNullDataAndLowQuality)
+//            .sortedByDescending { it.realEstatesPrices.apartment3BedroomsInCityCentre!! - it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!! }
+//            .take(limit)
+//            .map { it.cityName }
+//    }
+//    fun execute_DifferentPayRentingBetween_pricePerSquareMeterToBuy_IntCityCenter_OutCitycenter( limit: Int ): List<String> {
+//        return dataSource
+//            .getAllCitiesData()
+//            .filter(::excludeNullDataAndLowQuality)
+//            .sortedByDescending { it.realEstatesPrices.pricePerSquareMeterToBuyApartmentInCityCentre!! - it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!! }
+//            .take(limit)
+//            .map { it.cityName }
+//    }
+
+//    private fun excludeNullDataAndLowQuality(city: CityEntity): Boolean {
+//        return city.realEstatesPrices != null && city.realEstatesPrices != null && city.dataQuality
+//    }
 }
--- src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
+++ src/test/kotlin/interactor/GetCityHasHighestDifferentPayRentingApartmentTest.kt
@@ -6,78 +6,80 @@ import dataSource.utils.CsvParser
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+internal class TestGetCityWithHighestRentalPriceDifferenceInteractor{
 
-internal class TestGetHighestDifferenceBetweenCityCenterAndOutsideCityCenter {
-    val csvParser = CsvParser()
-    val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
-    var rating = interactor.GetHighestDifferenceBetweenCityCenterAndOutsideCityCenter(dataSource)
-
-    @Test
-    fun testGetHighestDifference_returnsCorrect_OneBedRoom_BetweenInCityCenter_OutCityCenter() {
-        //given
-        val cities = listOf(
-            "Abuja",
-            "Walton upon Thames",
-            "Port Moresby",
-        )
-        var expectedResult: List<String>
-//        for (i in cities.indices) {
-            if (cities.isEmpty()) {
-                emptyList<String>()
-            }else {
-                expectedResult = listOf(cities[0])
-                //when
-                val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
-                //then
-                assertEquals(expectedResult, result)
-//            }
-        }
-    }
-
-    @Test
-    fun testGetHighestDifference_returnsCorrect_3BedRoom_BetweenInCityCenter_OutCityCenter() {
-        //given
-        val cities = listOf(
-            "Abuja",
-            "New York",
-            "Logos",
-        )
-
-        var expectedResult: List<String>
-//        for (i in cities.indices){
-            if (cities.isEmpty()) {
-                emptyList<String>()
-            } else {
-                expectedResult = listOf(cities[0])
-                //when
-                val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
-                //then
-                assertEquals(expectedResult, result)
-//            }
-        }
-    }
-
-    @Test
-    fun testGetHighestDifference_returnsCorrect_pricePerSquareMeterToBuy_BetweenInCityCenter_OutCityCenter() {
-        val cities = listOf(
-            "Abuja",
-            "New York",
-            "Logos",
-        )
-        var expectedResult: List<String>
-//        for (i in cities.indices){
-           if (cities.isEmpty()) {
-               emptyList<String>()
-           } else {
-               expectedResult = listOf(cities[0])
-               //when
-               val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
-               //then
-               assertEquals(expectedResult, result)
-//           }
-       }
-    }
+//internal class TestGetHighestDifferenceBetweenCityCenterAndOutsideCityCenter {
+//    val csvParser = CsvParser()
+//    val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
+//
+//    var rating = interactor.GetHighestDifferenceBetweenCityCenterAndOutsideCityCenter(dataSource)
+//
+//    @Test
+//    fun testGetHighestDifference_returnsCorrect_OneBedRoom_BetweenInCityCenter_OutCityCenter() {
+//        //given
+//        val cities = listOf(
+//            "Abuja",
+//            "Walton upon Thames",
+//            "Port Moresby",
+//        )
+//        var expectedResult: List<String>
+////        for (i in cities.indices) {
+//            if (cities.isEmpty()) {
+//                emptyList<String>()
+//            }else {
+//                expectedResult = listOf(cities[0])
+//                //when
+//                val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
+//                //then
+//                assertEquals(expectedResult, result)
+////            }
+//        }
+//    }
+//
+//    @Test
+//    fun testGetHighestDifference_returnsCorrect_3BedRoom_BetweenInCityCenter_OutCityCenter() {
+//        //given
+//        val cities = listOf(
+//            "Abuja",
+//            "New York",
+//            "Logos",
+//        )
+//
+//        var expectedResult: List<String>
+////        for (i in cities.indices){
+//            if (cities.isEmpty()) {
+//                emptyList<String>()
+//            } else {
+//                expectedResult = listOf(cities[0])
+//                //when
+//                val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
+//                //then
+//                assertEquals(expectedResult, result)
+////            }
+//        }
+//    }
+//
+//    @Test
+//    fun testGetHighestDifference_returnsCorrect_pricePerSquareMeterToBuy_BetweenInCityCenter_OutCityCenter() {
+//        val cities = listOf(
+//            "Abuja",
+//            "New York",
+//            "Logos",
+//        )
+//        var expectedResult: List<String>
+////        for (i in cities.indices){
+//           if (cities.isEmpty()) {
+//               emptyList<String>()
+//           } else {
+//               expectedResult = listOf(cities[0])
+//               //when
+//               val result = rating.execute_DifferentPayRentingBetween3BedRoom_IntCityCenter_OutCitycenter(1)
+//               //then
+//               assertEquals(expectedResult, result)
+////           }
+//       }
+//    }
 }
 
 
