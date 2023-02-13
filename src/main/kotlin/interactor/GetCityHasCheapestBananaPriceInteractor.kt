2c99e4b36a7f02e2ad2d978eadaf7126fefcf5a1
AbdAlKareem M. Alaraj <kareem.m.alaraj@gmail.com>
2023-02-13 10:53:07 +0200

    create class to get city has the cheapest banana price

---
+++ src/main/kotlin/interactor/GetCityHasCheapestBananaPriceInteractor.kt
@@ -0,0 +1,11 @@
+package interactor
+
+class GetCityHasCheapestBananaPriceInteractor ( vararg cheapestBanana: CostOfLivingDataSource ) {
+/*
+    - A monkey lover guy who has a lot of monkeys to care about them,
+      he has a group of city that he plan to live there,
+      write a function that take vararg's cities
+      and return a sorted list of cities names sorted by cheapest banana prices.
+ */
+
+}
