6cdb6dfe12d61187112c5317f369ccb172811d4c
walaa244 <walaaesmail91@gmail.com>
2023-02-14 00:54:11 +0200

    create the class

---
+++ src/main/kotlin/interactor/GetTopCitiesFasterToBuyApartmentAndTheNumberOfYearToBuyIt.kt
@@ -0,0 +1,8 @@
+package interactor
+
+class GetTopCitiesFasterToBuyApartmentAndTheNumberOfYearToBuyIt (private val dataSource: CostOfLivingDataSource){
+    fun execute(SalaryOfFullTime:Int):List<Pair<String,Int>>{
+        return emptyList()
+    }
+
+}
