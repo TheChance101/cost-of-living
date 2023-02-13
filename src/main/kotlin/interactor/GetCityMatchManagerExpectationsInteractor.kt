b35a5c7e2a0ec672d6eef2a8eac7ea7179005a4c
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-02-13 13:01:41 +0300

    solve conflicts

--- src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
@@ -1,28 +1,17 @@
 package interactor
 
+import dataSource.utils.NorthAmericaCountries
 import model.CityEntity
 
 class GetCityMatchManagerExpectationsInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    private val northAmericaCountries: List<String> =
-        listOf(
-            "United States",
-            "Guatemala",
-            "Belize",
-            "El Salvador",
-            "Honduras",
-            "Nicaragua",
-            "Costa Rica",
-            "Panama",
-            "Canada",
-            "Mexico"
-        )
+    private fun getOnlyNorthAmericaCountries(city: CityEntity): Boolean = city.country in NorthAmericaCountries.list
 
     fun execute(): String {
         return dataSource.getAllCitiesData()
-            .filter { it.country in northAmericaCountries }.first().cityName
+            .filter(::getOnlyNorthAmericaCountries).first().country
     }
 
 }
