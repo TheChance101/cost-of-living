856fcc3d43b109d3515cbcc0852c273c89bd60c3
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-02-13 06:54:21 +0300

    add required instances for data source

--- src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
@@ -1,10 +1,22 @@
 package interactor
 
+import dataSource.FakeDataSource
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 
 class GetCityMatchManagerExpectationsInteractorTest {
+
+    private lateinit var interactor: GetCityMatchManagerExpectationsInteractor
+    private lateinit var dataSource: FakeDataSource
+
+    @BeforeEach
+    fun setup() {
+        dataSource = FakeDataSource()
+        interactor = GetCityMatchManagerExpectationsInteractor(dataSource)
+    }
 
     @Test
     fun execute() {
