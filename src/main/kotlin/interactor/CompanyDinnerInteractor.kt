07a577aa3d0f95a54042c1e641a033435f069a14
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-02-14 00:20:48 +0200

    add a helper class as data holder with its own functionality

--- src/main/kotlin/interactor/CompanyDinnerInteractor.kt
+++ src/main/kotlin/interactor/CompanyDinnerInteractor.kt
@@ -1,7 +1,19 @@
 package interactor
 
 import model.CityEntity
+import kotlin.math.abs
 
 class CompanyDinnerInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(boundary: Float = Float.MAX_VALUE): CityEntity? = dataSource.getAllCitiesData().getOrNull(0)
+    class CityMealsDataHolder(val city: CityEntity, vararg mealsPrices: Float?) {
+        private val filteredMeals = mealsPrices.filterNotNull().filterNot { it.isNaN() }
+        private val cheapestMeal = filteredMeals.minOrNull() ?: Float.MAX_VALUE
+        val priciestMeal = filteredMeals.maxOrNull() ?: Float.MIN_VALUE
+        fun minBoundary(price: Float) =
+            filteredMeals.fold(Float.MAX_VALUE) { acc, fl ->
+                val x = abs(fl - price)
+                if (x < acc) x else acc
+            }
+        fun isValid() = cheapestMeal != Float.MAX_VALUE
+    }
 }
