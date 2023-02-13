fafc742d2a00564217e1d366b47feeaa65ac4954
Rehammashal7 <rehammashal7@gmail.com>
2023-02-13 12:34:35 +0200

    implementation the function

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -5,20 +5,18 @@ import model.CityEntity
 class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
-
+//Reham and Zeinab
     fun execute(cities: List<CityEntity>): CityEntity{
-        return dataSource.getAllCitiesData()[0]
+        return if (cities.isEmpty()) throw IllegalArgumentException("Illegal input")
+        else cities.filter(::excludeIsNullAverageMonthlyNetSalaryAfterTaxAndinternet60MbpsOrMoreUnlimitedDataCableAdsl).
+     minByOrNull { it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!!/it.averageMonthlyNetSalaryAfterTax!!}!!
+
+
 
     }
-//        val citiesData = dataSource.getAllCitiesData()
-//        val citiesWithInternetData = citiesData.filter {
-//            it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null &&
-//                    it.averageMonthlyNetSalaryAfterTax != null
-//        }
-//
-//        return citiesWithInternetData.minByOrNull {
-//            (it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!! / it.averageMonthlyNetSalaryAfterTax!!)
-//        }
-//    }
+    private fun excludeIsNullAverageMonthlyNetSalaryAfterTaxAndinternet60MbpsOrMoreUnlimitedDataCableAdsl (city : CityEntity ) : Boolean{
+        return(city.averageMonthlyNetSalaryAfterTax != null &&
+                city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null)
+    }
 
 }
