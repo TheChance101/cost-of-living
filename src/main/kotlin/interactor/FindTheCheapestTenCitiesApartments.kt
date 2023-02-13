e3f14c4698954b469f6bd0635c1fdde38971b939
Ahmed Ghannam <iia7mt@gmail.com>
2023-02-13 11:24:48 +0200

    add 6 test cases

---
+++ src/main/kotlin/interactor/FindTheCheapestTenCitiesApartments.kt
@@ -0,0 +1,11 @@
+package interactor
+
+class FindTheCheapestTenCitiesApartments(
+    private val dataSource: CostOfLivingDataSource
+) {
+
+    fun execute():List<Pair<String,Double>>?{
+        return listOf()
+    }
+
+}
