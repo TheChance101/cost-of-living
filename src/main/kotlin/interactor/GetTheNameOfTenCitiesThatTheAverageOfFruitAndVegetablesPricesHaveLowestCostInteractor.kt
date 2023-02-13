b3cdb869c785c3fc5869ef8c23cb628a92fd589a
Mahmoud Badway <mahmoudbadawy406@gmail.com>
2023-02-13 21:17:26 +0200

    edit  code  for GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor to approved test cases

--- src/main/kotlin/interactor/GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor.kt
+++ src/main/kotlin/interactor/GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor.kt
@@ -1,11 +1,42 @@
 package interactor
 
+import model.CityEntity
+
 class GetTheNameOfTenCitiesThatTheAverageOfFruitAndVegetablesPricesHaveLowestCostInteractor (
   private val dataSource: CostOfLivingDataSource,) {
 
-  fun execute(): List<String>
-  {
-    throw Throwable("Not Implemented yet")
+  fun execute(): List<String> {
+    return dataSource
+      .getAllCitiesData()
+      .filter(::excludeNullSalariesAndFruitAndVegetablesPrices)
+      .sortedBy(::sortTheAverageOfFruitAndVegetablesPricesHaveLowestCost)
+      .take(10)
+      .map { it.cityName }
+  }
+
+
+  private fun excludeNullSalariesAndFruitAndVegetablesPrices(city: CityEntity): Boolean {
+    return city.averageMonthlyNetSalaryAfterTax != null &&
+            city.fruitAndVegetablesPrices.apples1kg != null &&
+            city.fruitAndVegetablesPrices.banana1kg!= null&&
+            city.fruitAndVegetablesPrices.lettuceOneHead!= null &&
+            city.fruitAndVegetablesPrices.onion1kg != null &&
+            city.fruitAndVegetablesPrices.oranges1kg != null&&
+            city.fruitAndVegetablesPrices.potato1kg != null &&
+            city.fruitAndVegetablesPrices.tomato1kg!= null
+
+  }
+  private fun sortTheAverageOfFruitAndVegetablesPricesHaveLowestCost(city: CityEntity):Float{
+
+    return (city.fruitAndVegetablesPrices.apples1kg!! +
+            city.fruitAndVegetablesPrices.banana1kg!! +
+            city.fruitAndVegetablesPrices.lettuceOneHead!! +
+            city.fruitAndVegetablesPrices.onion1kg!! +
+            city.fruitAndVegetablesPrices.oranges1kg!! +
+            city.fruitAndVegetablesPrices.potato1kg!! +
+            city.fruitAndVegetablesPrices.tomato1kg!!) / (2*city.averageMonthlyNetSalaryAfterTax!! )
+
+
   }
 
 
