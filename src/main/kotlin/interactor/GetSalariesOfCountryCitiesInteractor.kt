5460bf28dac273b4061771ea4147ca3dad2fa64b
mhmoudhabib <mhmoudrhabib@gmail.com>
2023-02-13 13:48:26 +0200

    implement feature interactor functions

--- src/main/kotlin/interactor/GetSalariesOfCountryCitiesInteractor.kt
+++ src/main/kotlin/interactor/GetSalariesOfCountryCitiesInteractor.kt
@@ -1,18 +1,28 @@
 package interactor
 
 import model.CityEntity
+import java.lang.Exception
 
 class GetSalariesOfCountryCitiesInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
-    fun execute(countryName: String): Map<String, Float>? {
-        return mapOf()
+    fun execute(countryName: String): Map<String, Float?>? {
+        return dataSource.getAllCitiesData()
+            .filter { countryName.lowercase() == it.country.lowercase() }
+            .run {
+                if (isEmpty())
+                    throw Exception("Wrong Country Name")
+                else
+                    filter(::excludeNullSalariesAndLowQualityData)
+                        .associate { it.cityName to it.averageMonthlyNetSalaryAfterTax }
+                        .ifEmpty { null }
+
+            }
     }
 
-    private fun excludeNullSalariesAndLowQualityData(city: CityEntity):Boolean{
-        return false
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.dataQuality && city.averageMonthlyNetSalaryAfterTax != null
     }
-
 
 
 }
