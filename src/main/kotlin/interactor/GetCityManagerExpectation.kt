3d9f841055c70690717bf510bc8f308e4fb5a975
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-02-13 14:24:30 +0200

    create class logic

--- src/main/kotlin/interactor/GetCityManagerExpectation.kt
+++ src/main/kotlin/interactor/GetCityManagerExpectation.kt
@@ -1,12 +1,36 @@
 package interactor
 
+import model.CityEntity
+
 class GetCityManagerExpectation(
     private val dataSource: CostOfLivingDataSource
 ) {
-    fun execute(): String {
+    fun execute(): String? {
         val data = dataSource.getAllCitiesData()
-
-        return ""
+            .filter(::excludeNullFoodPricesData)
+            .filterAllCitiesByCountry()
+            .sortingWithAveragePrice()
+            .take(1)
+        return if (data.isNotEmpty()) data[0].cityName else null
     }
 
+
+    private fun excludeNullFoodPricesData(city: CityEntity): Boolean {
+        return city.mealsPrices.mealAtMcDonaldSOrEquivalent != null
+                && city.mealsPrices.mealInexpensiveRestaurant != null
+                && city.mealsPrices.mealFor2PeopleMidRangeRestaurant != null
     }
+
+    private fun List<CityEntity>.filterAllCitiesByCountry(): List<CityEntity> {
+        return this.filter {
+            it.country == "USA" || it.country == "Canada" || it.country == "Mexico"
+        }
+    }
+
+    private fun List<CityEntity>.sortingWithAveragePrice(): List<CityEntity> {
+        return this.sortedByDescending {
+            (it.mealsPrices.mealInexpensiveRestaurant?.plus(it.mealsPrices.mealAtMcDonaldSOrEquivalent!!)
+                ?.plus(it.mealsPrices.mealFor2PeopleMidRangeRestaurant!!))?.div(3.0f)
+        }
+    }
+}
