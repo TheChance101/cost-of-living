d2a36f7798a50426ce4adce6702ae77c5429cf7a
Nahla <nahla@email.com>
2023-02-13 20:09:20 +0200

    update GetAverageSalariesInCountryInteractor class

--- src/main/kotlin/interactor/GetAverageSalariesInCountryInteractor.kt
+++ src/main/kotlin/interactor/GetAverageSalariesInCountryInteractor.kt
@@ -1,16 +1,17 @@
 package interactor
 
 import model.CityEntity
+import java.util.*
 
 class GetAverageSalariesInCountryInteractor (
     private val dataSource: CostOfLivingDataSource,) {
-
-        fun execute(countryName: String): List<Pair<String,Float>> {
-            return listOf(Pair("",0.0f))
-        }
-
-         fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
-            return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
-        }
-
+    fun execute(countryName: String): List<Pair<String,Float?>> {
+        return emptyList()
+    }
+    fun excludeLowQualityData(city: CityEntity): Boolean {
+        return city.dataQuality
+    }
+    fun excludeNullSalaries(city: CityEntity): Boolean{
+        return city.averageMonthlyNetSalaryAfterTax != null
+    }
 }
