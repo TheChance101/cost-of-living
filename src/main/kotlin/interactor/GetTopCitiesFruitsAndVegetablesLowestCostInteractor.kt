cb906fdddd8e78ee61833b10050f040d067a34f2
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-13 13:14:53 +0200

    enhance function execute and add some helper functions

--- src/main/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractor.kt
+++ src/main/kotlin/interactor/GetTopCitiesFruitsAndVegetablesLowestCostInteractor.kt
@@ -6,27 +6,26 @@ class GetTopCitiesFruitsAndVegetablesLowestCostInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(limit:Int): List<String> {
+    fun execute(limit: Int): List<String> {
         return dataSource
             .getAllCitiesData()
+            .filter(::excludeNullFruitAndVegetablePricesOrNullSalaries)
+            .sortedBy(::compareFruitsAndVegetablesCostToSalary)
             .take(limit)
             .map { it.cityName }
     }
 
-    fun excludeNullFruitsAndVegetables(city: CityEntity): Boolean {
-        return city.fruitAndVegetablesPrices.banana1kg != null||
-                city.fruitAndVegetablesPrices.apples1kg != null ||
-                city.fruitAndVegetablesPrices.onion1kg != null ||
-                city.fruitAndVegetablesPrices.lettuceOneHead != null ||
-                city.fruitAndVegetablesPrices.oranges1kg != null ||
-                city.fruitAndVegetablesPrices.tomato1kg != null ||
-                city.fruitAndVegetablesPrices.potato1kg != null
+    fun excludeNullFruitAndVegetablePricesOrNullSalaries(city: CityEntity): Boolean {
+        TODO("under testing")
     }
 
-    fun excludeNullSalaries(city: CityEntity):Boolean{
-        return city.averageMonthlyNetSalaryAfterTax != null
+    private fun compareFruitsAndVegetablesCostToSalary(city: CityEntity): Float {
+        TODO("under testing")
+
     }
 
-
+    fun getAverageFruitAndVegetablePricesForCity(city: CityEntity): Float? {
+        TODO("under testing")
+    }
 
 }
