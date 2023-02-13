6f62f455409f7d4224ceb67d6d841360dded4424
ameeramjed <pr.ameer98@gmail.com>
2023-02-13 15:17:33 +0300

    improve code and handel nullability salaries

--- src/main/kotlin/interactor/GetCitiesAverageSalary.kt
+++ src/main/kotlin/interactor/GetCitiesAverageSalary.kt
@@ -1,17 +1,28 @@
 package interactor
 
+import model.CityEntity
+
 class GetCitiesAverageSalary(private val dataSource: CostOfLivingDataSource) {
 
     fun execute(country: String): List<Pair<String, Double>> {
+
         val countryData = dataSource.getAllCitiesData().filter {
-            it.country.equals(country, ignoreCase = true) && it.dataQuality
+            excludeNullSalariesAndLowQualityDataAndCheckCountriesName(it, country)
         }
 
-        return if (countryData.isEmpty()) {
-            throw IllegalArgumentException("Invalid country name.")
+        return if (countryData.isNotEmpty()) {
+            countryData.map {
+                Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!.toDouble())
+            }
         } else {
-            countryData.map { Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!.toDouble()) }
+            throw IllegalArgumentException("Invalid country name.")
         }
     }
+
+    private fun excludeNullSalariesAndLowQualityDataAndCheckCountriesName(city: CityEntity, country: String): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+                && city.country.equals(country, ignoreCase = true)
+    }
+
 
 }
