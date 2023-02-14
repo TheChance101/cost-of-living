8edec29f8a2047afea0d6f02c38455dbddea92a3
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-02-14 17:03:22 +0200

    setup files

---
+++ src/main/kotlin/interactor/GetTheMostSuitableCityForSaving.kt
@@ -0,0 +1,11 @@
+package interactor
+
+import model.CityEntity
+
+class GetTheMostSuitableCityForSaving(
+   private val dataSource: CostOfLivingDataSource
+) {
+    fun execute():CityEntity {
+    throw Exception("")
+    }
+}
---
+++ src/test/kotlin/interactor/GetTheMostSuitableCityForSavingTest.kt
@@ -0,0 +1,24 @@
+package interactor
+
+import dataSource.FakeCsvDataSource
+import dataSource.utils.CsvParser
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetTheMostSuitableCityForSavingTest {
+    lateinit var csvParser : CsvParser
+    lateinit var dataSource: CostOfLivingDataSource
+    lateinit var citiesName: GetCitiesNameHasHighestClothesPriceInteractor
+
+    @BeforeAll
+    fun setup(){
+        csvParser= CsvParser()
+        dataSource  = FakeCsvDataSource(csvParser)
+        citiesName = GetCitiesNameHasHighestClothesPriceInteractor(dataSource)
+    }
+
+}
