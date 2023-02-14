5af48c9b6367c6500d8643fca9f91190db1506ba
Abdallah Mohamed <84878301+abdallahFc@users.noreply.github.com>
2023-02-14 04:04:39 +0200

    update implementation to exclude Null Salaries And Internet

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,9 +6,14 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity{
+    fun execute(): CityEntity?{
         val cities=dataSource
             .getAllCitiesData()
-        return cities.minBy { it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!! / it.averageMonthlyNetSalaryAfterTax!! }
+            .filter(::excludeNullSalariesAndInternet)
+        return cities.minByOrNull { it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!! / it.averageMonthlyNetSalaryAfterTax!! }
+    }
+    private fun excludeNullSalariesAndInternet(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null
+                && city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null
     }
 }
