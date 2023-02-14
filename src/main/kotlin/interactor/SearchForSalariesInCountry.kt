796251e7981605e9a87bd186f302f79969889a6c
zeinab wahdan <zeinabwahdan3677@gmail.com>
2023-02-14 11:55:37 +0200

    add class

--- src/main/kotlin/interactor/SearchForSalariesInCountry.kt
+++ src/main/kotlin/interactor/SearchForSalariesInCountry.kt
@@ -1,23 +1,23 @@
 package interactor
 import model.CityEntity
 
-class SearchForSalariesInCountry (  private val dataSource: CostOfLivingDataSource,){
-    fun execute(limit: Int): List<String> {
-        return dataSource
+
+class SearchForSalariesInCountry (private val dataSource: CostOfLivingDataSource,) {
+
+    fun execute(countryName: String): List<Pair<String, Float>> {
+
+        val listOfCities = dataSource
             .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedBy {it.averageMonthlyNetSalaryAfterTax}
-            .take(limit)
-            .map { it.country }
+            .filter { countryName.lowercase() == it.country.lowercase() && it.dataQuality }
+        if (listOfCities.isNotEmpty()) {
+            var listOfPairs= mutableListOf<Pair<String, Float>>()
+            listOfCities.forEach {
+                listOfPairs.add(Pair(it.cityName, it.averageMonthlyNetSalaryAfterTax!!))
+            }
+            return listOfPairs
+        } else
+            throw Exception("Enter correct country name")
     }
 
-    fun searchForSalary(list:List<CityEntity>): List<Pair<String,Float>> {
-       for(i in 0 until list.size) {
-           if (list[i].isUpper() || list[i].isLower()) {
 
-
-                   return List(list[i],it.averageMonthlyNetSalaryAfterTax)
-
-           }
-       }
 }
