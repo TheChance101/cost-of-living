b571de86f20f41f5d8cc906b9d09dfdb35b3618a
hsalim78 <h.salim.dz@hotmail.com>
2023-02-14 15:12:42 +0100

    applying unit testing

--- src/main/kotlin/interactor/GetTopFiveCitiesForFashionInteractor.kt
+++ src/main/kotlin/interactor/GetTopFiveCitiesForFashionInteractor.kt
@@ -8,12 +8,7 @@ class GetTopFiveCitiesForFashionInteractor (
 ){
 
     fun execute(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedWith(compareBy({it.clothesPrices.onePairOfJeansLevis50oneOrSimilar},{it.clothesPrices.oneSummerDressInAChainStoreZaraHAndM},{it.clothesPrices.onePairOfNikeRunningShoesMidRange},{it.clothesPrices.onePairOfMenLeatherBusinessShoes}))
-            .take(limit)
-            .map { it.cityName }
+        return listOf("")
 
     }
 
--- src/test/kotlin/interactor/GetTopFiveCitiesForFashionInteractorTest.kt
+++ src/test/kotlin/interactor/GetTopFiveCitiesForFashionInteractorTest.kt
@@ -1,26 +1,30 @@
 package interactor
 
+import fakedata.FakeData
 import model.CityEntity
+import org.junit.jupiter.api.Test
 
+import org.junit.jupiter.api.Assertions.*
 
-class GetTopFiveCitiesForFashionInteractor (
-    val dataSource: CostOfLivingDataSource,
-){
+class GetTopFiveCitiesForFashionInteractorTest {
+    private lateinit var dataSource: CostOfLivingDataSource
 
-    fun execute(limit: Int): List<String> {
-        return dataSource
-            .getAllCitiesData()
-            .filter(::excludeNullSalariesAndLowQualityData)
-            .sortedBy { it.cityName }
-            .take(10)
+    private lateinit var fakeData: FakeData
+
+    @Test
+    fun should_returnListOfFiveCities_whenListOfCitiesNotEmpty() {
+
+        // given cities not empty
+        var cities: List<CityEntity> = dataSource.getAllCitiesData().filter { it.averageMonthlyNetSalaryAfterTax != null && it.dataQuality }
+
+        // when find top five cities for fashion
+        var topFiveCites = GetTopFiveCitiesForFashionInteractorTest().dataSource.getAllCitiesData()
             .sortedWith(compareBy({it.clothesPrices.onePairOfJeansLevis50oneOrSimilar},{it.clothesPrices.oneSummerDressInAChainStoreZaraHAndM},{it.clothesPrices.onePairOfNikeRunningShoesMidRange},{it.clothesPrices.onePairOfMenLeatherBusinessShoes}))
-            .take(limit)
+            .take(5)
             .map { it.cityName }
 
-    }
+        // then check result
+        assertEquals(listOf("Accra", "Abuja", "Abu Dhabi", "Aberdeen", "Abidjan"), topFiveCites)
 
-    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
-        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
     }
-
 }
