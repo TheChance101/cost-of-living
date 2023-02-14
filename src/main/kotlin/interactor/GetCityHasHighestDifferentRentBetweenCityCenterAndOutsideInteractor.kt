eb934722449b357389d211141dac5dde9afe91b6
Rehammashal7 <rehammashal7@gmail.com>
2023-02-14 12:42:04 +0200

    implementation of the function

--- src/main/kotlin/interactor/GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor.kt
@@ -8,51 +8,43 @@ class GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
 
-    fun execute(type: TheTypeOfApartments): CityEntity? {
-        return null
-    }
-//        when (type) {
-//            1 -> {
-//                return dataSource.getAllCitiesData()
-//                    .filter(::excludeNullRentOneTypeOneAndLowQualityData)
-//                    .maxByOrNull {
-//                        abs(
-//                            it.realEstatesPrices.apartmentOneBedroomInCityCentre!!
-//                                    - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
-//                        )
-//                    }!!
-//                    .cityName
-//            }
-//
-//            2 -> {
-//                return return dataSource.getAllCitiesData()
-//                    .filter(::excludeNullRentOneTypeTwoAndLowQualityData)
-//                    .maxByOrNull {
-//                        abs(
-//                            it.realEstatesPrices.apartment3BedroomsInCityCentre!! -
-//                                    it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
-//                        )
-//                    }!!
-//                    .cityName
-//            }
-//
-//            else -> return null
-//        }
-//    }
-//    private fun excludeNullRentOneTypeOneAndLowQualityData(city: CityEntity): Boolean {
-//        return city.dataQuality
-//                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
-//                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
-//    }
-//
-//    private fun excludeNullRentOneTypeTwoAndLowQualityData(city: CityEntity): Boolean {
-//        return city.dataQuality
-//                && city.realEstatesPrices.apartment3BedroomsInCityCentre != null
-//                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
-//
-//
-//    }
+    fun execute(type: TheTypeOfApartments): CityEntity {
+        if (type == TheTypeOfApartments.ONE_BED_ROOM) {
+            return dataSource.getAllCitiesData()
+                .filter(::excludeNullRentOneTypeOneAndLowQualityData)
+                .maxByOrNull { abs(it.realEstatesPrices.apartmentOneBedroomInCityCentre!!
+                        - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!) }!!
 
+        } else   {
+            return dataSource.getAllCitiesData()
+                .filter(::excludeNullRentOneTypeTwoAndLowQualityData)
+                .maxByOrNull {
+                    abs(
+                        it.realEstatesPrices.apartment3BedroomsInCityCentre!! -
+                                it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
+                    )
+                }!!
+
+        }
+    }
+
+
+    private fun excludeNullRentOneTypeOneAndLowQualityData(city: CityEntity): Boolean {
+        return city.dataQuality
+                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+    }
+
+
+
+    private fun excludeNullRentOneTypeTwoAndLowQualityData(city: CityEntity): Boolean {
+
+        return city.dataQuality
+                && city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
+
+
+    }
 }
 
 
