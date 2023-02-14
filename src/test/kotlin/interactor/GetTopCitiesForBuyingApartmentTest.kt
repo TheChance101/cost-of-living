34d6ad0ef91fd87572f1bf1be62d7b3275f1cdc9
Mahmoud Elboray <elboray456@gmail.com>
2023-02-14 04:07:37 +0200

    Commit a new test

--- src/test/kotlin/interactor/GetTopCitiesForBuyingApartmentTest.kt
+++ src/test/kotlin/interactor/GetTopCitiesForBuyingApartmentTest.kt
@@ -20,5 +20,18 @@ class GetTopCitiesForBuyingApartmentTest {
         assertFalse(result.any { it.second == null })
     }
 
+    @Test
+    fun `findTop10CitiesFor100MeterApartment filters out duplicated cities`() {
+        // Given
+        val fakeData = GetTopCitiesForBuyingApartment(FakeCostOfLivingDataSource())
+
+        // When
+        val result = fakeData.findTop10CitiesFor100MeterApartment()
+
+        // Then
+        assertEquals(result.toSet().size, result.size)
+    }
+
   
+
 }
