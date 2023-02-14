64702d8c4e5b39974ab71e703a00d5cb241dd896
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-02-14 11:40:01 +0100

    add new test cases to fruit and vegetables

--- src/test/kotlin/interactor/GetAverageFruitAndVegetablesInteractorTest.kt
+++ src/test/kotlin/interactor/GetAverageFruitAndVegetablesInteractorTest.kt
@@ -11,18 +11,23 @@ import org.junit.jupiter.api.TestInstance
 class GetAverageFruitAndVegetablesInteractorTest {
 
     private lateinit var getAverageFruitAndVegetablesInteractor: GetAverageFruitAndVegetablesInteractor
-    private lateinit var fakeData: FakeDataSource
+    private lateinit var fakeData : FakeDataSource
 
     @BeforeAll
     fun setup(){
         fakeData = FakeDataSource()
         getAverageFruitAndVegetablesInteractor = GetAverageFruitAndVegetablesInteractor(fakeData)
-        fakeData.changeDataSource(TestCase.AverageFruitAndVegetables)
     }
 
     @Test
     fun should_ReturnListOf10CitiesNames_When_TheDataSourceIsValid() {
+        // given valid data
+        fakeData.changeDataSource(TestCase.AverageFruitAndVegetables)
+
+        // when check and return cities name
         val result = getAverageFruitAndVegetablesInteractor.execute()
+
+        // then asserted the return and the expected
         val expected = listOf(
             "Christiansburg",
             "Brookdale",
@@ -35,6 +40,48 @@ class GetAverageFruitAndVegetablesInteractorTest {
             "Airway Heights",
             "Ridgecrest"
         )
+        assertEquals(expected,result)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_TheFruitsAndVegetablesAreNull() {
+        // given null data to fruits and vegetables
+        fakeData.changeDataSource(TestCase.InvalidData)
+
+        // when check and return cities name
+        val result = getAverageFruitAndVegetablesInteractor.execute()
+
+        // then asserted the return and the expected
+        val expected = emptyList<String>()
+
+        assertEquals(expected,result)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_TheAverageMonthlyNetSalaryAfterTaxIsNullOrNaN() {
+        // given null data to average monthly net salary after tax
+        fakeData.changeDataSource(TestCase.AverageMonthlyNetSalary)
+
+        // when check and return cities name
+        val result = getAverageFruitAndVegetablesInteractor.execute()
+
+        // then asserted the return and the expected
+        val expected = emptyList<String>()
+
+        assertEquals(expected,result)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_DataSourceIsEmpty() {
+        // given empty data source
+        fakeData.changeDataSource(TestCase.Empty)
+
+        // when check and return cities name
+        val result = getAverageFruitAndVegetablesInteractor.execute()
+
+        // then asserted the return and the expected
+        val expected = emptyList<String>()
+
         assertEquals(expected,result)
     }
 }
