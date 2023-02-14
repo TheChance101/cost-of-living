378f5cb2b18ae3e9a4df49316a50f7ad389b8995
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-14 17:20:32 +0200

    use data source in class GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor and update the function

--- src/test/kotlin/interactor/GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractorTest.kt
+++ src/test/kotlin/interactor/GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractorTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
 import kotlin.test.assertEquals
 
 
@@ -21,19 +22,32 @@ class GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractorTest {
     }
 
     @Test
-    fun returnSizeOfList() {
-        val limit = 10
-        val cities = fakeData.getAllCitiesData()
-        val result = testingData.execute(limit, cities)
-        assertEquals(8, result.size)
+    fun `should return correct data when has a limit with correct data`() {
+        //Given eight countries as a limit
+        val limit = 8
+        // When getting result
+        val result = testingData.execute(limit)
+        // Then return listOf Pair(country name and the average prices for those drinks for this country)
+        val listOfPair = listOf(
+            Pair("Cuba", 2.0f),
+            Pair("Egypt", 2.0f),
+            Pair("Koura", 2.0f),
+            Pair("Lebanon", 2.0f),
+            Pair("Argentina", 2.0f),
+            Pair("Loliput", 2.0f),
+            Pair("Albania", 2.0f),
+            Pair("Syria", 0.82f)
+        )
+        assertEquals(listOfPair, result)
     }
 
     @Test
-    fun throwNullException() {
-        var list: List<CityEntity> = listOf()
-        val noData = org.junit.jupiter.api.function.Executable { testingData.execute(-1, list) }
+    fun ` should throws exception when has a limit less than zero `() {
+        // Given limit less than zero
+        val limit = -1
+        // When no data
+        val noData = Executable { testingData.execute(limit) }
+        // Then should Throw Exception
         assertThrows(Exception::class.java, noData)
     }
-
-
 }
