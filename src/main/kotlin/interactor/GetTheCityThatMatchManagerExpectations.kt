e80010aac5f43fc96f985b3ca8d35c73bb46ab50
Saif Alhaider <saifharith90@gmail.com>
2023-02-14 17:53:20 +0300

    edited the functionality of execute

--- src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectations.kt
+++ src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectations.kt
@@ -7,14 +7,20 @@ import java.lang.Math.floor
 class GetTheCityThatMatchManagerExpectations(private val dataSource: CostOfLivingDataSource) {
     fun execute(): CityEntity {
         val northAmericaCountries = filterByCountryAndNoneNullable(dataSource.getAllCitiesData())
-            .sortedBy { it.mealsPrices.mealFor2PeopleMidRangeRestaurant }
+            .sortedBy {
+                (it.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2)
+                        + it.mealsPrices.mealInexpensiveRestaurant!! + it.mealsPrices.mealAtMcDonaldSOrEquivalent!!) / 3
+            }
         return northAmericaCountries.get(getMedianValue(northAmericaCountries.size))
     }
 
     fun filterByCountryAndNoneNullable(countries: List<CityEntity>): List<CityEntity> =
         countries.filter {
-            (it.country == "United States" || it.country == "Mexico" || it.country == "Canada")
+            it.country in listOf<String>("Canada", "Mexico", "United States")
                     && it.mealsPrices.mealFor2PeopleMidRangeRestaurant != null
+                    && it.mealsPrices.mealInexpensiveRestaurant != null
+                    && it.mealsPrices.mealAtMcDonaldSOrEquivalent != null
+
         }
 
     fun getMedianValue(number: Int): Int = kotlin.math.floor(number / 2.0).toInt()
