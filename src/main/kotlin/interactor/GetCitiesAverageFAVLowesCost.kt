2f42d38af1774a97dc572d2f159ae99444f842e2
youssefahmed55 <joussefahmed2012@gmail.com>
2023-02-14 02:31:29 +0200

    Update GetCitiesAverageFAVLowesCost Class

--- src/main/kotlin/interactor/GetCitiesAverageFAVLowesCost.kt
+++ src/main/kotlin/interactor/GetCitiesAverageFAVLowesCost.kt
@@ -8,9 +8,37 @@ class GetCitiesAverageFAVLowesCost (
 ) {
 
     fun execute(limit: Int): List<String> {
-        return listOf("Cairo","Aswan","Luxor","Alexandria","Sharm el sheikh","Hurghada","Helwan","Maadi","Dahb","Edfu","Damanhur").take(limit)
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullSalariesAndFAVPrices)
+            .sortedByDescending {getAverageFAVPricesCompareToSalaries(it.fruitAndVegetablesPrices,it.averageMonthlyNetSalaryAfterTax!!)}
+            .take(limit)
+            .map { it.cityName }
     }
 
+    private fun getAverageFAVPricesCompareToSalaries(
+        fruitAndVegetablesPrices: FruitAndVegetablesPrices,
+        averageMonthlyNetSalaryAfterTax: Float
+    ) : Float {
+        var count = 0.0f
+        fruitAndVegetablesPrices.apply {
+            if(apples1kg != null){count++}
+            if(banana1kg != null){count++}
+            if(oranges1kg != null){count++}
+            if(tomato1kg != null){count++}
+            if(potato1kg != null){count++}
+            if(onion1kg != null){count++}
+            if(lettuceOneHead != null){count++}
 
+            return averageMonthlyNetSalaryAfterTax - (((apples1kg?:0.0f) + (banana1kg?:0.0f) + (oranges1kg?:0.0f) + (tomato1kg?:0.0f) + (potato1kg?:0.0f) + (onion1kg?:0.0f) + (lettuceOneHead?:0.0f)) / count)
+        }
+    }
+
+    private fun excludeNullSalariesAndFAVPrices(city: CityEntity): Boolean {
+        city.fruitAndVegetablesPrices.apply {
+            return city.averageMonthlyNetSalaryAfterTax != null && apples1kg != null && banana1kg != null && oranges1kg != null && tomato1kg != null && potato1kg != null && onion1kg != null && lettuceOneHead != null
+        }
+
+    }
 
 }
