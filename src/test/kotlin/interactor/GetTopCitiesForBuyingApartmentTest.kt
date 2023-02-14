740a7dc3df18bd5bde60236394da0542de065425
Mahmoud Elboray <elboray456@gmail.com>
2023-02-14 04:05:17 +0200

    The test is already well written and is testing that the findTop10CitiesFor100MeterApartment function, called on an instance of GetTopCitiesForBuyingApartment with a FakeCostOfLivingDataSource, filters out any cities with missing data

---
+++ src/test/kotlin/interactor/GetTopCitiesForBuyingApartmentTest.kt
@@ -0,0 +1,24 @@
+package interactor
+
+import fakedata.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GetTopCitiesForBuyingApartmentTest {
+
+    @Test
+    fun `findTop10CitiesFor100MeterApartment filters out cities with missing data`() {
+        // Given
+        val fakeData = GetTopCitiesForBuyingApartment(FakeCostOfLivingDataSource())
+
+        // When
+        val result = fakeData.findTop10CitiesFor100MeterApartment()
+
+        // Then
+        assertFalse(result.any { it.second == null })
+    }
+
+  
+}
