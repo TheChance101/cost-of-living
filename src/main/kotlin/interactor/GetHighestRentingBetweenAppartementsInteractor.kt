d16e888c0165e40daa23e769101babfd91ec1835
febronia53 <febroniasameh5@gmail.com>
2023-02-14 17:56:26 +0200

    add function implementation

--- src/main/kotlin/interactor/GetHighestRentingBetweenAppartementsInteractor.kt
+++ src/main/kotlin/interactor/GetHighestRentingBetweenAppartementsInteractor.kt
@@ -6,6 +6,28 @@ class GetCityHasHighestRentingBetweenApartmentsInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(): CityEntity {
-        throw Throwable("Not Implemented yet")
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullValuesAndLowQualityData)
+            .sortingWithRentingPrices()
+            .take(1)[0]
     }
+
+    private fun excludeNullValuesAndLowQualityData(city: CityEntity): Boolean {
+        return city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
+                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+                && city.dataQuality
+    }
+
+    private fun List<CityEntity>.sortingWithRentingPrices(): List<CityEntity> {
+        return this.sortedBy {
+            (it.realEstatesPrices.apartment3BedroomsInCityCentre?.plus(it.realEstatesPrices.apartmentOneBedroomInCityCentre!!))
+                ?.div(
+                    it.realEstatesPrices.apartment3BedroomsOutsideOfCentre?.plus(it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!)!!
+                )
+        }
+    }
+
 }
