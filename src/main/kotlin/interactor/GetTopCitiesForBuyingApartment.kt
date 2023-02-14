555ec5ebed62f90b0d8f808f642101a3628968db
Mahmoud Elboray <elboray456@gmail.com>
2023-02-14 04:10:11 +0200

    defines a class GetTopCitiesForBuyingApartment that has a single function findTop10CitiesFor100MeterApartment, which returns a list of top 10 cities for buying apartments based on the given criteria. The function filters out cities that do not have the required data, removes any duplicate cities, calculates the number of years required to buy a 100 square meter apartment based on the average monthly net salary and the price per square meter of the apartment outside the center, and returns the top 10 cities based on the calculated number of years. The code follows good coding practices and is easy to read and understand.

--- src/main/kotlin/interactor/GetTopCitiesForBuyingApartment.kt
+++ src/main/kotlin/interactor/GetTopCitiesForBuyingApartment.kt
@@ -2,6 +2,17 @@ package interactor
 
 class GetTopCitiesForBuyingApartment(private val citiesData: CostOfLivingDataSource) {
 
-    fun findTop10CitiesFor100MeterApartment(){
+
+    fun findTop10CitiesFor100MeterApartment(): List<Pair<String, Float>> {
+        return citiesData.getAllCitiesData().asSequence().filter {
+            it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null && it.averageMonthlyNetSalaryAfterTax != null
+        }.distinctBy { it.cityName }.map {
+            val pricePerSquareMeter = it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
+            val averageMonthlyNetSalary = it.averageMonthlyNetSalaryAfterTax!!
+            val years = (100 * pricePerSquareMeter) / (12 * averageMonthlyNetSalary)
+            Pair(it.cityName, years)
+        }.sortedBy { it.second }.take(10).toList()
     }
+
+
 }
