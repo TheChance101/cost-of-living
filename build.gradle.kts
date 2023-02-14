ee5faa5f27ea86d6a79010fb6af1891f509f0904
Abdallah Sayes <abdallahsayed3355@gmail.com>
2023-02-14 02:02:51 +0200

    modify the function and make it easier

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("com.google.truth:truth:1.1.3")
 }
 
 tasks.test {
--- src/main/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountry.kt
+++ src/main/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountry.kt
@@ -6,17 +6,14 @@ class GetCitiesNamesAndAverageSalariesInCountry (
 
     fun execute(country: String): List<Pair<String, Float>> {
 
-        if (dataSource
-                .getAllCitiesData()
-                .filter { it.country.lowercase() == country.lowercase() && it.dataQuality }
-                .map { it.cityName to it.averageMonthlyNetSalaryAfterTax!! }.isEmpty()
-        )
-            throw Exception("wrong name")
-        else
-            return dataSource
-                .getAllCitiesData()
-                .filter { it.country.lowercase() == country.lowercase() && it.dataQuality }
-                .map { it.cityName to it.averageMonthlyNetSalaryAfterTax!! }
+        val dataSource=dataSource
+            .getAllCitiesData()
+            .filter{it.country.lowercase()==country.lowercase()&&it.dataQuality}
+            .map{it.cityName to it.averageMonthlyNetSalaryAfterTax!!}
+
+        if (dataSource.isEmpty()) throw Exception("wrong name")
+
+        return dataSource
 
     }
 
