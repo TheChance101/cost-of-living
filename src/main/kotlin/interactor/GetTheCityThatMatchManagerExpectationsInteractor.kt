c5cb0fbcda5b9175294b113c05c5700daf4084ae
Saif Alhaider <saifharith90@gmail.com>
2023-02-14 18:05:09 +0300

    used constants in filterByCountryAndNoneNullable function

--- src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetTheCityThatMatchManagerExpectationsInteractor.kt
@@ -8,15 +8,25 @@ import interactor.util.Constants.UNITED_STATES
 class GetTheCityThatMatchManagerExpectationsInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(): CityEntity {
         val northAmericaCountries = filterByCountryAndNoneNullable(dataSource.getAllCitiesData())
-            .sortedBy { it.mealsPrices.mealFor2PeopleMidRangeRestaurant }
-        return northAmericaCountries[getMedianValue(northAmericaCountries.size)]
+            .sortedBy {
+                (it.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2)
+                        + it.mealsPrices.mealInexpensiveRestaurant!! + it.mealsPrices.mealAtMcDonaldSOrEquivalent!!) / 3
+            }
+        return northAmericaCountries.get(getMedianValue(northAmericaCountries.size))
     }
 
     fun filterByCountryAndNoneNullable(countries: List<CityEntity>): List<CityEntity> =
         countries.filter {
-            (it.country == UNITED_STATES || it.country == MEXICO || it.country == CANADA)
+            it.country in listOf<String>(UNITED_STATES, MEXICO, CANADA)
                     && it.mealsPrices.mealFor2PeopleMidRangeRestaurant != null
+                    && it.mealsPrices.mealInexpensiveRestaurant != null
+                    && it.mealsPrices.mealAtMcDonaldSOrEquivalent != null
+
         }
 
     fun getMedianValue(number: Int): Int = kotlin.math.floor(number / 2.0).toInt()
 }
+
+
+
+
