6d630ed580d41750c8c14ceb0762627f0fb3804c
Abdurrahman-Salah-ad-Din <abdosalahas.01128@gmail.com>
2023-02-16 02:44:01 +0200

    fix and enhance the execute method. move a function to be inside the class. remove the documentation. organize the code shape.

--- src/main/kotlin/interactor/GetFastest10CitiesToBuyAPTInteractor.kt
+++ src/main/kotlin/interactor/GetFastest10CitiesToBuyAPTInteractor.kt
@@ -5,48 +5,32 @@ import model.CityEntity
 class GetFastest10CitiesToBuyAPTInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
-    fun execute(): List<Pair<String, Double>> {
+    fun execute(limit: Int): List<Pair<String, Double>> {
+        if (limit < 1) throw IllegalArgumentException("Invalid Argument!")
         return dataSource.getAllCitiesData()
             .filter { excludeInvalidSalaries(it) && excludeInvalidApartmentPrice(it) && excludeLowQualityData(it) }
             .map {
                 val numberOfYearsToBuyAPT = calculateNumberOfYearsToBuyAPT(it)
                 Pair(it.cityName, numberOfYearsToBuyAPT)
             }
-            .take(10)
+            .take(limit)
     }
 
 
-    /**
-     * Returns `true` if the value of `averageMonthlyNetSalaryAfterTax` is valid.
-     * @param cityEntity represents a city.
-     * @return  if the salary is not equal to `null` and positive value.
-     * @author Abdurrahman Salah ad-Din
-     */
     fun excludeInvalidSalaries(cityEntity: CityEntity) =
-        cityEntity.averageMonthlyNetSalaryAfterTax != null && cityEntity.averageMonthlyNetSalaryAfterTax > 0
+        cityEntity.averageMonthlyNetSalaryAfterTax != null &&
+                cityEntity.averageMonthlyNetSalaryAfterTax > 0
 
-
-    /**
-     * Returns `true` for high quality data and `false` for low quality data.
-     * @param cityEntity represents a city.
-     * @return the value of `DataQuality`.
-     * @author Abdurrahman Salah ad-Din
-     */
     fun excludeLowQualityData(cityEntity: CityEntity) = cityEntity.dataQuality
-    /**
-     * Returns `true` if the value of `pricePerSquareMeterToBuyApartmentOutsideOfCentre` is valid.
-     * @param cityEntity represents a city.
-     * @return  if the `pricePerSquareMeterToBuyApartmentOutsideOfCentre` is not equal to `null` and
-     * positive value.
-     * @author Abdurrahman Salah ad-Din
-     */
+
     fun excludeInvalidApartmentPrice(cityEntity: CityEntity) =
-        cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null && cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre > 0
-}
+        cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&
+                cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre > 0
 
-
-fun calculateNumberOfYearsToBuyAPT(it: CityEntity): Double {
-    val priceForPer100SquareMeterToBuyApartmentOutsideOfCentre =
-        100.0 * it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
-    return priceForPer100SquareMeterToBuyApartmentOutsideOfCentre / it.averageMonthlyNetSalaryAfterTax!!
+    fun calculateNumberOfYearsToBuyAPT(it: CityEntity): Double {
+        val priceToBuy100MeterApartmentOutsideOfCentre =
+            100.0 * it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
+        val averageYearlyNetSalaryAfterTax = it.averageMonthlyNetSalaryAfterTax!! * 12
+        return priceToBuy100MeterApartmentOutsideOfCentre / averageYearlyNetSalaryAfterTax
+    }
 }
