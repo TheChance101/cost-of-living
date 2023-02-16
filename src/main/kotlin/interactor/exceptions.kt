1a7f0bec25919cd722c79bdb806cef6bf760bd85
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2023-02-17 00:34:01 +0200

    add custom exceptions

---
+++ src/main/kotlin/interactor/exceptions.kt
@@ -0,0 +1,6 @@
+package interactor
+
+class InvalidMetersException(message: String) : Exception(message)
+class InvalidLimitException(message: String) : Exception(message)
+
+//Define your exception here
