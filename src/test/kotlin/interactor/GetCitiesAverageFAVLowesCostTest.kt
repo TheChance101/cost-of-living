d356a0f394ccbdf93d42b05c7a473674c50e38fd
youssefahmed55 <joussefahmed2012@gmail.com>
2023-02-17 19:08:00 +0200

    Update GetCitiesAverageFAVLowesCostTest : Add More Tests

--- src/test/kotlin/interactor/GetCitiesAverageFAVLowesCostTest.kt
+++ src/test/kotlin/interactor/GetCitiesAverageFAVLowesCostTest.kt
@@ -1,6 +1,6 @@
 package interactor
 
-import fakedata.FakeDataGetCitiesAverageFAVLowesCost
+import fakedata.*
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -10,19 +10,56 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCitiesAverageFAVLowesCostTest {
 
-    private lateinit var fakeNormalData: FakeDataGetCitiesAverageFAVLowesCost
+    private lateinit var fakeDataGetCitiesAverageFAVLowesCostThatCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices: FakeDataGetCitiesAverageFAVLowesCostThatCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices
+    private lateinit var fakeDataGetCitiesAverageFAVLowesCostThatSomeCitiesHaveNullSalariesOrNullFruitsAndVegetablesPrices: FakeDataGetCitiesAverageFAVLowesCostThatSomeCitiesHaveNullSalariesOrNullFruitsAndVegetablesPrices
+    private lateinit var fakeDataAllCitiesHaveNullSalaryCase: FakeDataAllCitiesHaveNullSalaryCase
+    private lateinit var fakeDataAllCitiesHaveNullPricesOfFruitsAndVegetables: FakeDataAllCitiesHaveNullPricesOfFruitsAndVegetables
     @BeforeAll
     fun setUp() {
-        fakeNormalData = FakeDataGetCitiesAverageFAVLowesCost()
+        fakeDataGetCitiesAverageFAVLowesCostThatCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices = FakeDataGetCitiesAverageFAVLowesCostThatCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices()
+        fakeDataGetCitiesAverageFAVLowesCostThatSomeCitiesHaveNullSalariesOrNullFruitsAndVegetablesPrices = FakeDataGetCitiesAverageFAVLowesCostThatSomeCitiesHaveNullSalariesOrNullFruitsAndVegetablesPrices()
+        fakeDataAllCitiesHaveNullSalaryCase = FakeDataAllCitiesHaveNullSalaryCase()
+        fakeDataAllCitiesHaveNullPricesOfFruitsAndVegetables =  FakeDataAllCitiesHaveNullPricesOfFruitsAndVegetables()
     }
     @Test
-    fun should_ReturnTenCitiesNames_When_GetAverageOfFruitsAndVegetablesLowestCost() {
-        //Given FakeDataOfCityEntities
-        val fakeDataOfCityEntities = fakeNormalData
+    fun should_ReturnAtLeastTenCitiesNames_When_GetCitiesNamesThatAverageOfFruitsAndVegetablesHaveLowestCostAndAllCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices() {
+        //Given fakeDataOfCityEntities
+        val fakeDataOfCityEntities = fakeDataGetCitiesAverageFAVLowesCostThatCitiesHaveNotNullSalariesOrNullFruitsAndVegetablesPrices
         //When execute to get Cities Names That Average Fruits And Vegetables Lowest Cost
         val citiesNames = GetCitiesAverageFAVLowesCost(fakeDataOfCityEntities).execute(10)
         //Then Get At Least 10 Names Of Cities
-        assertEquals(listOf("Dendera","Deir Mawas","Dairut","Dekernes","Dar El Salam","Daraw","Dishna","Cairo","Dahab","Damanhur"),citiesNames)
-
+        assertEquals(listOf("Dendera","Deir Mawas","Dairut","Dekernes","Dar El Salam","Daraw","Dishna","Cairo","Dahab","Desouk"),citiesNames)
     }
+
+    @Test
+    fun should_ReturnAtLeastTenCitiesNames_When_GetCitiesNamesThatAverageOfFruitsAndVegetablesHaveLowestCostAndSomeOfCitiesHaveNullSalariesAndNullFruitsAndVegetablesPrices() {
+        //Given FakeDataOfCityEntities
+        val fakeDataOfCityEntities = fakeDataGetCitiesAverageFAVLowesCostThatSomeCitiesHaveNullSalariesOrNullFruitsAndVegetablesPrices
+        //When execute to get Cities Names That Average Fruits And Vegetables Lowest Cost
+        val citiesNames = GetCitiesAverageFAVLowesCost(fakeDataOfCityEntities).execute(10)
+        //Then Get At Least 10 Names Of Cities
+        assertEquals(listOf("Edfu","Dishna","Dar El Salam","Dairut","Dahab","Desouk","Cairo","Daraw"),citiesNames)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_GetCitiesNamesThatAverageOfFruitsAndVegetablesHaveLowestCostAndAllCitiesHaveNonOrNullSalaries() {
+        //Given fakeDataOfCitiesNullSalary
+        val fakeDataOfCitiesNullSalary = fakeDataAllCitiesHaveNullSalaryCase
+        //When execute to get Cities Names That Average Fruits And Vegetables Lowest Cost
+        val citiesNames = GetCitiesAverageFAVLowesCost(fakeDataOfCitiesNullSalary).execute(10)
+        //Then Get EmptyList Of Cities
+        assertEquals(emptyList<String>(),citiesNames)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_GetCitiesNamesThatAverageOfFruitsAndVegetablesHaveLowestCostAndPricesOfFruitsAndVegetablesOfAllCitiesAreNull() {
+        //Given fakeDataAllCitiesHaveNullPricesOfFAV
+        val fakeDataAllCitiesHaveNullPricesOfFAV = fakeDataAllCitiesHaveNullPricesOfFruitsAndVegetables
+        //When execute to get Cities Names That Average Fruits And Vegetables Lowest Cost
+        val citiesNames = GetCitiesAverageFAVLowesCost(fakeDataAllCitiesHaveNullPricesOfFAV).execute(10)
+        //Then Get EmptyList Of Cities
+        assertEquals(emptyList<String>(),citiesNames)
+    }
+
+
 }
