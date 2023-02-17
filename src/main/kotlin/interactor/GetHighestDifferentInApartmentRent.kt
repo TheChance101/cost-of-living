3c5152a3829b11d155cad17c0d09bf6ec67ad98f
jalal cheff <jlalalmyahy571@gmail.com>
2023-02-18 02:13:29 +0300

    delete the condition on type of bedrooms apartments

--- src/main/kotlin/interactor/GetHighestDifferentInApartmentRent.kt
+++ src/main/kotlin/interactor/GetHighestDifferentInApartmentRent.kt
@@ -1,54 +1,36 @@
 package interactor
-
 import model.CityEntity
+import utils.isNotNull
 
-class GetHighestDifferentInApartmentRent (  private val dataSource: CostOfLivingDataSource,
-) {
+class GetHighestDifferentInApartmentRent (
+    private val dataSource: CostOfLivingDataSource,
+)
+{
 
-    fun execute(apartmentOneBedroom: Boolean, apartment3Bedrooms: Boolean, limit: Int): String? {
-        return if (dataSource.getAllCitiesData().isNotEmpty())
+    fun execute(limit: Int): CityEntity {
+        return if (limit==1)
             dataSource.getAllCitiesData()
                 .filter(::excludeNullApartmentBedroom)
-                .sortedByDescending { it.findHighestDifferentInCitiesRent(apartmentOneBedroom, apartment3Bedrooms) }
-                .takeIf { apartment3Bedrooms || apartmentOneBedroom }
-                ?.take(limit)
-                ?.map { it.cityName }?.get(0)
-        else null
+                .sortedByDescending { it.findHighestDifferentInCitiesRent() }
+                .take(limit)
+                .first()
+        else throw InvalidLimitException("invalid limit value")
     }
 
     private fun excludeNullApartmentBedroom(city: CityEntity): Boolean {
-        return city.realEstatesPrices.apartment3BedroomsInCityCentre != null
-                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
-                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
-                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+        return city.realEstatesPrices.apartment3BedroomsInCityCentre.isNotNull()
+                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre.isNotNull()
+                && city.realEstatesPrices.apartmentOneBedroomInCityCentre.isNotNull()
+                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre.isNotNull()
                 && city.dataQuality
     }
 
-    private fun CityEntity.findHighestDifferentInCitiesRent(
-        apartmentOneBedroom: Boolean,
-        apartment3Bedrooms: Boolean
-    ): Float {
-        if (apartmentOneBedroom && !apartment3Bedrooms)
-            return kotlin.math.abs(
-                realEstatesPrices.apartmentOneBedroomInCityCentre!!
-                        - realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
-            )
-        else if (apartment3Bedrooms && !apartmentOneBedroom)
-            return kotlin.math.abs(
+    private fun CityEntity.findHighestDifferentInCitiesRent()=kotlin.math.abs(
+        realEstatesPrices.apartmentOneBedroomInCityCentre!!
+                - realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
+    )+
+            kotlin.math.abs(
                 realEstatesPrices.apartment3BedroomsInCityCentre!!
                         - realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
             )
-        else if (apartment3Bedrooms && apartmentOneBedroom) (
-                return kotlin.math.abs(
-                    realEstatesPrices.apartmentOneBedroomInCityCentre!!
-                            - realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!
-                ) +
-                        kotlin.math.abs(
-                            realEstatesPrices.apartment3BedroomsInCityCentre!!
-                                    - realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
-                        )
-
-                )
-        else return 0f
-    }
 }
