55a040b5aa62dbe9359a2f16d094093874ccd2ff
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-02-17 23:47:57 +0200

    add review notes on the tasks Sort Cities in Ascending Banana Price

--- src/main/kotlin/interactor/GetCitiesNamesSortedCheapestBananaPrices.kt
+++ src/main/kotlin/interactor/GetCitiesNamesSortedCheapestBananaPrices.kt
@@ -1,18 +1,20 @@
 package interactor
 
 import model.CityEntity
+import utils.isNotNull
 
 class GetCitiesNamesSortedCheapestBananaPrices {
 
     fun execute(vararg cities:CityEntity):List<String>{
         return cities
-            .filter(::excludeNullCityAndLowQualityData)
+            .filter(::excludeNullBananaPricesAndLowQualityData)
+            .let { it.ifEmpty { throw NoReturnedDataException("The list of cities you entered is empty") } }
             .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
             .map { it.cityName }
     }
     
-    private fun excludeNullCityAndLowQualityData(city: CityEntity): Boolean {
-        return city.fruitAndVegetablesPrices.banana1kg != null && city.dataQuality
+    private fun excludeNullBananaPricesAndLowQualityData(city: CityEntity): Boolean {
+        return city.fruitAndVegetablesPrices.banana1kg.isNotNull() && city.dataQuality
     }
 
 }
--- src/test/kotlin/interactor/GetCitiesNamesSortedCheapestBananaPricesTest.kt
+++ src/test/kotlin/interactor/GetCitiesNamesSortedCheapestBananaPricesTest.kt
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCitiesNamesSortedCheapestBananaPricesTest {
     private lateinit var citiesSortedCheapestBananaPrices:GetCitiesNamesSortedCheapestBananaPrices
-    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var fakeDataSource:CostOfLivingDataSource
 
     @BeforeEach
     fun setup(){
@@ -19,19 +20,8 @@ class GetCitiesNamesSortedCheapestBananaPricesTest {
     }
 
     @Test
-    fun `should return empty list when isn't given data`(){
-        //given empty data
-
-        //When execute the use case
-        val actual=citiesSortedCheapestBananaPrices.execute()
-
-        //then
-        assertTrue(actual.isEmpty())
-    }
-
-    @Test
     fun `should return correct result when price city banana isn't null And high Quality Data`(){
-       //given cities to be sorted
+        //given cities to be sorted
         val cities = arrayOf(
             fakeDataSource.getAllCitiesData()[14],
             fakeDataSource.getAllCitiesData()[15],
@@ -42,30 +32,15 @@ class GetCitiesNamesSortedCheapestBananaPricesTest {
 
         //When execute the use case
         val actual=citiesSortedCheapestBananaPrices.execute(*cities)
-
-        //then
-        val expecte= listOf(
+        val expected= listOf(
             fakeDataSource.getAllCitiesData()[18].cityName,
             fakeDataSource.getAllCitiesData()[19].cityName,
             fakeDataSource.getAllCitiesData()[17].cityName,
             fakeDataSource.getAllCitiesData()[15].cityName,
             fakeDataSource.getAllCitiesData()[16].cityName,
             fakeDataSource.getAllCitiesData()[14].cityName)
-        assertEquals(expecte,actual)
-    }
-
-    @Test
-    fun `should return empty list when city banana price is null and low quality data`(){
-        //given cities to be sorted
-        val cities = arrayOf(
-            fakeDataSource.getAllCitiesData()[11],
-            fakeDataSource.getAllCitiesData()[12])
-
-        //When execute the use case
-        val result=citiesSortedCheapestBananaPrices.execute(*cities)
-
         //then
-        assertTrue(result.isEmpty())
+        assertEquals(expected,actual)
     }
 
     @Test
@@ -79,19 +54,19 @@ class GetCitiesNamesSortedCheapestBananaPricesTest {
             fakeDataSource.getAllCitiesData()[13])
 
         //When execute the use case
-        val result=citiesSortedCheapestBananaPrices.execute(*cities)
-
-        //then
-        val valueExpected= listOf(
+        val actual=citiesSortedCheapestBananaPrices.execute(*cities)
+        val expected= listOf(
             fakeDataSource.getAllCitiesData()[13].cityName,
             fakeDataSource.getAllCitiesData()[4].cityName,
             fakeDataSource.getAllCitiesData()[10].cityName)
-        assertEquals(valueExpected,result)
+
+        //then
+        assertEquals(expected,actual)
     }
 
     @Test
     fun `should return correct result Exclude low quality data when data quality false`(){
-        //Given cities to be sorted
+        //given cities to be sorted
         val cities = arrayOf(
             fakeDataSource.getAllCitiesData()[0],
             fakeDataSource.getAllCitiesData()[13],
@@ -99,14 +74,37 @@ class GetCitiesNamesSortedCheapestBananaPricesTest {
             fakeDataSource.getAllCitiesData()[19],
             fakeDataSource.getAllCitiesData()[5],
             fakeDataSource.getAllCitiesData()[10])
-        //When execute the use case
-        val result=citiesSortedCheapestBananaPrices.execute(*cities)
 
-        //then
-        val valueExpected= listOf(
+        //When execute the use case
+        val actual=citiesSortedCheapestBananaPrices.execute(*cities)
+        val expected= listOf(
             fakeDataSource.getAllCitiesData()[19].cityName,
             fakeDataSource.getAllCitiesData()[13].cityName,
             fakeDataSource.getAllCitiesData()[10].cityName)
-        assertEquals(valueExpected,result)
+
+        //then
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun `should throw exception when city banana price is null and low quality data`(){
+        //given cities null banana price and low quality data
+        val cities = arrayOf(
+            fakeDataSource.getAllCitiesData()[11],
+            fakeDataSource.getAllCitiesData()[12])
+
+        //When execute the use case
+        val actual=Executable{ citiesSortedCheapestBananaPrices.execute(*cities) }
+
+        //then
+        assertThrows(NoReturnedDataException::class.java,actual)
+    }
+
+    @Test
+    fun `should throw exception when isn't given data`(){
+        //When execute the use case
+        val actual=Executable{ citiesSortedCheapestBananaPrices.execute() }
+        //then
+        assertThrows(NoReturnedDataException::class.java,actual)
     }
 }
