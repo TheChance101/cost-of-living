9302481183b0c4a2003f65b18acd0622e2d9b079
Mahmoud Elsayed <mahmoudelsayedessa@gamil.com>
2023-02-17 16:57:58 +0200

    add scope function and concise the code

--- src/main/kotlin/interactor/GetFastestCitiesToBuyApartmentInteractor.kt
+++ src/main/kotlin/interactor/GetFastestCitiesToBuyApartmentInteractor.kt
@@ -5,30 +5,27 @@ import model.CityEntity
 class GetFastestCitiesToBuyApartmentInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
-    fun execute(limit: Int): List<Pair<String, Double>> {
-        if (limit < 1) throw IllegalArgumentException("Invalid Argument!")
-        return dataSource.getAllCitiesData()
-            .filter { excludeInvalidSalaries(it) && excludeInvalidApartmentPrice(it) && excludeLowQualityData(it) }
-            .map { Pair(it.cityName, calculateNumberOfYearsToBuyAPT(it)) }
-            .sortedBy { it.second }
-            .take(limit)
-    }
+    fun execute(limit: Int) = dataSource.getAllCitiesData()
+        .also { require(limit >= 1) { "Invalid Argument!" } }
+        .filter { excludeInvalidSalaries(it) && excludeInvalidApartmentPrice(it) && excludeLowQualityData(it) }
+        .map { it.cityName to calculateNumberOfYearsToBuyAPT(it) }
+        .sortedBy { it.second }
+        .take(limit)
+}
 
+fun excludeInvalidSalaries(cityEntity: CityEntity) =
+    cityEntity.averageMonthlyNetSalaryAfterTax != null &&
+            cityEntity.averageMonthlyNetSalaryAfterTax > 0
 
-    fun excludeInvalidSalaries(cityEntity: CityEntity) =
-        cityEntity.averageMonthlyNetSalaryAfterTax != null &&
-                cityEntity.averageMonthlyNetSalaryAfterTax > 0
+fun excludeLowQualityData(cityEntity: CityEntity) = cityEntity.dataQuality
 
-    fun excludeLowQualityData(cityEntity: CityEntity) = cityEntity.dataQuality
+fun excludeInvalidApartmentPrice(cityEntity: CityEntity) =
+    cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&
+            cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre > 0
 
-    fun excludeInvalidApartmentPrice(cityEntity: CityEntity) =
-        cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&
-                cityEntity.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre > 0
-
-    fun calculateNumberOfYearsToBuyAPT(it: CityEntity): Double {
-        val priceToBuy100MeterApartmentOutsideOfCentre =
-            100.0 * it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
-        val averageYearlyNetSalaryAfterTax = it.averageMonthlyNetSalaryAfterTax!! * 12
-        return priceToBuy100MeterApartmentOutsideOfCentre / averageYearlyNetSalaryAfterTax
-    }
+fun calculateNumberOfYearsToBuyAPT(it: CityEntity): Double {
+    val priceToBuy100MeterApartmentOutsideOfCentre =
+        100.0 * it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
+    val averageYearlyNetSalaryAfterTax = it.averageMonthlyNetSalaryAfterTax!! * 12
+    return priceToBuy100MeterApartmentOutsideOfCentre / averageYearlyNetSalaryAfterTax
 }
