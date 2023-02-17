a7d9721e8787c2a1c31ad644a937b5dbd4db4400
Momen Mustafa Al_Hafidh <momen.myasar@gmail.com>
2023-02-17 17:11:41 +0300

    solve zero and negative numbers and large limit problem

--- src/main/kotlin/interactor/GetLowCostFruitVegetableCitiesWithHighSalariesInteractor.kt
+++ src/main/kotlin/interactor/GetLowCostFruitVegetableCitiesWithHighSalariesInteractor.kt
@@ -4,7 +4,6 @@ import model.CityEntity
 import model.FruitAndVegetablesPrices
 
 
-
 class GetLowCostFruitVegetableCitiesWithHighSalariesInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
@@ -13,9 +12,9 @@ class GetLowCostFruitVegetableCitiesWithHighSalariesInteractor(
         return dataSource.getAllCitiesData()
             .filter(::excludeNullSalariesAndNullFruitVegPrices)
             .sortedBy { calculateFruitVegTotalPrice(it.fruitAndVegetablesPrices).div(it.averageMonthlyNetSalaryAfterTax!!) }
-            .takeUnless { limit<=0 || limit > it.count() }!!
-            .take(limit)
-            .map { it.cityName }
+            .takeUnless { limit<=0 || limit > it.count() }
+            ?.take(limit)
+            ?.map { it.cityName } ?: emptyList()
     }
     private fun excludeNullSalariesAndNullFruitVegPrices(city: CityEntity): Boolean {
         return city.averageMonthlyNetSalaryAfterTax != null &&
@@ -24,9 +23,7 @@ class GetLowCostFruitVegetableCitiesWithHighSalariesInteractor(
                     this.onion1kg!= null && this.oranges1kg!= null && this.potato1kg!= null &&
                     this.tomato1kg!= null
                 }
-
     }
-
     private fun calculateFruitVegTotalPrice(fruitAndVegetablesPrices: FruitAndVegetablesPrices): Float =
         with(fruitAndVegetablesPrices){
                 return this.apples1kg!! + this.banana1kg!!+ this.lettuceOneHead!!+ this.onion1kg!!+ this.oranges1kg!!+
