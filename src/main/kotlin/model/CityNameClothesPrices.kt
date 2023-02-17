88e2537368bb464a18e62f55b5d1bc9313674cb7
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-02-17 22:37:04 +0200

    delete the additional class and data class

--- src/main/kotlin/model/CityNameClothesPrices.kt
+++
@@ -1,9 +0,0 @@
-package model
-
-data class CityNameClothesPrices(
-    val cityName: String,
-    val onePairOfJeansLevis50oneOrSimilar: Float?,
-    val oneSummerDressInAChainStoreZaraHAndM: Float?,
-    val onePairOfNikeRunningShoesMidRange: Float?,
-    val onePairOfMenLeatherBusinessShoes: Float?,
-)
