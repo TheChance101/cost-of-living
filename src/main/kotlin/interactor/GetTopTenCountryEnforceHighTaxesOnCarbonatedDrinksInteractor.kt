d16d19b96dbb9e45a8820dd297a56554c89746e2
Zeinab Abdien <suzanabdien15@gmail.com>
2023-02-17 22:23:01 +0200

    add asSequence to improve performance

--- src/main/kotlin/interactor/GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor.kt
+++ src/main/kotlin/interactor/GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor.kt
@@ -5,11 +5,13 @@ import model.CityEntity
 class GetTopTenCountryEnforceHighTaxesOnCarbonatedDrinksInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(limit: Int): List<Pair<String, Float?>> {
         return dataSource.getAllCitiesData()
+            .asSequence()
             .filter(::isLegalData)
             .sortedByDescending { it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants }
             .distinctBy { it.country }
             .take(limit)
             .map { (it.country to it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants) }
+            .toList()
     }
 
     private fun isLegalData(city: CityEntity): Boolean {
