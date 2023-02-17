b52585b322904473ef6f45919b358b1034da988f
Abdallah Sayes <abdallahsayed3355@gmail.com>
2023-02-17 17:43:57 +0200

    change the name of val in the function

--- src/main/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountry.kt
+++ src/main/kotlin/interactor/GetCitiesNamesAndAverageSalariesInCountry.kt
@@ -6,14 +6,14 @@ class GetCitiesNamesAndAverageSalariesInCountry (
 
     fun execute(country: String): List<Pair<String, Float>> {
 
-        val dataSource=dataSource
+        val list=dataSource
             .getAllCitiesData()
             .filter{it.country.lowercase()==country.lowercase()&&it.dataQuality}
             .map{it.cityName to it.averageMonthlyNetSalaryAfterTax!!}
 
-        if (dataSource.isEmpty()) throw Exception("wrong name")
+        if (list.isEmpty()) throw Exception("wrong name")
 
-        return dataSource
+        return list
 
     }
 
