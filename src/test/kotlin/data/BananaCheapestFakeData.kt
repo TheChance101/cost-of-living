fba621acc23110ecf2c1e350f57c37761b281fb8
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-02-17 12:52:12 +0200

    fix(banana): change argument type

--- src/test/kotlin/data/BananaCheapestFakeData.kt
+++ src/test/kotlin/data/BananaCheapestFakeData.kt
@@ -5,6 +5,79 @@ import model.*
 
 
 class BananaCheapestFakeData : CostOfLivingDataSource {
+    companion object {
+        //region helper functions
+        fun citiesListOf(vararg citiesNames: String) = citiesNames.map(::makeCity).toTypedArray()
+        fun makeCity(cityName: String) =
+            CityEntity(cityName, "Cuba", MealsPrices(
+                    mealInexpensiveRestaurant = 6.5f,
+                    mealFor2PeopleMidRangeRestaurant = 15.0f,
+                    mealAtMcDonaldSOrEquivalent = 5.0f
+                ), DrinksPrices(
+                    cappuccinoRegularInRestaurants = 2.0f,
+                    cokePepsiAThirdOfLiterBottleInRestaurants = 2.0f,
+                    waterAThirdOfLiterBottleInRestaurants = 1.0f,
+                    milkRegularOneLiter = 3.8f,
+                    waterOneAndHalfLiterBottleAtTheMarket = 2.0f
+                ), FruitAndVegetablesPrices(
+                    apples1kg = 11.02f,
+                    banana1kg = 0.44f,
+                    oranges1kg = 1.1f,
+                    tomato1kg = 0.88f,
+                    potato1kg = 1.1f,
+                    onion1kg = 2.2f,
+                    lettuceOneHead = 0.25f
+                ),
+                foodPrices = FoodPrices(
+                    loafOfFreshWhiteBread500g = 1.1f,
+                    riceWhite1kg = 1.1f,
+                    eggsRegular12 = 2.03f,
+                    localCheese1kg = 9.0f,
+                    chickenFillets1kg = 6.61f,
+                    beefRound1kgOrEquivalentBackLegRedMeat = 6.61f
+                ),
+                servicesPrices = ServicesPrices(
+                    basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
+                    oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = 0.1f,
+                    internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
+                    fitnessClubMonthlyFeeForOneAdult = null,
+                    tennisCourtRentOneHourOnWeekend = null,
+                    cinemaInternationalReleaseOneSeat = 1.0f,
+                    preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
+                    internationalPrimarySchoolYearlyForOneChild = null
+                ),
+                clothesPrices = ClothesPrices(
+                    onePairOfJeansLevis50oneOrSimilar = 40.0f,
+                    oneSummerDressInAChainStoreZaraHAndM = 40.0f,
+                    onePairOfNikeRunningShoesMidRange = 60.0f,
+                    onePairOfMenLeatherBusinessShoes = 50.0f
+                ),
+                transportationsPrices = TransportationsPrices(
+                    oneWayTicketLocalTransport = null,
+                    monthlyPassRegularPrice = null,
+                    taxiStartNormalTariff = null,
+                    taxi1kmNormalTariff = 0.7f,
+                    taxi1hourWaitingNormalTariff = null,
+                    gasolineOneLiter = null
+                ),
+                carsPrices = CarsPrices(
+                    volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
+                    toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = null
+                ),
+                realEstatesPrices = RealEstatesPrices(
+                    apartmentOneBedroomInCityCentre = null,
+                    apartmentOneBedroomOutsideOfCentre = null,
+                    apartment3BedroomsInCityCentre = 600.0f,
+                    apartment3BedroomsOutsideOfCentre = null,
+                    pricePerSquareMeterToBuyApartmentInCityCentre = null,
+                    pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
+                ),
+                averageMonthlyNetSalaryAfterTax = 18f,
+                dataQuality = true
+            )
+        // endregion
+    }
+
     override fun getAllCitiesData(): List<CityEntity> {
         return listOf(
             CityEntity(
@@ -221,14 +294,5 @@ class BananaCheapestFakeData : CostOfLivingDataSource {
                 dataQuality = false
             )
         )
-
     }
 }
-
-
-
-
-
-
-
-
--- src/test/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCitiesNamesWithCheapestBananaPricesInteractorTest.kt
@@ -1,37 +1,64 @@
 package interactor
 
-import data.FakeDataSource
-import data.TestCase
+import data.BananaCheapestFakeData
+import data.BananaCheapestFakeData.Companion.citiesListOf
+import data.BananaCheapestFakeData.Companion.makeCity
+import data.EmptyFakeData
+import data.InvalidFakeData
+import model.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     // region init
-    private lateinit var getCitiesNamesWithCheapestBananaPricesInteractor: GetCitiesNamesWithCheapestBananaPricesInteractor
+    private lateinit var getCitiesNamesWithCheapestBananaPrices: GetCitiesNamesWithCheapestBananaPricesInteractor
+    private lateinit var fakeData: BananaCheapestFakeData
 
-    private lateinit var fakeData: FakeDataSource
-
-    @BeforeAll
+    @BeforeEach
     fun setup() {
-        fakeData = FakeDataSource()
-        getCitiesNamesWithCheapestBananaPricesInteractor =
-            GetCitiesNamesWithCheapestBananaPricesInteractor(fakeData)
-        fakeData.changeDataSource(TestCase.BananaCheapest)
-
+        fakeData = BananaCheapestFakeData()
+        getCitiesNamesWithCheapestBananaPrices = GetCitiesNamesWithCheapestBananaPricesInteractor(fakeData)
     }
     // endregion
 
     // region one city cases
     @Test
-    fun should_ReturnOneCity_When_TheInputIsOneCityIncluded() {
+    fun should_ReturnEmptyList_When_TheDataIsInvalid() {
+        getCitiesNamesWithCheapestBananaPrices = GetCitiesNamesWithCheapestBananaPricesInteractor(InvalidFakeData())
+
         // given one city included
-        val city = "Santiago de Cuba"
+        val city = makeCity("Santiago de Cuba")
 
         // when the output is list of one city
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
+
+        // then check
+        assertEquals(emptyList<String>(), result)
+    }
+    @Test
+    fun should_ReturnEmptyList_When_TheDataIsEmpty() {
+        getCitiesNamesWithCheapestBananaPrices = GetCitiesNamesWithCheapestBananaPricesInteractor(EmptyFakeData())
+
+        // given one city included
+        val city = makeCity("Santiago de Cuba")
+
+        // when the output is list of one city
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
+
+        // then check
+        assertEquals(emptyList<String>(), result)
+    }
+
+    @Test
+    fun should_ReturnOneCity_When_TheInputIsOneCityIncluded() {
+        // given one city included
+        val city = makeCity("Santiago de Cuba")
+
+        // when the output is list of one city
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
 
         // then check
         assertEquals(listOf("Santiago de Cuba"), result)
@@ -40,10 +67,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnEmptyList_when_TheInputIsOneCityNotIncluded() {
         // given one city is not included
-        val city = "Galaxy"
+        val city = makeCity("Galaxy")
 
         // when the output is empty list of city
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
 
         // then check 
         assertEquals(emptyList<String>(), result)
@@ -52,10 +79,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnOneCity_When_TheInputIsOneCityIncludedInLowercase() {
         // given one city included
-        val city = "santiago de cuba"
+        val city = makeCity("santiago de cuba")
 
         // when the output is list of one city
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
 
         // then check
         assertEquals(listOf("Santiago de Cuba"), result)
@@ -64,10 +91,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnOneCity_When_TheInputIsOneCityIncludedInLowercaseAndMoreOuterSpace() {
         // given one city included
-        val city = " santiago de cuba "
+        val city = makeCity(" santiago de cuba ")
 
         // when the output is list of one city
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor.execute(city)
+        val result = getCitiesNamesWithCheapestBananaPrices(city)
 
         // then check
         assertEquals(listOf("Santiago de Cuba"), result)
@@ -78,11 +105,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCityAllIncluded() {
         // given multi city all of this is included
-        val cities = arrayOf("Santiago de Cuba", "Sancti Spiritus")
+        val cities = citiesListOf("Santiago de Cuba", "Sancti Spiritus")
 
         // when the output is list of sorted cities by Banana Price
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(*cities)
+        val result = getCitiesNamesWithCheapestBananaPrices(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
@@ -91,11 +117,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCityAllIncludedInDifferentCase() {
         // given multi city all of this is included
-        val cities = arrayOf("SaNtiago de cuba", "sAncti spirItus")
+        val cities = citiesListOf("SaNtiago de cuba", "sAncti spirItus")
 
         // when the output is list of sorted cities by Banana Price
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(*cities)
+        val result = getCitiesNamesWithCheapestBananaPrices(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
@@ -104,11 +129,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCitySomeIncluded() {
         // given multi city some of this is included
-        val cities = arrayOf("Santiago de Cuba", "Sancti Spiritus", "Galaxy")
+        val cities = citiesListOf("Santiago de Cuba", "Sancti Spiritus", "Galaxy")
 
         // when the output is list of sorted cities by Banana Price
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(*cities)
+        val result = getCitiesNamesWithCheapestBananaPrices(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
@@ -117,11 +141,10 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnSortedList_When_TheInputIsMultiCitySomeIncludedInDifferentCases() {
         // given multi city some of this is included
-        val cities = arrayOf("santiago de cuba", "Sancti Spiritus", "Galaxy")
+        val cities = citiesListOf("santiago de cuba", "Sancti Spiritus", "Galaxy")
 
         // when the output is list of sorted cities by Banana Price
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(*cities)
+        val result = getCitiesNamesWithCheapestBananaPrices(*cities)
 
         // then check
         assertEquals(listOf("Santiago de Cuba", "Sancti Spiritus"), result)
@@ -130,14 +153,15 @@ internal class GetCitiesNamesWithCheapestBananaPricesInteractorTest {
     @Test
     fun should_ReturnSortedList_when_TheInputIsMultiCityAllNotIncluded() {
         // given multi city all of this is not included
-        val cities = arrayOf("", "Farwala", "Galaxy")
+        val cities = citiesListOf("", "Farwala", "Galaxy")
 
         // when the output is empty list
-        val result = getCitiesNamesWithCheapestBananaPricesInteractor
-            .execute(*cities)
+        val result = getCitiesNamesWithCheapestBananaPrices(*cities)
 
         // then check
         assertEquals(emptyList<String>(), result)
     }
     // endregion
+
+
 }
