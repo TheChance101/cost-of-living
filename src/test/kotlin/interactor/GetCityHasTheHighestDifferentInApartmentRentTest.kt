c97f60c47cf32081bfd7aae947eeeaea22c17ba1
jalal cheff <jlalalmyahy571@gmail.com>
2023-02-18 02:04:45 +0300

    remove unneeded test cases

--- src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
+++ src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
@@ -16,18 +16,6 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
         highestDifferentInApartmentRent =  GetHighestDifferentInApartmentRent(dataSource)
     }
 
-    @Test
-    fun `should return null when give null apartment bedrooms`(){
-        // given null apartment bedrooms
-        val apartmentOneBedroom=false
-        val apartment3Bedrooms=false
-        val limit=1
-        // when check what is the highest different in list with giving emptyList
-        val result = highestDifferentInApartmentRent.execute(apartmentOneBedroom,apartment3Bedrooms,limit)
-        // then check the result
-        Assertions.assertNull(result)
-    }
-
 
     @Test
     fun `should return city has highest different in apartment when given list of cities`(){
@@ -42,31 +30,7 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
         Assertions.assertEquals(cities[16].cityName, result)
     }
 
-    @Test
-    fun `should return highest different city with one bedroom appartment when want list of cities`(){
-        // given list of cityEntity
-        val cities = dataSource.getAllCitiesData()
-        val apartmentOneBedroom=true
-        val apartment3Bedrooms=false
-        val limit=1
-        // when check what is the cities has highest different in apartment rent
-        val result = highestDifferentInApartmentRent.execute(apartmentOneBedroom,apartment3Bedrooms,limit)
-        // then check the result
-        Assertions.assertEquals(cities[4].cityName, result)
-    }
 
-    @Test
-    fun `should return highest different city with 3 bedrooms appartment when want list of cities`(){
-        // given list of cityEntity
-        val cities = dataSource.getAllCitiesData()
-        val apartmentOneBedroom=false
-        val apartment3Bedrooms=true
-        val limit=1
-        // when check what is the cities has highest different in apartment rent
-        val result = highestDifferentInApartmentRent.execute(apartmentOneBedroom,apartment3Bedrooms,limit)
-        // then check the result
-        Assertions.assertEquals(cities[16].cityName, result)
-    }
     @Test
     fun `should return empty list when limit equal to zero`(){
         // given list of cityEntity
