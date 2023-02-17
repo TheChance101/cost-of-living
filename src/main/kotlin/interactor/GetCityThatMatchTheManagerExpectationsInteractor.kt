52deaa0bbafc0ef38d6be3938566cdba9f361302
Sajadio <sjd04969@gmail.com>
2023-02-17 12:52:35 +0300

    improve the readability of the code

--- src/main/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractor.kt
@@ -9,47 +9,33 @@ class GetCityThatMatchTheManagerExpectationsInteractor(
 ) {
 
     fun execute(): CityEntity {
-
-        val listOfCities = dataSource
-            .getAllCitiesData()
+        return dataSource.getAllCitiesData()
             .filterByCountryName()
-
-        return if (listOfCities.isNotEmpty()) {
-                listOfCities
-                .excludeNullPricesOfMeals()
-                .getCityThatMatchExpectations()
-        }
-        else throw Throwable("List of cities is empty")
-
+            .takeIf { it.isNotEmpty() }
+            ?.run {
+                excludeNullPricesOfMeals().getCityThatMatchExpectations()
+            } ?: throw IllegalStateException("List of cities is empty")
     }
 
-    private fun List<CityEntity>.filterByCountryName() = filter { it.country.lowercase() in selectedCountries}
+
+    private fun List<CityEntity>.filterByCountryName() = filter { it.country.lowercase() in selectedCountries }
 
     private fun List<CityEntity>.excludeNullPricesOfMeals() = filter {
         it.mealsPrices.mealFor2PeopleMidRangeRestaurant != null &&
                 it.mealsPrices.mealInexpensiveRestaurant != null &&
                 it.mealsPrices.mealAtMcDonaldSOrEquivalent != null
-        }
+    }
 
+    private fun CityEntity.getSummationPriceOfMeals(): Float {
+        return mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2) +
+                mealsPrices.mealInexpensiveRestaurant!! +
+                mealsPrices.mealAtMcDonaldSOrEquivalent!!
+    }
 
     private fun List<CityEntity>.getCityThatMatchExpectations(): CityEntity {
-        val pricesOfMeals = mutableListOf<Float>()
-        forEach {
-            pricesOfMeals.add(
-                it.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2) +
-                it.mealsPrices.mealInexpensiveRestaurant!!+
-                it.mealsPrices.mealAtMcDonaldSOrEquivalent!!
-            )
-        }
+        val pricesOfMeals = map { it.getSummationPriceOfMeals() }
         val matchedPrice = (pricesOfMeals.maxOf { it } + pricesOfMeals.minOf { it }).div(2)
-        return minByOrNull {
-            abs(
-                matchedPrice - (it.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2) +
-                        it.mealsPrices.mealInexpensiveRestaurant!! +
-                        it.mealsPrices.mealAtMcDonaldSOrEquivalent!!
-                        )
-            )
-        }!!
+        return minByOrNull { abs(matchedPrice - it.getSummationPriceOfMeals()) }!!
     }
 
     companion object {
