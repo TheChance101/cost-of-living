217d06cfbb3ac93251c705fd171f29efb2682968
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2023-02-17 21:01:12 +0200

    add common exception to use for common interactors

--- src/main/kotlin/interactor/exceptions.kt
+++ src/main/kotlin/interactor/exceptions.kt
@@ -4,3 +4,5 @@ class InvalidMetersException(message: String) : Exception(message)
 class InvalidLimitException(message: String) : Exception(message)
 
 //Define your exception here
+class NoReturnedDataException(message: String) : Exception(message)
+class InvalidCountryNameException(message: String) : Exception(message)
