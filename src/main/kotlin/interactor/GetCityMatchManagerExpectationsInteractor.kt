9c456fd29a57bd2a64900287deb0735782378ac3
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-02-17 14:25:49 +0300

    get the highest city restaurant price

--- src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityMatchManagerExpectationsInteractor.kt
@@ -7,34 +7,43 @@ class GetCityMatchManagerExpectationsInteractor(
 ) {
 
     fun execute(): CityEntity {
-        val sortedList = dataSource.getAllCitiesData()
-            .filter { it.country in onlyRequiredCountries }
-            .sortedBy { it.mealsPrices.mealFor2PeopleMidRangeRestaurant }
-        val cityWithMidRangePrice = getCityWithMidRangePrice(sortedList)?.mealsPrices?.mealFor2PeopleMidRangeRestaurant
-        return sortedList
-            .getClosestMealPriceCity(cityWithMidRangePrice ?: 0.0f) ?: dataSource.getAllCitiesData()[0]
+        return dataSource.getAllCitiesData()
+            .filter { it.country in onlyRequiredCountries }.let(::getHighestPrice)!!
+
     }
+    //            .sortedBy { it.mealsPrices.mealFor2PeopleMidRangeRestaurant }
+//        val cityWithMidRangePrice = getCityWithMidRangePrice(sortedList)?.mealsPrices?.mealFor2PeopleMidRangeRestaurant
+//        return sortedList
+//            .getClosestMealPriceCity(cityWithMidRangePrice ?: 0.0f) ?: dataSource.getAllCitiesData()[0]
+
+
+    private fun getHighestPrice(cities: List<CityEntity>) =
+        cities.maxByOrNull {
+            listOfNotNull(
+                it.mealsPrices.mealInexpensiveRestaurant,
+                it.mealsPrices.mealFor2PeopleMidRangeRestaurant?.div(2) ?: 0f,
+                it.mealsPrices.mealAtMcDonaldSOrEquivalent
+            )
+                .maxOrNull() ?: 0f
+        }
 
 
     private fun List<CityEntity>.getClosestMealPriceCity(midRangePrice: Float): CityEntity? =
         minByOrNull { kotlin.math.abs(it.mealsPrices.mealFor2PeopleMidRangeRestaurant ?: (0.0f - midRangePrice)) }
 
-    private fun getCityWithMidRangePrice(cities: List<CityEntity>): CityEntity? =
-        cities.find {
-            it.mealsPrices.mealFor2PeopleMidRangeRestaurant == getMidRangePrice(
-                getLowestPrice(cities),
-                getHighestPrice(cities)
-            )
-        }
+//    private fun getCityWithMidRangePrice(cities: List<CityEntity>): CityEntity? =
+//        cities.find {
+//            it.mealsPrices.mealFor2PeopleMidRangeRestaurant == getMidRangePrice(
+//                getLowestPrice(cities),
+//                getHighestPrice(cities)
+//            )
+//        }
 
     private fun getMidRangePrice(lowestPrice: Float?, highestPrice: Float?) =
         (lowestPrice ?: 0.0f) + ((highestPrice ?: 0.0f) - (lowestPrice ?: 0.0f)) / 2
 
-    private fun getLowestPrice(sortedCities: List<CityEntity>) =
-        sortedCities.firstOrNull()?.mealsPrices?.mealFor2PeopleMidRangeRestaurant
-
-    private fun getHighestPrice(sortedCities: List<CityEntity>) =
-        sortedCities.lastOrNull()?.mealsPrices?.mealFor2PeopleMidRangeRestaurant
+    private fun getLowestPrice(cities: List<CityEntity>) =
+        cities.firstOrNull()?.mealsPrices?.mealFor2PeopleMidRangeRestaurant
 
 
     companion object {
