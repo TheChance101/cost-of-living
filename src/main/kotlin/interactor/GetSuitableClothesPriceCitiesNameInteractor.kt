886d8702945d65da150722cce28d376682b71995
Mohamed Esam <moesam138@gmail.com>
2023-02-17 19:44:57 +0200

    update function to return empty list if limit is negative

--- src/main/kotlin/interactor/GetSuitableClothesPriceCitiesNameInteractor.kt
+++ src/main/kotlin/interactor/GetSuitableClothesPriceCitiesNameInteractor.kt
@@ -10,32 +10,31 @@ class GetSuitableClothesPriceCitiesNameInteractor(
             .getAllCitiesData()
             .filter (::excludeCityWitNoBrand)
             .sortedBy(::getAveragePriceForClothes)
-            .take(limit)
-            .map { it.cityName }
+            .distinctBy { it.cityName }
+            .takeIf { limit >0 }
+            ?.take(limit)
+            ?.map { it.cityName } ?: emptyList()
     }
     private fun getAveragePriceForClothes(cityEntity: CityEntity):Float
     {
-        var totalPrice:Float =0.0F
-        if(cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM !=null) totalPrice+=cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM
-        if(cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange !=null) totalPrice+=cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange
-        if (cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes !=null) totalPrice+=cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes
-        if(cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar !=null) totalPrice+=cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar
+        var totalPrice:Float =0.0f
+        cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM?.let { totalPrice+=cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM }
+        cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange?.let { totalPrice+=cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange}
+        cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes?.let { totalPrice+=cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes }
+        cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar?.let { totalPrice+=cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar }
         return totalPrice / getNumberOfBrand(cityEntity)
     }
     private fun getNumberOfBrand(cityEntity: CityEntity):Int{
         var totalBrand =0
-        if(cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM != null) totalBrand++
-        if(cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange != null) totalBrand++
-        if(cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes != null) totalBrand++
-        if(cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar != null) totalBrand++
+        cityEntity.clothesPrices.oneSummerDressInAChainStoreZaraHAndM?.let { totalBrand++ }
+        cityEntity.clothesPrices.onePairOfNikeRunningShoesMidRange?.let { totalBrand++ }
+        cityEntity.clothesPrices.onePairOfMenLeatherBusinessShoes?.let {totalBrand++}
+        cityEntity.clothesPrices.onePairOfJeansLevis50oneOrSimilar?.let {totalBrand++ }
         return totalBrand
 
     }
     fun excludeCityWitNoBrand(city: CityEntity): Boolean {
-        return !(city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM == null &&
-                city.clothesPrices.onePairOfJeansLevis50oneOrSimilar == null &&
-                city.clothesPrices.onePairOfMenLeatherBusinessShoes == null &&
-                city.clothesPrices.onePairOfNikeRunningShoesMidRange == null)
+        return getNumberOfBrand(city) > 0
     }
 
 }
