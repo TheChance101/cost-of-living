ed57d433be7cbcb53dcca78927fe064922c08385
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2023-02-18 01:03:24 +0300

    move extension functions to util

--- src/main/kotlin/model/ClothesPrices.kt
+++ src/main/kotlin/model/ClothesPrices.kt
@@ -6,19 +6,3 @@ data class ClothesPrices(
     val onePairOfNikeRunningShoesMidRange: Float?,
     val onePairOfMenLeatherBusinessShoes: Float?,
 )
-
-fun ClothesPrices.hasNoNull(): Boolean {
-    return this::class.java.declaredFields
-        .map {
-            it.isAccessible = true
-            it.get(this)
-        }.all { it != null }
-}
-
-fun ClothesPrices.averagePrice(): Float {
-    return this::class.java.declaredFields
-        .map {
-            it.isAccessible = true
-            it.get(this) as Float
-        }.average().toFloat()
-}
