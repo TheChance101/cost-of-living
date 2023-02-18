ab7e4ffd120d3ecfd48046b0beae117be87aa150
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-02-19 00:26:51 +0200

    create empty test cases

--- src/test/kotlin/interactor/GetCitiesLeastAvgCarPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCitiesLeastAvgCarPricesInteractorTest.kt
@@ -1,7 +1,30 @@
 package interactor
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCitiesLeastAvgCarPricesInteractorTest {
+    private lateinit var interactor: GetCitiesLeastAvgCarPricesInteractor
+
+    @Test
+    fun should_ReturnTopCityNames_when_inputIsValid() {
+
+    }
+
+    @Test
+    fun should_ReturnEmptyList_when_allCarsAreNull() {
+    }
+
+    @Test
+    fun should_ReturnMaxLimitSize_when_inputIsValid() {
+    }
+
+    @Test
+    fun should_ReturnThrowException_when_salaryIsNull() {
+    }
+
+    @Test
+    fun should_ReturnLessSize_when_inputHasDuplicates() {
+    }
 }
