ca48e9fae20c3273a27fe876ffe143e6877f9247
Asia sama <sehunexo710@gmail.com>
2023-02-18 08:59:16 +0300

    rename common test file

--- src/test/kotlin/interactor/util/CommonKtTest.kt
+++ src/test/kotlin/interactor/util/CommonKtTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.function.Executable
 
-class ExtensionKtTest {
+class CommonKtTest {
     
     @Test
     fun should_ReturnFormatedSentance_when_EntringFormatedSentance() {
