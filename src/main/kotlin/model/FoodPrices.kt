da779fe893df279572fce1987d3b13c49eb78066
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-18 16:21:36 +0200

    add function getSumOfFoodPricesByEveryItemQuantity in FoodPrices model

--- src/main/kotlin/model/FoodPrices.kt
+++ src/main/kotlin/model/FoodPrices.kt
@@ -7,4 +7,23 @@ data class FoodPrices(
     val localCheese1kg: Float?,
     val chickenFillets1kg: Float?,
     val beefRound1kgOrEquivalentBackLegRedMeat: Float?,
-)
+) {
+    fun getSumOfFoodPricesByEveryItemQuantity(
+        whiteBreadQuantityForEvery500g: Float,
+        riceWhiteQuantity: Float,
+        cheeseQuantity: Float,
+        chickenFilletsQuantity: Float,
+        redMeetBeefQuantity: Float
+    ): Float? {
+        val prices = listOf(
+            loafOfFreshWhiteBread500g!! * whiteBreadQuantityForEvery500g,
+            riceWhite1kg!! * riceWhiteQuantity,
+            localCheese1kg!! * cheeseQuantity,
+            chickenFillets1kg!! * chickenFilletsQuantity,
+            beefRound1kgOrEquivalentBackLegRedMeat!! * redMeetBeefQuantity,
+        )
+        val total = prices.sum()
+        val count = prices.count { true }
+        return if (count > 0) total / count else null
+    }
+}
