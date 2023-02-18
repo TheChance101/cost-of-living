6e2898e67c7c667bfe419a3ca0b2a9f23fb79b5b
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2023-02-18 22:36:08 +0300

    remove unnecessary constants

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -2,5 +2,4 @@ package utils
 
 object Constants {
     const val INVALID_LIMIT_EXCEPTION_MSG = "Limit must be greater than 0"
-    const val EMPTY_DATA_SOURCE_EXCEPTION_MSG = "There is no data to process"
 }
