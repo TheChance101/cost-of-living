1bed4f1be96108e6ff2fc25d5e38a067fdabd498
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-18 15:16:41 +0200

    add fun getAverageFruitAndVegetablesPrices in FruitAndVegetablesPrices

--- src/main/kotlin/model/FruitAndVegetablesPrices.kt
+++ src/main/kotlin/model/FruitAndVegetablesPrices.kt
@@ -8,4 +8,19 @@ data class FruitAndVegetablesPrices(
     val potato1kg: Float?,
     val onion1kg: Float?,
     val lettuceOneHead: Float?,
-)
+) {
+    fun getAverageFruitAndVegetablesPrices(): Float? {
+        val prices = listOf(
+            apples1kg,
+            banana1kg,
+            oranges1kg,
+            tomato1kg,
+            potato1kg,
+            onion1kg,
+            lettuceOneHead
+        )
+        val total = prices.filterNotNull().sum()
+        val count = prices.count { it != null }
+        return if (count > 0) total / count else null
+    }
+}
