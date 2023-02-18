21dce4f2a80709f80d614e96edecfdddacbf6937
Haidy <haidyabugom3a@gmail.com>
2023-02-18 17:10:04 +0200

    add scope function and exception in interactor

--- src/main/kotlin/interactor/GetSalaryAverageAndCitiesNamesInCountryInteractor.kt
+++ src/main/kotlin/interactor/GetSalaryAverageAndCitiesNamesInCountryInteractor.kt
@@ -10,13 +10,16 @@ class GetSalaryAverageAndCitiesNamesInCountryInteractor(
     fun execute(countryName:String):List<Pair<String,Float>> {
         return dataSource
             .getAllCitiesData()
+            .also{
+                    if(it.isEmpty()) throw java.lang.IllegalStateException("Something went wrong")
+                    if(countryName.lowercase() !in it.map{city -> city.country.lowercase()}) return emptyList()
+            }
             .filter{
+                    countryName.lowercase() == it.country.lowercase() &&
                     it.averageMonthlyNetSalaryAfterTax.isNotNull() &&
-                    it.dataQuality &&
-                    it.country.lowercase() == countryName.lowercase()
+                    it.dataQuality
             }
             .map{Pair(it.cityName,it.averageMonthlyNetSalaryAfterTax!!)}
-            .takeIf { it.isNotEmpty() } ?: throw InvalidCountryNameException("Invalid country name")
 
     }
 
