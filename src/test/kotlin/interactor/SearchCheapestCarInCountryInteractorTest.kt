c1dae39f1320fb698be43de7fc1db6a00d7bab20
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-18 21:57:39 +0200

    edit test cases in SearchCheapestCarInCountryInteractorTest

--- src/test/kotlin/interactor/SearchCheapestCarInCountryInteractorTest.kt
+++ src/test/kotlin/interactor/SearchCheapestCarInCountryInteractorTest.kt
@@ -4,7 +4,7 @@ import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.unmockkAll
-import mockData.MockCityEntity
+import mockdata.MockCityEntity
 import model.CarsPrices
 import org.junit.jupiter.api.Test
 
@@ -45,116 +45,115 @@ class SearchCheapestCarInCountryInteractorTest {
                 ), 1500.0f
             )
         )
-        // When averageSalary not null and high dataQuality
+        // When averageSalary and cars prices not null
         every { mockData.getAllCitiesData() } returns (mockCity)
-        // Then return a list of pair of city name and average salary
+        // Then return a list of pair of city name and car price
         val actualResult = searchInteractor.execute(mockCity[0].country, 1)
         assertEquals(listOf(Pair("You can buy the car after working 9.333333 months in Los Angeles", 14000.0f)
-            ), actualResult
+        ), actualResult
         )
     }
 
-        @Test
-        fun `should return the minimum car price when both prices are valid`() {
-            // Given correct country name
-            val mockCity = listOf(
-                MockCityEntity.createMockCity(
-                    "Egypt",
-                    "Cairo",
-                    CarsPrices(
-                        10000.0f,
-                        11000.0f
-                    ), 500.0f
-                )
+    @Test
+    fun `should return the minimum car price when both prices are valid`() {
+        // Given correct country name
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                "Egypt",
+                "Cairo",
+                CarsPrices(
+                    10000.0f,
+                    11000.0f
+                ), 500.0f
             )
-            // When averageSalary not null and high dataQuality
-            every { mockData.getAllCitiesData() } returns (mockCity)
-            // Then return a list of pair of city name and average salary
-            val actualResult = searchInteractor.searchCheapestCar(mockCity[0])
-            assertEquals(10000.0f, actualResult)
-        }
+        )
+        // When averageSalary and cars prices not null
+        every { mockData.getAllCitiesData() } returns (mockCity)
+        // Then return a list of pair of city name and car price
+        val actualResult = searchInteractor.searchCheapestCar(mockCity[0])
+        assertEquals(10000.0f, actualResult)
+    }
 
-        @Test
-        fun `should return true when both car prices are not null`() {
-            // Given correct country name
-            val mockCity = listOf(
-                MockCityEntity.createMockCity(
-                    "Saudi Arabia",
-                    "Riyadh",
-                    CarsPrices(
-                        12000.0f,
-                        13000.0f
-                    ), 2000.0f
-                )
+    @Test
+    fun `should return true when both car prices are not null`() {
+        // Given correct country name
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                "Saudi Arabia",
+                "Riyadh",
+                CarsPrices(
+                    12000.0f,
+                    13000.0f
+                ), 2000.0f
             )
-            // When averageSalary not null and high dataQuality
-            every { mockData.getAllCitiesData() } returns (mockCity)
-            // Then return a list of pair of city name and average salary
-            val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
-            assertTrue(actualResult)
-        }
+        )
+        // When both car prices are not null
+        every { mockData.getAllCitiesData() } returns (mockCity)
+        // Then return true
+        val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
+        assertTrue(actualResult)
+    }
 
-        @Test
-        fun `should return false when the price of a Volkswagen Golf is null`() {
-            // Given correct country name
-            val mockCity = listOf(
-                MockCityEntity.createMockCity(
-                    "Egypt",
-                    "Damanhour",
-                    CarsPrices(
-                        null,
-                        13000.0f
-                    ),400.0f
-                )
+    @Test
+    fun `should return false when the price of a Volkswagen Golf is null`() {
+        // Given correct country name
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                "Egypt",
+                "Damanhour",
+                CarsPrices(
+                    null,
+                    13000.0f
+                ),400.0f
             )
-            // When averageSalary not null and high dataQuality
-            every { mockData.getAllCitiesData() } returns (mockCity)
-            // Then return a list of pair of city name and average salary
-            val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
-            assertFalse(actualResult)
-        }
+        )
+        // When the price of a Volkswagen Golf is null
+        every { mockData.getAllCitiesData() } returns (mockCity)
+        // Then return false
+        val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
+        assertFalse(actualResult)
+    }
 
-        @Test
-        fun `should return false when the price of a Toyota Corolla is null`() {
-            // Given correct country name
-            val mockCity = listOf(
-                MockCityEntity.createMockCity(
-                    "Egypt",
-                    "Fayoum",
-                    CarsPrices(
-                        15000.0f,
-                        null
-                    ),350.0f
-                )
+    @Test
+    fun `should return false when the price of a Toyota Corolla is null`() {
+        // Given correct country name
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                "Egypt",
+                "Fayoum",
+                CarsPrices(
+                    15000.0f,
+                    null
+                ),350.0f
             )
-            // When averageSalary not null and high dataQuality
-            every { mockData.getAllCitiesData() } returns (mockCity)
-            // Then return a list of pair of city name and average salary
-            val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
-            assertFalse(actualResult)
-        }
+        )
+        // When the price of a Toyota Corolla is null
+        every { mockData.getAllCitiesData() } returns (mockCity)
+        // Then return false
+        val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
+        assertFalse(actualResult)
+    }
 
-        @Test
-        fun `should return false when both car prices are null`() {
-            // Given country name Uppercase
-            val mockCity = listOf(
-                MockCityEntity.createMockCity(
-                    "Netherlands",
-                    //This city does not have any cars
-                    "Giethoorn",
-                    CarsPrices(
-                        null,
-                        null
-                    ),1100.0f
-                )
+    @Test
+    fun `should return false when both car prices are null`() {
+        // Given correct country name
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                "Netherlands",
+                //This city does not have any cars
+                "Giethoorn",
+                CarsPrices(
+                    null,
+                    null
+                ),1100.0f
             )
-            // When averageSalary not null and high dataQuality
-            every { mockData.getAllCitiesData() } returns (mockCity)
-            // Then return a list of pair of city name and average salary
-            val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
-            assertFalse(actualResult)
-        }
-
+        )
+        // When both car prices are null
+        every { mockData.getAllCitiesData() } returns (mockCity)
+        // Then return false
+        val actualResult = searchInteractor.excludeNullCarsPricesAndSalaries(mockCity[0])
+        assertFalse(actualResult)
+    }
 }
 
 
