9fe55b6f9b7377f289574dbacd0406631153b18e
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-18 20:48:12 +0200

    update test cases in GetCityHasCheapestInternetConnectionInteractorTest using mock library

--- src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -1,62 +1,131 @@
 package interactor
 
-import fakeData.FakeData.Companion.createCity
+import mockdata.MockCityEntity.createMockCity
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
-import model.*
+import io.mockk.unmockkAll
+import model.CityEntity
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasCheapestInternetConnectionInteractorTest {
 
-    private val dataSource = mockk<CostOfLivingDataSource>()
-    private val interactor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+    private lateinit var interactor: GetCityHasCheapestInternetConnectionInteractor
+    private val mockData = mockk<CostOfLivingDataSource>()
 
-    @Test
-    fun `should return city with cheapest internet connection`() {
-        // given
-        val citiesData = listOf(
-            createCity("City 1", 50f, 10f),
-            createCity("City 2", 2000f, 40f),
-            createCity("City 3", 5000f, 200f)
-        )
-        //when(dataSource.getAllCitiesData()).thenRetu(citiesData)
-        every { dataSource.getAllCitiesData() } returns (citiesData)
-        // when
-        val result = interactor.execute()
-
-        // then
-        assertEquals("City 2", result?.cityName)
+    @BeforeAll
+    fun setup() {
+        unmockkAll()
+        clearAllMocks()
+        interactor = GetCityHasCheapestInternetConnectionInteractor(mockData)
     }
 
     @Test
-    fun `should return null if no cities have internet price`() {
+    fun `should return city with cheapest internet connection when given a limit with three correct cites`() {
         // given
-        val citiesData = listOf(
-            createCity("City 1", 1000f, null),
-            createCity("City 2", 2000f, null),
-            createCity("City 3", 5000f, null)
+        val limit = 3
+        val mockCities = listOf(
+            createMockCity("City 1", 50f, 10f),
+            createMockCity("City 2", 2000f, 40f),
+            createMockCity("City 3", 5000f, 200f)
         )
-        every { dataSource.getAllCitiesData() } returns (citiesData)
-
+        every { mockData.getAllCitiesData() } returns (mockCities)
         // when
-        val result = interactor.execute()
+        val result = interactor.execute(limit)
+        // then
+        assertEquals(mockCities[1], result)
+    }
 
+    @Test
+    fun `should return null when given a list with all has null internet price`() {
+        // given
+        val limit = 3
+        val mockCities = listOf(
+            createMockCity("City 1", 50f, null),
+            createMockCity("City 2", 2000f, null),
+            createMockCity("City 3", 5000f, null)
+        )
+        every { mockData.getAllCitiesData() } returns (mockCities)
+        // when
+        val result = interactor.execute(limit)
         // then
         assertNull(result)
     }
 
     @Test
-    fun `should throw exception when data source throws exception`() {
+    fun `should return null when given a list with all has null salary`() {
         // given
-        val exception = IllegalStateException("Error getting city data")
-        every { dataSource.getAllCitiesData() } throws (exception)
-
+        val limit = 3
+        val mockCities = listOf(
+            createMockCity("City 1", null, 10f),
+            createMockCity("City 2", null, 40f),
+            createMockCity("City 3", null, 200f)
+        )
+        every { mockData.getAllCitiesData() } returns (mockCities)
+        // when
+        val result = interactor.execute(limit)
         // then
-        assertThrows<IllegalStateException> {
-            interactor.execute()
-        }
+        assertNull(result)
     }
+
+    @Test
+    fun `should return correct city entity when given a list has some incorrect cities`() {
+        // given
+        val limit = 4
+        val mockCities = listOf(
+            createMockCity("City 1", 50f, 10f),
+            createMockCity("City 2", null, 40f),
+            createMockCity("City 3", 5000f, 200f),
+            createMockCity("City 4", 5000f, null)
+        )
+        every { mockData.getAllCitiesData() } returns (mockCities)
+        // when
+        val result = interactor.execute(limit)
+        // then
+        assertEquals(mockCities[2], result)
+    }
+    @Test
+    fun `should return correct city entity when given a list has a city repeated one more time`() {
+        // given
+        val limit = 3
+        val mockCities = listOf(
+            createMockCity("City 1", 50f, 10f),
+            createMockCity("City 1", 50f, 10f),
+            createMockCity("City 1", 50f, 10f),
+        )
+        every { mockData.getAllCitiesData() } returns (mockCities)
+        // when
+        val result = interactor.execute(limit)
+        // then
+        assertEquals(mockCities[0], result)
+    }
+    @Test
+    fun `should return null when given a limit by zero`() {
+        // given
+        val limit = 0
+        val mockCities = emptyList<CityEntity>()
+        every { mockData.getAllCitiesData() } returns (mockCities)
+        // when
+        val result = interactor.execute(limit)
+        // then
+        assertNull(result)
+    }
+
+    @Test
+    fun `should throws exception when limit is negative`() {
+        // given a limit that is less than zero
+        val limit = -1
+        // when getting result
+        val result = Executable { interactor.execute(limit) }
+        // then check if it throws exception or not
+        Assertions.assertThrows(Exception::class.java, result)
+    }
+
 }
