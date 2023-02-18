0811e4cb35f7d974b46da44dc2f572d9150f14ef
jalal cheff <jlalalmyahy571@gmail.com>
2023-02-18 22:33:13 +0300

    eliminate test cases as a result of delete limit variable

--- src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
+++ src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.function.Executable
 import java.lang.IllegalStateException
 import java.util.NoSuchElementException
 import kotlin.test.assertEquals
-
 class GetCityHasTheHighestDifferentInApartmentRentTest {
     private lateinit var cityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
     private lateinit var emptyListOfCityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
@@ -29,65 +28,27 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
     }
 
     @Test
-    fun `should return city has highest different in apartment rent when given list of cities and limit equal to one`(){
-        // given limit
-        val limit=1
+    fun `should return city has highest different in apartment rent when given list of cities`(){
         // when the city has highest different in apartment rent
-        val actual = cityHasTheHighestDifferentInApartmentRent.execute(limit)
+        val actual = cityHasTheHighestDifferentInApartmentRent.execute()
         val expected=fakeDataSource.getAllCitiesData()[16]
         // then
         assertEquals(expected,actual)
     }
 
-    @Test
-    fun `should return empty list when limit equal to zero`(){
-        // given limit
-        val limit=0
-        // when limit equal to zero
-        val actual =Executable {cityHasTheHighestDifferentInApartmentRent.execute(limit)}
-        val expected=InvalidLimitException::class.java
-        // then
-        assertThrows(expected,actual)
-    }
-
-    @Test
-    fun `should throw exception when limit is negative`(){
-        // given limit
-        val limit=-1
-        // when the limit is negative
-        val actual =Executable {cityHasTheHighestDifferentInApartmentRent.execute(limit)}
-        val expected=InvalidLimitException::class.java
-        // then
-        assertThrows(expected,actual)
-    }
-
-    @Test
-    fun `should throw exception when limit is greater than one `(){
-        // given limit
-        val limit=2
-        // when the limit is greater than one
-        val actual =Executable {cityHasTheHighestDifferentInApartmentRent.execute(limit)}
-        val expected=InvalidLimitException::class.java
-        // then
-        assertThrows(expected, actual)
-    }
 
     @Test
     fun `should return empty city when list after filtering is empty`(){
-        // given limit
-        val limit=1
         // when the limit is greater than one
-        val actual =Executable {listOfNullCity.execute(limit)}
+        val actual =Executable {listOfNullCity.execute()}
         val expected= NoSuchElementException::class.java
         // then
         assertThrows(expected,actual)
     }
     @Test
     fun `should return empty city when given empty list`(){
-        // given limit
-        val limit=1
         // when the city has highest different in apartment rent
-        val actual =Executable{emptyListOfCityHasTheHighestDifferentInApartmentRent.execute(limit)}
+        val actual =Executable{emptyListOfCityHasTheHighestDifferentInApartmentRent.execute()}
         val expected=IllegalStateException::class.java
         // then
         assertThrows(expected,actual)
