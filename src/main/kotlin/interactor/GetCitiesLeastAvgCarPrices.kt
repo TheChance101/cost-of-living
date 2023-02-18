fb3bede6a8b403408ff7ad7f188d99dba577029b
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-02-19 00:08:20 +0200

    create interactor class

---
+++ src/main/kotlin/interactor/GetCitiesLeastAvgCarPrices.kt
@@ -0,0 +1,9 @@
+package interactor
+
+import model.CityEntity
+
+class GetCitiesLeastAvgCarPrices : CostOfLivingDataSource {
+    override fun getAllCitiesData(): List<CityEntity> {
+        TODO("Implement")
+    }
+}
