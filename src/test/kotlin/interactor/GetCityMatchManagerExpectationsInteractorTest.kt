38b9cc3496a97e2ff70a1297b6199ce5664acb54
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-02-18 13:31:59 +0300

    add test case for throwing exception

--- src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityMatchManagerExpectationsInteractorTest.kt
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.function.Executable
+import utils.Constants
 
 class GetCityMatchManagerExpectationsInteractorTest {
 
@@ -20,55 +23,13 @@ class GetCityMatchManagerExpectationsInteractorTest {
 
 
     @Test
-    fun `should return false when country is in one of north america countries`() {
-        // given a list of required countries
-        val givenList = GetCityMatchManagerExpectationsInteractor.onlyRequiredCountries
+    fun `should throw exception when country is not in one of north america countries`() {
         // when we check if it is one of the required countries
-        val result = interactor.execute()
-        // then we should get false
-        assertFalse(givenList.contains(result.country))
-    }
+        val actual = Executable { interactor.execute() }
+        val expected = Exception::class.java
 
-    @Test
-    fun `should return true when country is not in one of required countries`() {
-        // given a list of required countries
-        val givenList = GetCityMatchManagerExpectationsInteractor.onlyRequiredCountries
-        // when we check if it is not one of the required countries
-        val result = interactor.execute()
-        // then we should get true
-        assertTrue(!givenList.contains(result.country))
-    }
-
-
-    @Test
-    fun `should return meal price when mealFor2PeopleMidRangeRestaurant is not null`() {
-        // given a null value
-        val givenValue = null
-        // when we check if mealFor2PeopleMidRangeRestaurant is not null
-        val result = interactor.execute()
-        // then we should get true
-        assertNotEquals(givenValue, result.mealsPrices.mealFor2PeopleMidRangeRestaurant)
-    }
-
-
-    @Test
-    fun `should return meal price when mealInexpensiveRestaurant is not null`() {
-        // given a null value
-        val givenValue = null
-        // when we check if mealInexpensiveRestaurant is not null
-        val result = interactor.execute()
-        // then we should get true
-        assertNotEquals(givenValue, result.mealsPrices.mealInexpensiveRestaurant)
-    }
-
-    @Test
-    fun `should return meal price when mealAtMcDonaldSOrEquivalent is not null`() {
-        // given a null value
-        val givenValue = null
-        // when we check if mealAtMcDonaldSOrEquivalent is not null
-        val result = interactor.execute()
-        // then we should get true
-        assertNotEquals(givenValue, result.mealsPrices.mealAtMcDonaldSOrEquivalent)
+        // then we should assert exception
+        assertThrows(expected, actual)
     }
 
 
