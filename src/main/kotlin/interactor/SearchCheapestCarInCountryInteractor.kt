22f89afa749d95616181101b5fe7fc4d9c182af1
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-02-18 14:03:41 +0200

    write all functions in SearchCheapestCarInCountryInteractor

--- src/main/kotlin/interactor/SearchCheapestCarInCountryInteractor.kt
+++ src/main/kotlin/interactor/SearchCheapestCarInCountryInteractor.kt
@@ -1,7 +1,5 @@
 package interactor
-
 import model.CityEntity
-
 /**
  * Created by Mohamed Elgohary on 2/17/2023.
  */
@@ -9,10 +7,18 @@ class SearchCheapestCarInCountryInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun execute(countryName: String, limit: Int): List<Pair<String, Float>> {
-        return listOf(Pair("New York", 15000.0f))
+        return (dataSource.getAllCitiesData()
+            .filter { excludeNullCarsPrices(it) && it.country.equals(countryName, true) }
+            .takeIf { it.isNotEmpty() } ?: throw Exception("No cities found in country $countryName"))
+            .take(limit)
+            .map { city -> Pair(city.cityName, searchCheapestCar(city)) }
     }
-
-    fun searchCheapestCar(city: CityEntity) = 15000.0f
-
-    fun excludeNullCarsPrices(city: CityEntity) = true
+    fun searchCheapestCar(city: CityEntity) = minOf(
+        city.carsPrices.volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar!!,
+        city.carsPrices.toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar!!
+    )
+    fun excludeNullCarsPrices(city: CityEntity) = city.carsPrices.run {
+        volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar != null &&
+                toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar !=null
+    }
 }
