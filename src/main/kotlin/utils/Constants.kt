ace7ef9450e3b76aaa3ba49f89dbb53b855c1cab
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2023-02-18 19:23:54 +0300

    create empty data exception message

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -2,4 +2,5 @@ package utils
 
 object Constants {
     const val INVALID_LIMIT_EXCEPTION_MSG = "Limit must be greater than 0"
+    const val EMPTY_DATA_SOURCE_EXCEPTION_MSG = "There is no data to process"
 }
