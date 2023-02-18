5023009c624f95505724cf280702f02d281248e1
youssefahmed55 <joussefahmed2012@gmail.com>
2023-02-18 21:20:46 +0200

    Update GetCitiesAverageFAVLowesCost class

--- src/main/kotlin/interactor/GetCitiesAverageFAVLowesCost.kt
+++ src/main/kotlin/interactor/GetCitiesAverageFAVLowesCost.kt
@@ -1,41 +1,37 @@
 package interactor
 
 import model.CityEntity
-import model.FruitAndVegetablesPrices
 
-class GetCitiesAverageFAVLowesCost (
+class GetCitiesAverageFAVLowesCost(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
     fun execute(limit: Int): List<String> {
         return dataSource
-            .getAllCitiesData()
-            .filter(::isNotNullSalaryAndAnyOfFruitsAndVegetablesPricesAreNotNull)
-            .sortedByDescending {getAverageFAVPricesCompareToSalaries(it.fruitAndVegetablesPrices,it.averageMonthlyNetSalaryAfterTax!!)}
+            .getAllCitiesData().let { if (limit < 1) return emptyList() else it }
+            .filter(::isNotNullSalaryAndNotNullFruitsAndVegetablesPrices)
+            .sortedByDescending { it.getAverageFAVPricesCompareToSalaries() }
             .take(limit)
             .map { it.cityName }
     }
 
-    private fun getAverageFAVPricesCompareToSalaries(
-        fruitAndVegetablesPrices: FruitAndVegetablesPrices,
-        averageMonthlyNetSalaryAfterTax: Float
-    ) : Float {
-        var count = 0.0f
-        var total = 0.0f
-        fruitAndVegetablesPrices.apply {
-            apples1kg?.let {count++ ; total += apples1kg }
-            banana1kg?.let {count++ ; total += banana1kg}
-            oranges1kg?.let{count++ ; total += oranges1kg}
-            tomato1kg?.let{count++ ; total += tomato1kg}
-            potato1kg?.let{count++ ; total += potato1kg}
-            onion1kg?.let{count++ ; total += onion1kg}
-            lettuceOneHead?.let{count++ ; total += lettuceOneHead}
+    private fun CityEntity.getAverageFAVPricesCompareToSalaries() =
+        fruitAndVegetablesPrices.run {
+            var count = 0.0f
+            var total = 0.0f
+            apples1kg?.let { count++; total += apples1kg }
+            banana1kg?.let { count++; total += banana1kg }
+            oranges1kg?.let { count++; total += oranges1kg }
+            tomato1kg?.let { count++; total += tomato1kg }
+            potato1kg?.let { count++; total += potato1kg }
+            onion1kg?.let { count++; total += onion1kg }
+            lettuceOneHead?.let { count++; total += lettuceOneHead }
 
-            return averageMonthlyNetSalaryAfterTax - (total / count)
+            averageMonthlyNetSalaryAfterTax!! - (total / count)
         }
-    }
 
-    private fun isNotNullSalaryAndAnyOfFruitsAndVegetablesPricesAreNotNull(city: CityEntity): Boolean {
+
+    private fun isNotNullSalaryAndNotNullFruitsAndVegetablesPrices(city: CityEntity): Boolean {
         city.fruitAndVegetablesPrices.apply {
             return city.averageMonthlyNetSalaryAfterTax != null && (apples1kg != null || banana1kg != null || oranges1kg != null || tomato1kg != null || potato1kg != null || onion1kg != null || lettuceOneHead != null)
         }
