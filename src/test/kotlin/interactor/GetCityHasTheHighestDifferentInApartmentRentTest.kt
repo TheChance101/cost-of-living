96ddad1b3d8969349afab1a26d367a6c3d6796a4
jalal cheff <jlalalmyahy571@gmail.com>
2023-02-18 19:38:55 +0300

    add test cases

--- src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
+++ src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentTest.kt
@@ -1,23 +1,31 @@
 package interactor
 import dataSource.FakeDataSource
-import model.CityEntity
-import org.junit.jupiter.api.Assertions
+import dataSource.FakeEmptyDataSource
+import dataSource.FakeNullDataSource
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
+import java.lang.IllegalStateException
+import java.util.NoSuchElementException
+import kotlin.test.assertEquals
 
 class GetCityHasTheHighestDifferentInApartmentRentTest {
     private lateinit var cityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
-    private lateinit var dataSource: FakeDataSource
-    private lateinit var cities : List<CityEntity>
+    private lateinit var emptyListOfCityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
+    private lateinit var listOfNullCity: GetCityHasTheHighestDifferentInApartmentRent
+    private lateinit var fakeDataSource: CostOfLivingDataSource
+    private lateinit var fakeNullDataSource: CostOfLivingDataSource
+    private lateinit var fakeEmptyDataSource: CostOfLivingDataSource
 
     @BeforeEach
     fun setup(){
-        dataSource = FakeDataSource()
-        cityHasTheHighestDifferentInApartmentRent =  GetCityHasTheHighestDifferentInApartmentRent(dataSource)
-        cities=dataSource.getAllCitiesData()
-
+        fakeDataSource = FakeDataSource()
+        fakeEmptyDataSource=FakeEmptyDataSource()
+        fakeNullDataSource=FakeNullDataSource()
+        cityHasTheHighestDifferentInApartmentRent =  GetCityHasTheHighestDifferentInApartmentRent(fakeDataSource)
+        emptyListOfCityHasTheHighestDifferentInApartmentRent=GetCityHasTheHighestDifferentInApartmentRent(fakeEmptyDataSource)
+        listOfNullCity= GetCityHasTheHighestDifferentInApartmentRent(fakeNullDataSource)
     }
 
     @Test
@@ -26,9 +34,9 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
         val limit=1
         // when the city has highest different in apartment rent
         val actual = cityHasTheHighestDifferentInApartmentRent.execute(limit)
-        val expected=cities[16]
+        val expected=fakeDataSource.getAllCitiesData()[16]
         // then
-        Assertions.assertEquals(expected,actual)
+        assertEquals(expected,actual)
     }
 
     @Test
@@ -63,4 +71,26 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
         // then
         assertThrows(expected, actual)
     }
+
+    @Test
+    fun `should return empty city when list after filtering is empty`(){
+        // given limit
+        val limit=1
+        // when the limit is greater than one
+        val actual =Executable {listOfNullCity.execute(limit)}
+        val expected= NoSuchElementException::class.java
+        // then
+        assertThrows(expected,actual)
+    }
+    @Test
+    fun `should return empty city when given empty list`(){
+        // given limit
+        val limit=1
+        // when the city has highest different in apartment rent
+        val actual =Executable{emptyListOfCityHasTheHighestDifferentInApartmentRent.execute(limit)}
+        val expected=IllegalStateException::class.java
+        // then
+        assertThrows(expected,actual)
+    }
+
 }
