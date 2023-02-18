158140d0a6a3f941d93e5e9e25a3181d1f829af8
Nour Elden Alaa <nourelden515@gmail.com>
2023-02-18 13:48:05 +0200

    refactor GetCheapestApartmentsCitiesNamesInteractor.kt refactoring the implementation and test classes

--- src/main/kotlin/interactor/GetCheapestApartmentsCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestApartmentsCitiesNamesInteractor.kt
@@ -6,54 +6,36 @@ class GetCheapestApartmentsCitiesNamesInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
 
-    fun execute(limit: Int): List<Pair<String, Float>> {
+    fun execute(limit: Int, meters: Int): List<Pair<String, Float>> {
 
-        val data = dataSource.getAllCitiesData()
-            .filter(::excludeNullSalariesAndNullPricesAndLowQualityData)
-            .sortingWithBestPrice()
+        return dataSource.getAllCitiesData()
+            .excludeNullSalariesNullPricesAndLowQualityData()
+            .sortedByDescending {
+                it.averageMonthlyNetSalaryAfterTax!! /
+                        it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
+            }
             .take(limit)
-            .getNumberOfYears()
+            .map { Pair(it.cityName, it.calculateNumberOfYears(meters)) }
+            .takeIf { it.isNotEmpty() && it.size == limit }
+            ?: listOf(
+                Pair("Couldn't find Cities that meet your requirements :(", 0.0f)
+            )
+    }
 
-        return if (data.isEmpty()) {
-            listOf(Pair("Couldn't find Cities that meet your requirements :(", 0.0f))
-        } else {
-            data
+    private fun List<CityEntity>.excludeNullSalariesNullPricesAndLowQualityData(): List<CityEntity> {
+        return this.filter {
+            it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null &&
+                    it.averageMonthlyNetSalaryAfterTax != null &&
+                    it.dataQuality
         }
     }
 
-    private fun excludeNullSalariesAndNullPricesAndLowQualityData(city: CityEntity): Boolean {
-        return city.averageMonthlyNetSalaryAfterTax != null &&
-                city.dataQuality &&
-                city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null
-    }
-
-    /**
-     * Sort the list with the ratio between monthly salary and squareMeter's price.
-     * @return sorted List<CityEntity>
-     */
-    private fun List<CityEntity>.sortingWithBestPrice(): List<CityEntity> {
-        return this.sortedByDescending {
-            (it.averageMonthlyNetSalaryAfterTax?.div(
-                it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!
-            ))
-        }
-    }
-
-    /**
-     * Calculate how many years needed to buy 100meter apartment outSide center of the city
-     * depending on monthly salary and squareMeter's price.
-     * @return new list of <Pair<String, Float>>
-     */
-    private fun List<CityEntity>.getNumberOfYears(): List<Pair<String, Float>> {
-        val newList = mutableListOf<Pair<String, Float>>()
-        var numberOfYears: Float
-        this.forEach {
-            numberOfYears =
-                (it.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre?.times(100))?.div(
-                    (it.averageMonthlyNetSalaryAfterTax!! * 12)
-                )!!
-            newList.add(Pair(it.cityName, numberOfYears))
-        }
-        return newList
+    /*
+    Calculate how many years needed to buy 100meter apartment outSide center of the city
+    depending on monthly salary and squareMeter's price.
+    */
+    private fun CityEntity.calculateNumberOfYears(meters: Int): Float {
+        return this.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!! * meters /
+                (this.averageMonthlyNetSalaryAfterTax!! * 12)
     }
 }
