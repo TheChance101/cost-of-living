d164217b28950c746d83b0690d8ed6a63efc023a
Aziza Helmy <aziza.helmy35@gmail.com>
2023-02-18 23:58:12 +0200

    clean up the code

--- src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractor.kt
@@ -6,25 +6,24 @@ class GetCityHasLowestYearsToBuyApartmentInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
     fun execute(limit: Int): List<Pair<String, Float>> {
-
         return (dataSource
             .getAllCitiesData()
             .filter(::excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData)
-            .sortedBy { getNumberOfYearsToBuyApartment( it) }
-            .takeIf {  (limit>0)  }?: throw Exception("Not valid limit or full time salary "))
+            .sortedBy { getNumberOfYearsToBuyApartment(it) }
+            .takeIf { (limit > 0) } ?: throw Exception("Not valid limit or full time salary "))
             .take(limit)
             .map { Pair(it.cityName, getNumberOfYearsToBuyApartment(it)) }
     }
 
-    fun excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData
-                (city: CityEntity): Boolean {
-        return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null&& city.dataQuality
-                &&city.averageMonthlyNetSalaryAfterTax!=null
+    fun excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(city: CityEntity): Boolean {
+        return city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre != null
+                && city.dataQuality
+                && city.averageMonthlyNetSalaryAfterTax != null
     }
 
-    fun getNumberOfYearsToBuyApartment( city: CityEntity): Float {
-        return 100*(city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!)/
-                (city.averageMonthlyNetSalaryAfterTax!!*12)
+    fun getNumberOfYearsToBuyApartment(city: CityEntity): Float {
+        return 100 * (city.realEstatesPrices.pricePerSquareMeterToBuyApartmentOutsideOfCentre!!) /
+                (city.averageMonthlyNetSalaryAfterTax!! * 12)
 
-}
+    }
 }
--- src/test/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasLowestYearsToBuyApartmentInteractorTest.kt
@@ -1,107 +1,118 @@
 package interactor
+
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.unmockkAll
 import mockdata.MockCityEntity
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.function.Executable
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasLowestYearsToBuyApartmentInteractorTest {
-    private val dataSource = mockk<CostOfLivingDataSource>()
+
     private lateinit var interactor: GetCityHasLowestYearsToBuyApartmentInteractor
+    private val mockData = mockk<CostOfLivingDataSource>()
+
     @BeforeAll
-    fun setUp (){
+    fun setUp() {
         unmockkAll()
         clearAllMocks()
-        interactor = GetCityHasLowestYearsToBuyApartmentInteractor(dataSource)
+        interactor = GetCityHasLowestYearsToBuyApartmentInteractor(mockData)
     }
+
     @Test
-    fun `should return true when  high data qulity and not null price of apartment and  net salary`(){
+    fun `should return true when high data quality and not null price of apartment and net salary`() {
         //given
-        val  mockCity = listOf(
+        val mockCity = listOf(
             MockCityEntity.createMockCity(
                 cityName = "Santiago de Cuba",
-                pricePerSquareMeterToBuyApartmentOutsideOfCentre=  6000f,
-            dataQuality =true ,
-            averageMonthlyNetSalaryAfterTax = 1580f
-            ) )
-        every { dataSource.getAllCitiesData() } returns mockCity
+                pricePerSquareMeterToBuyApartmentOutsideOfCentre = 6000f,
+                dataQuality = true,
+                averageMonthlyNetSalaryAfterTax = 1580f
+            )
+        )
+        every { mockData.getAllCitiesData() } returns mockCity
         //when
-       val result =interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
+        val result = interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
         //then
         assertTrue(result)
-   }
+    }
+
     @Test
-    fun `should return false when  low data qulity `(){
+    fun `should return false when low data quality`() {
         //given
-        val  mockCity = listOf(
+        val mockCity = listOf(
             MockCityEntity.createMockCity(
                 cityName = "Santiago de Cuba",
-                pricePerSquareMeterToBuyApartmentOutsideOfCentre=  230f,
-                dataQuality = false ,
+                pricePerSquareMeterToBuyApartmentOutsideOfCentre = 230f,
+                dataQuality = false,
                 averageMonthlyNetSalaryAfterTax = 1580f
-            ) )
+            )
+        )
         //when
-        val result =interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
+        val result = interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
         //then
         assertFalse(result)
     }
+
     @Test
-    fun `should return false when null price of apartment or null net salary`(){
+    fun `should return false when null price of apartment or null net salary`() {
         //given
-        val  mockCity = listOf(
+        val mockCity = listOf(
             MockCityEntity.createMockCity(
                 cityName = "Santiago de Cuba",
-                pricePerSquareMeterToBuyApartmentOutsideOfCentre=  null,
-                dataQuality = true ,
+                pricePerSquareMeterToBuyApartmentOutsideOfCentre = null,
+                dataQuality = true,
                 averageMonthlyNetSalaryAfterTax = 200f
-            ) )
+            )
+        )
         //when
-        val result =interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
+        val result = interactor.excludeNullSalariesAndNullPricePerSquareApartmentAndLowQualityData(mockCity[0])
         //then
         assertFalse(result)
     }
+
     @Test
-    fun  `should return Correct List when Enter Postive limit`() {
+    fun `should return Correct List when Enter Postive limit`() {
         //given
-        val  mockCity = listOf(
+        val mockCity = listOf(
             MockCityEntity.createMockCity(
                 cityName = "Santiago de Cuba",
-                pricePerSquareMeterToBuyApartmentOutsideOfCentre=  100.0f,
-                dataQuality = true ,
+                pricePerSquareMeterToBuyApartmentOutsideOfCentre = 100.0f,
+                dataQuality = true,
                 averageMonthlyNetSalaryAfterTax = 1580f
-            ) )
-        every { dataSource.getAllCitiesData() } returns mockCity
-        val limit =1
+            )
+        )
+        every { mockData.getAllCitiesData() } returns mockCity
+        val limit = 1
         //when
         val resultList = interactor.execute(limit)
         //then
-        assertEquals(listOf(Pair("Santiago de Cuba",.5274262f)),resultList)
+        assertEquals(listOf(Pair("Santiago de Cuba", .5274262f)), resultList)
     }
+
     @Test
-    fun `should throws exception when  enter negative limit `(){
+    fun `should throws exception when  enter negative limit `() {
         //given
-        val limit =-1
+        val limit = -1
         val resultList: Executable = Executable { interactor.execute(limit) }
         // then eception should be thrown
-        assertThrows(Exception::class.java , resultList)
+        assertThrows(Exception::class.java, resultList)
     }
+
     @Test
-    fun `should throws exception when limit zero limit `(){
+    fun `should throws exception when limit zero limit `() {
         //given
-        val limit =0
+        val limit = 0
         //when
         val resultList: Executable = Executable { interactor.execute(limit) }
         // then eception should be thrown
-        assertThrows(Exception::class.java , resultList)
+        assertThrows(Exception::class.java, resultList)
     }
-
 
 
 }
