be8ecb32b629da5fac7a682379a9d263b867bfe4
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-02-19 22:28:57 +0200

    Delete GetCitiesLeastAvgCarPrices.kt

--- src/main/kotlin/interactor/GetCitiesLeastAvgCarPrices.kt
+++
@@ -1,9 +0,0 @@
-package interactor
-
-import model.CityEntity
-
-class GetCitiesLeastAvgCarPricesInteractor : CostOfLivingDataSource {
-    override fun getAllCitiesData(): List<CityEntity> {
-        TODO("Implement")
-    }
-}
