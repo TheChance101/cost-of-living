d8bcf84fe53254777bccb7af6d362a2ae45c1c7e
Amr Nasser <amr931111@gmail.com>
2023-02-19 22:20:33 +0200

    this commit check the function is work or not

--- src/test/kotlin/interactor/GetTheMostExpensiveCitiesNamesIntTermOfCinemaTicketsTest.kt
+++ src/test/kotlin/interactor/GetTheMostExpensiveCitiesNamesIntTermOfCinemaTicketsTest.kt
@@ -1,14 +1,19 @@
 package interactor
 import dataSource.FakeCsvDataSourceForCinemaTicketsPrices
+import dataSource.utils.CsvParser
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
-
+import org.junit.jupiter.api.function.Executable
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GetTheMostExpensiveCitiesNamesIntTermOfCinemaTicketsTest {
     lateinit var dataSource: CostOfLivingDataSource
-   lateinit var getTheMostExpensiveCitiesNamesIntTermOfCinemaTickets : GetTheMostExpensiveCitiesNamesIntTermOfCinemaTickets
+    lateinit var getTheMostExpensiveCitiesNamesIntTermOfCinemaTickets : GetTheMostExpensiveCitiesNamesIntTermOfCinemaTickets
 
 
     @BeforeAll
@@ -18,6 +23,15 @@ internal class GetTheMostExpensiveCitiesNamesIntTermOfCinemaTicketsTest {
         getTheMostExpensiveCitiesNamesIntTermOfCinemaTickets=GetTheMostExpensiveCitiesNamesIntTermOfCinemaTickets(dataSource)
     }
 
+    @Test
+    fun should_ReturnValueNotNull_when_Execute() {
+        // given a postive number to this function to implement
+        val limit = 5
+        // when execute this function
+        val result = getTheMostExpensiveCitiesNamesIntTermOfCinemaTickets.execute(limit)
+        // then the result should return not null
+        assertNotNull(result)
+    }
 
 
 }
