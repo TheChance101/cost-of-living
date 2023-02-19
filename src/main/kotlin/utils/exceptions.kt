8a3cc745a300aafaed17f16aa6e6c2c63ef00d94
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2023-02-19 23:44:23 +0200

    delete unused custom-exception

--- src/main/kotlin/utils/exceptions.kt
+++ src/main/kotlin/utils/exceptions.kt
@@ -3,4 +3,3 @@ package utils
 class InvalidMetersException(message: String) : Exception(message)
 class InvalidLimitException(message: String) : Exception(message)
 class NoReturnedDataException(message: String) : Exception(message)
-class InvalidCountryNameException(message: String) : Exception(message)
