d2065087d51d6c07a5512ec222d23ff5532c9e8b
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-02-19 17:02:47 +0200

    add initial implementation for interactor and test

---
+++ src/test/kotlin/interactor/GetCityHasLowestPricePerMeterSquareInCityCenterInteractorTest.kt
@@ -0,0 +1,15 @@
+package interactor
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class GetCityHasLowestPricePerMeterSquareInCityCenterInteractorTest {
+
+    @Test
+    fun `should return null when given null data`() {
+        //given
+        //when
+        //then
+    }
+}
