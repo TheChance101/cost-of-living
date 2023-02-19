ba95ac5f1233506fd51ad8d5311bfa9ff7f7bc42
TarekIdrees <tarekdrias321@gmail.com>
2023-02-19 13:03:04 +0200

    refactor testcase

--- src/test/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityThatMatchTheManagerExpectationsInteractorTest.kt
@@ -7,35 +7,41 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
+import java.util.NoSuchElementException
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityThatMatchTheManagerExpectationsInteractorTest {
-
-    private lateinit var fakeData: ManagerExpectationsFakeData
+    private lateinit var getCityThatMatchTheManagerExpectationsInteractor: GetCityThatMatchTheManagerExpectationsInteractor
 
     @BeforeAll
     fun setUp() {
-        fakeData = ManagerExpectationsFakeData
+        getCityThatMatchTheManagerExpectationsInteractor = GetCityThatMatchTheManagerExpectationsInteractor(ManagerExpectationsFakeData.emptyData)
     }
+
 
     @Test
     fun `should return city when countries and prices of meals are valid`() {
-        // Given that countries' names and prices of meals are valid
-        val validData = GetCityThatMatchTheManagerExpectationsInteractor(fakeData.getValidCountriesAndPricesOfMeals)
+        // change source of fake data to countries' names and prices of meals which are valid
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.validCountriesAndPricesOfMeals
+            )
         // When return city that match manager expectations
-        val actualData = validData.execute().cityName
+        val actualData = interactor.execute(countries).cityName
         // Then check the result
-        assertEquals("Chetumal", actualData)
+        assertEquals("Great Falls", actualData)
     }
 
     @Test
     fun `should return city when countries and some prices of meals are valid`() {
         // Given that countries' names and some prices of meals are valid
-        val mixedData =
-            GetCityThatMatchTheManagerExpectationsInteractor(fakeData.validCountriesAndSomeInvalidPricesOfMeals)
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.validCountriesAndSomeInvalidPricesOfMeals
+            )
         // When return the city that match manager expectations
-        val actualData = mixedData.execute().cityName
+        val actualData = interactor.execute(countries).cityName
         // Then check the final result
         assertEquals("Chetumal", actualData)
     }
@@ -43,48 +49,76 @@ class GetCityThatMatchTheManagerExpectationsInteractorTest {
     @Test
     fun `should return city when some countries and prices of meals are valid`() {
         // Given that some countries' names and prices of meals are valid
-        val mixedData =
-            GetCityThatMatchTheManagerExpectationsInteractor(fakeData.someInvalidCountriesAndValidPricesOfMeals)
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.someInvalidCountriesAndValidPricesOfMeals
+            )
         // When return the city that match manager expectations
-        val actualData = mixedData.execute().cityName
+        val actualData = interactor.execute(countries).cityName
         // Then check the final result
-        assertEquals("Chetumal", actualData)
+        assertEquals("Moose Jaw", actualData)
     }
 
     @Test
     fun `should return city when some countries and some prices of meals are valid`() {
         // Given that some countries' names and some prices of meals are valid
-        val validData =
-            GetCityThatMatchTheManagerExpectationsInteractor(fakeData.invalidSomeCountriesAndSomePricesOfMeals)
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.invalidSomeCountriesAndSomePricesOfMeals
+            )
         // When return the city that match manager expectations
-        val actualData = validData.execute().cityName
+        val actualData = interactor.execute(countries).cityName
         // Then check the final result
-        assertEquals("Great Falls", actualData)
+        assertEquals("Chetumal", actualData)
     }
 
     @Test
     fun `should return exception when countries are valid and prices of meals are invalid`() {
         // Given that countries' names are valid and prices of meals are invalid
-        val invalidData =
-            GetCityThatMatchTheManagerExpectationsInteractor(fakeData.validCountriesAndInvalidPricesOfMeals)
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.validCountriesAndInvalidPricesOfMeals
+            )
         // When return the city that match manager expectations
         val actualData = Executable {
-            invalidData.execute().cityName
+            interactor.execute(countries).cityName
         }
         // Then check the final result
-        assertThrowsExactly(Exception::class.java, actualData)
+        assertThrows(NoSuchElementException::class.java, actualData)
     }
 
     @Test
     fun `should return exception when countries and prices of meals are invalid`() {
         // Given that countries' names  and prices of meals are invalid
-        val invalidData =
-            GetCityThatMatchTheManagerExpectationsInteractor(fakeData.invalidData)
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.invalidData
+            )
         // When return the city that match manager expectations
         val actualData = Executable {
-            invalidData.execute().cityName
+            interactor.execute(countries).cityName
         }
         // Then check the final result
-        assertThrowsExactly(Exception::class.java, actualData)
+        assertThrows(IllegalStateException::class.java, actualData)
+    }
+
+    @Test
+    fun `should return exception when empty countries`() {
+        // Given that countries' names  and prices of meals are invalid
+        val interactor =
+            GetCityThatMatchTheManagerExpectationsInteractor(
+                ManagerExpectationsFakeData.emptyData
+            )
+        // When return the city that match manager expectations
+        val actualData = Executable {
+            interactor.execute(countries).cityName
+        }
+        // Then check the final result
+        assertThrows(IllegalStateException::class.java, actualData)
+    }
+
+    companion object{
+        // Given the countries which will search on it
+        val countries = listOf("united states", "canada", "mexico")
     }
 }
