b2219a3754e61407439c239112da965572618c1c
youssef <yr22990@gmail.com>
2023-02-19 19:21:51 +0200

    change GetMostSuitableSavingCityInteractor file to class

--- src/main/kotlin/interactor/GetMostSuitableSavingCityInteractor.kt
+++ src/main/kotlin/interactor/GetMostSuitableSavingCityInteractor.kt
@@ -3,13 +3,7 @@ package interactor
 import model.CityEntity
 import model.FoodPrices
 
-object Const{
-    const val LOAF_OF_FRESH_WHITE_AMOUNT=  2 * 15f // 2 to convert it to kilos because the data from csv is 500g
-    const val LOCAL_CHEESE_AMOUNT = 1f
-    const val READ_MEAT_AMOUNT = 4f
-    const val CHICKEN_FILLETS_AMOUNT = 10f
-    const val RICE_WHITE_AMOUNT = 2f
-}
+
 
 class GetMostSuitableSavingCityInteractor(
     private val dataSource: CostOfLivingDataSource
@@ -48,4 +42,13 @@ class GetMostSuitableSavingCityInteractor(
         val riceCost = Const.RICE_WHITE_AMOUNT * (foodPrice.riceWhite1kg!!)
         return breadCost + cheeseCost + beefCost + chickenCost + riceCost
     }
+
+    object Const{
+        const val LOAF_OF_FRESH_WHITE_AMOUNT=  2 * 15f // 2 to convert it to kilos because the data from csv is 500g
+        const val LOCAL_CHEESE_AMOUNT = 1f
+        const val READ_MEAT_AMOUNT = 4f
+        const val CHICKEN_FILLETS_AMOUNT = 10f
+        const val RICE_WHITE_AMOUNT = 2f
+    }
 }
+
