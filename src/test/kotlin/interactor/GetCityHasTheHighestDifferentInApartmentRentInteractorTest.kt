16efd3252706e248a04a791cab9ee8aaaba9904f
jalal cheff <jlalalmyahy571@gmail.com>
2023-02-19 16:57:46 +0300

    rename the class GetCityHasTheHighestDifferentInApartmentRentInteractorTest.kt by add interactor word

--- src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasTheHighestDifferentInApartmentRentInteractorTest.kt
@@ -9,10 +9,10 @@ import org.junit.jupiter.api.function.Executable
 import java.lang.IllegalStateException
 import java.util.NoSuchElementException
 import kotlin.test.assertEquals
-class GetCityHasTheHighestDifferentInApartmentRentTest {
-    private lateinit var cityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
-    private lateinit var emptyListOfCityHasTheHighestDifferentInApartmentRent: GetCityHasTheHighestDifferentInApartmentRent
-    private lateinit var listOfNullCity: GetCityHasTheHighestDifferentInApartmentRent
+class GetCityHasTheHighestDifferentInApartmentRentInteractorTest {
+    private lateinit var dataInteractor: GetCityHasTheHighestDifferentInApartmentRentInteractor
+    private lateinit var emptyDataInteractor: GetCityHasTheHighestDifferentInApartmentRentInteractor
+    private lateinit var nullDataInteractor: GetCityHasTheHighestDifferentInApartmentRentInteractor
     private lateinit var fakeDataSource: CostOfLivingDataSource
     private lateinit var fakeNullDataSource: CostOfLivingDataSource
     private lateinit var fakeEmptyDataSource: CostOfLivingDataSource
@@ -22,15 +22,15 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
         fakeDataSource = FakeDataSource()
         fakeEmptyDataSource=FakeEmptyDataSource()
         fakeNullDataSource=FakeNullDataSource()
-        cityHasTheHighestDifferentInApartmentRent =  GetCityHasTheHighestDifferentInApartmentRent(fakeDataSource)
-        emptyListOfCityHasTheHighestDifferentInApartmentRent=GetCityHasTheHighestDifferentInApartmentRent(fakeEmptyDataSource)
-        listOfNullCity= GetCityHasTheHighestDifferentInApartmentRent(fakeNullDataSource)
+        dataInteractor =  GetCityHasTheHighestDifferentInApartmentRentInteractor(fakeDataSource)
+        emptyDataInteractor=GetCityHasTheHighestDifferentInApartmentRentInteractor(fakeEmptyDataSource)
+        nullDataInteractor= GetCityHasTheHighestDifferentInApartmentRentInteractor(fakeNullDataSource)
     }
 
     @Test
     fun `should return city has highest different in apartment rent when given list of cities`(){
-        // when the city has highest different in apartment rent
-        val actual = cityHasTheHighestDifferentInApartmentRent.execute()
+        // when the city has the highest different in apartment rent
+        val actual = dataInteractor.execute()
         val expected=fakeDataSource.getAllCitiesData()[16]
         // then
         assertEquals(expected,actual)
@@ -38,17 +38,17 @@ class GetCityHasTheHighestDifferentInApartmentRentTest {
 
 
     @Test
-    fun `should return empty city when list after filtering is empty`(){
-        // when the limit is greater than one
-        val actual =Executable {listOfNullCity.execute()}
+    fun `should throw exception when filtered list is empty`(){
+        // when filtered list is empty
+        val actual =Executable {nullDataInteractor.execute()}
         val expected= NoSuchElementException::class.java
         // then
         assertThrows(expected,actual)
     }
     @Test
-    fun `should return empty city when given empty list`(){
-        // when the city has highest different in apartment rent
-        val actual =Executable{emptyListOfCityHasTheHighestDifferentInApartmentRent.execute()}
+    fun `should throw exception when retrieved data is empty`(){
+        // when the list is empty
+        val actual =Executable{emptyDataInteractor.execute()}
         val expected=IllegalStateException::class.java
         // then
         assertThrows(expected,actual)
