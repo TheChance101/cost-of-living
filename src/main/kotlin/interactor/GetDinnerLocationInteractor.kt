10eec0b716257b91d62042f72d6a9be7adf35ad3
Abdurrahman-Salah-ad-Din <abdosalahas.01128@gmail.com>
2023-02-19 02:46:20 +0200

    enhance the execute method enhance the getAverage method enhance the getClosestCity method

--- src/main/kotlin/interactor/GetDinnerLocationInteractor.kt
+++ src/main/kotlin/interactor/GetDinnerLocationInteractor.kt
@@ -5,36 +5,29 @@ import kotlin.math.abs
 
 class GetDinnerLocationInteractor(private val dataSource: CostOfLivingDataSource) {
     fun execute(): CityEntity? {
-        val citiesSortedByMealPrice = dataSource.getAllCitiesData()
-            .filter { it.country in listOf("USA", "Canada", "Mexico") }
-            .filter { it.mealsPrices?.mealInexpensiveRestaurant != null }
-            .sortedBy { it.mealsPrices?.mealInexpensiveRestaurant!! }
-        if (citiesSortedByMealPrice.isEmpty()) return null
-        val cheapestCity = citiesSortedByMealPrice.first()
-        val mostExpensiveCity = citiesSortedByMealPrice.last()
-        val averageMealPrice =
-            getAverage(cheapestCity.mealsPrices!!, mostExpensiveCity.mealsPrices!!)
-        val closestCity: CityEntity? = getClosestCity(citiesSortedByMealPrice, averageMealPrice)
-        return closestCity
+        return dataSource.getAllCitiesData()
+            .filter {
+                it.country in listOf("USA", "Canada", "Mexico") &&
+                        it.mealsPrices.mealInexpensiveRestaurant != null
+            }
+            .sortedBy { it.mealsPrices.mealInexpensiveRestaurant }
+            .also { if (it.size < 2) return null }
+            .run {
+                val averageMealPrice = getAverage(this.first().mealsPrices, this.last().mealsPrices)
+                getClosestCity(this, averageMealPrice)
+            }
     }
 }
-fun getAverage(cheapestCity: MealsPrices, mostExpensiveCity: MealsPrices): Float {
-    val averageMealPrice =
+
+fun getAverage(cheapestCity: MealsPrices, mostExpensiveCity: MealsPrices) =
         (cheapestCity.mealInexpensiveRestaurant!! + mostExpensiveCity.mealInexpensiveRestaurant!!) / 2
-    return averageMealPrice
-}
+
 fun getClosestCity(
     citiesSortedByMealPrice: List<CityEntity>,
     averageMealPrice: Float,
-): CityEntity? {
-    var closestCity1: CityEntity? = null
-    var closestDifference = Double.MAX_VALUE
-    for (city in citiesSortedByMealPrice) {
-        val difference = abs(city.mealsPrices?.mealInexpensiveRestaurant!! - averageMealPrice)
-        if (difference < closestDifference) {
-            closestDifference = difference.toDouble()
-            closestCity1 = city
-        }
-    }
-    return closestCity1
+): CityEntity {
+    return citiesSortedByMealPrice.map {
+        it to abs(it.mealsPrices.mealInexpensiveRestaurant!! - averageMealPrice)
+    }.sortedBy { it.second }[0].first
 }
+
