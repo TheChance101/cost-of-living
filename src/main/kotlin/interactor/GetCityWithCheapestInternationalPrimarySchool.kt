9ed64802d38c1a1cf34f3e84e2f4e7bf8f76f5af
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-02-19 02:48:17 +0200

    creat class GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractor

---
+++ src/main/kotlin/interactor/GetCityWithCheapestInternationalPrimarySchool.kt
@@ -0,0 +1,16 @@
+package interactor
+import model.CityEntity
+
+class GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractor(
+    private val dataSource: CostOfLivingDataSource
+) {
+    fun execute(limit:Int): List<Pair<String,Float?>> {
+        TODO("Not Implemented Yet")
+    }
+
+    fun excludeNullInterNationalPrimarySchoolYearlyForOneChildAndLowQualityData(city: CityEntity): Boolean {
+        TODO("Not Implemented Yet")
+    }
+
+    
+}
