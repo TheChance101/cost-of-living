1e4ff1179f8f164d23793f7fd9400d8f1cfd575f
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-02-19 08:48:19 +0200

    test: add test cases for cheapest tennis price

---
+++ src/test/kotlin/interactor/plus/GetCitiesNameWithCheapestTennisPriceOnWeekendInteractorTest.kt
@@ -0,0 +1,77 @@
+package interactor.plus
+
+import data.BananaCheapestFakeData
+import data.EmptyFakeData
+import data.InvalidFakeData
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class GetCitiesNameWithCheapestTennisPriceOnWeekendInteractorTest {
+    private lateinit var getCitiesNameWithCheapestTennisPrice: GetCitiesNameWithCheapestTennisPriceOnWeekendInteractor
+
+    @BeforeEach
+    fun setup() {
+        getCitiesNameWithCheapestTennisPrice =
+            GetCitiesNameWithCheapestTennisPriceOnWeekendInteractor(BananaCheapestFakeData)
+    }
+
+    @Test
+    fun `should return correct 2 cities when the data is valid`() {
+        // given limit the return by 2
+        val limit = 2
+        // when the actual result is 2 cities names
+        val result = getCitiesNameWithCheapestTennisPrice(limit)
+        // then check of the result
+        assertEquals(
+            listOf(
+                "Santiago de Cuba",
+                "Sancti Spiritus"
+            ),
+            result
+        )
+    }
+
+    @Test
+    fun `should return correct list of 1 city name when the data is valid`() {
+        // given limit the return by 1
+        val limit = 1
+        // when the actual result is list of 1 city name
+        val result = getCitiesNameWithCheapestTennisPrice(limit)
+        // then check of the result
+        assertEquals(
+            listOf(
+                "Santiago de Cuba",
+            ),
+            result
+        )
+    }
+
+    @Test
+    fun `should throw exception when the data is invalid`() {
+        // change data to be invalid
+        getCitiesNameWithCheapestTennisPrice =
+            GetCitiesNameWithCheapestTennisPriceOnWeekendInteractor(InvalidFakeData)
+        // given limit the return by 1
+        val limit = 1
+        // when the actual result is exception
+        val result = Executable { getCitiesNameWithCheapestTennisPrice(limit) }
+        // then check of the result
+        assertThrows(Exception::class.java, result)
+    }
+    @Test
+    fun `should throw exception when the data is empty`() {
+        // change data to be empty
+        getCitiesNameWithCheapestTennisPrice =
+            GetCitiesNameWithCheapestTennisPriceOnWeekendInteractor(EmptyFakeData)
+        // given limit the return by 1
+        val limit = 1
+        // when the actual result is exception
+        val result = Executable { getCitiesNameWithCheapestTennisPrice(limit) }
+        // then check of the result
+        assertThrows(Exception::class.java, result)
+    }
+}
