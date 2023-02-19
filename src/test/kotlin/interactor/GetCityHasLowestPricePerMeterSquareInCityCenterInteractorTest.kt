439074a2ca8d558412956633af1c5684d739640d
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-02-19 17:22:51 +0200

    add test cases for wrong data and normal data

--- src/test/kotlin/interactor/GetCityHasLowestPricePerMeterSquareInCityCenterInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasLowestPricePerMeterSquareInCityCenterInteractorTest.kt
@@ -1,15 +1,76 @@
 package interactor
 
+import fakedata.LowestPricePerMeterSquareInCityCenterFakeData
 import org.junit.jupiter.api.Test
-
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityHasLowestPricePerMeterSquareInCityCenterInteractorTest {
+
+    lateinit var fakeData: LowestPricePerMeterSquareInCityCenterFakeData
+
+    @BeforeAll
+    fun setup() {
+        fakeData = LowestPricePerMeterSquareInCityCenterFakeData
+    }
 
     @Test
     fun `should return null when given null data`() {
-        //given
-        //when
-        //then
+        //given data of null cases
+        val getCityHasLowestPricePerMeterSquareInCityCenterInteractor =
+            GetCityHasLowestPricePerMeterSquareInCityCenterInteractor(fakeData.fakeNullData)
+        //when find city with the lowest price per meter square in city center
+        val lowestPricePerMeterSquareCity = getCityHasLowestPricePerMeterSquareInCityCenterInteractor.execute()
+        //then check null
+        assertNull(lowestPricePerMeterSquareCity)
     }
+
+    @Test
+    fun `should return null when given empty data source`() {
+        //given empty data source
+        val getCityHasLowestPricePerMeterSquareInCityCenterInteractor =
+            GetCityHasLowestPricePerMeterSquareInCityCenterInteractor(fakeData.emptyDataSource)
+        //when find city with the lowest price per meter square in city center
+        val lowestPricePerMeterSquareCity = getCityHasLowestPricePerMeterSquareInCityCenterInteractor.execute()
+        //then check null
+        assertNull(lowestPricePerMeterSquareCity)
+    }
+
+    @Test
+    fun `should return null when given data with zero price`() {
+        //given data with zero price only
+        val getCityHasLowestPricePerMeterSquareInCityCenterInteractor =
+            GetCityHasLowestPricePerMeterSquareInCityCenterInteractor(fakeData.fakeDataWithZeroPrice)
+        //when find city with the lowest price per meter square in city center
+        val lowestPricePerMeterSquareCity = getCityHasLowestPricePerMeterSquareInCityCenterInteractor.execute()
+        //then check null
+        assertNull(lowestPricePerMeterSquareCity)
+    }
+
+    @Test
+    fun `should return null when given data with negative price`() {
+        //given data with negative price only
+        val getCityHasLowestPricePerMeterSquareInCityCenterInteractor =
+            GetCityHasLowestPricePerMeterSquareInCityCenterInteractor(fakeData.fakeDataWithNegativePrice)
+        //when find city with the lowest price per meter square in city center
+        val lowestPricePerMeterSquareCity = getCityHasLowestPricePerMeterSquareInCityCenterInteractor.execute()
+        //then check null
+        assertNull(lowestPricePerMeterSquareCity)
+    }
+
+    @Test
+    fun `should return correct city when given normal data`() {
+        //given data of high quality, low quality, null , zero and negative cases
+        val getCityHasLowestPricePerMeterSquareInCityCenterInteractor =
+            GetCityHasLowestPricePerMeterSquareInCityCenterInteractor(fakeData.fakeNormalData)
+        //when find city with the lowest price per meter square in city center
+        val lowestPricePerMeterSquareCity = getCityHasLowestPricePerMeterSquareInCityCenterInteractor.execute()
+        //then check the city
+        assertEquals(fakeData.highQualityCase1, lowestPricePerMeterSquareCity)
+    }
+
+
+
 }
