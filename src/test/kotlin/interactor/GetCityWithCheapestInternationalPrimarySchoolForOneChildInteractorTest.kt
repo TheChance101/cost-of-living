fdd4b2eaa62a14a300b150ddef42aa4265e10dab
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-02-19 03:26:49 +0200

    add first test case

--- src/test/kotlin/interactor/GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractorTest.kt
@@ -1,10 +1,13 @@
 package interactor
 
 import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.unmockkAll
+import mockData.MockCityEntity
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -17,4 +20,21 @@ class GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractorTest{
         unmockkAll()
         clearAllMocks()
         interactor = GetCityWithCheapestInternationalPrimarySchoolForOneChildInteractor(dataSource)
-    }}
+    }
+    @Test
+    fun `should return true when high data quality and cost of international primary school is not null` () {
+        //given
+        val mockCity = listOf(
+            MockCityEntity.createMockCity(
+                internationalPrimarySchoolYearlyForOneChild =2676.54f,
+                cityName = "Damascus",
+                dataQuality = true
+            )
+        )
+        every { dataSource.getAllCitiesData() } returns mockCity
+        //when
+        val result = interactor.excludeNullInterNationalPrimarySchoolYearlyForOneChildAndLowQualityData(mockCity[0])
+        //then
+        assertTrue(result)
+    }
+}
