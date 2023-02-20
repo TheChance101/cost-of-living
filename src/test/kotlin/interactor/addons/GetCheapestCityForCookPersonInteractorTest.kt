9aae44ceda82b99291ce8edfe366383e0e88e4d8
hassanwasfy <wasfyelgamed@gmail.com>
2023-02-20 18:52:10 +0200

    implement one more test case to GetCheapestCityForCookPersonInteractor method

--- src/test/kotlin/interactor/addons/GetCheapestCityForCookPersonInteractorTest.kt
+++ src/test/kotlin/interactor/addons/GetCheapestCityForCookPersonInteractorTest.kt
@@ -4,6 +4,7 @@ import fakeDataSource.addons.FakeDataSourceForCrookPerson
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.function.Executable
 
 class GetCheapestCityForCookPersonInteractorTest {
 
@@ -28,7 +29,19 @@ class GetCheapestCityForCookPersonInteractorTest {
         assertEquals(expected,data)
     }
 
+    @Test
+    fun should_Throw_Exception_When_List_Is_Empty(){
+        interactor = GetCheapestCityForCookPersonInteractor(
+            fakeDataSource.getListWithNullSalaryAndNullPrice())
 
+        val data = Executable {interactor.execute()}
+
+        val expected = NullPointerException()::class.java
+
+
+        assertThrows(expected,data)
+
+    }
 
     @Test
     fun should_Return_True_When_Data_Quality_High() {
