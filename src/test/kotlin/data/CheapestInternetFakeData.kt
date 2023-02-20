db9acbdcb76cccd960b456dcc4a525537c92c98e
David samuel <davidsamuelramsis@gmail.com>
2023-02-20 13:15:20 +0200

    test(internet): refactor fake data

--- src/test/kotlin/data/CheapestInternetFakeData.kt
+++ src/test/kotlin/data/CheapestInternetFakeData.kt
@@ -4,9 +4,9 @@ import interactor.CostOfLivingDataSource
 import model.*
 
 
-class CheapestInternetFakeData : CostOfLivingDataSource {
-    override fun getAllCitiesData(): List<CityEntity> {
-        return listOf(
+object CheapestInternetFakeData {
+     private val validData by lazy{
+         listOf(
             CityEntity(
                 cityName = "Santiago de Cuba",
                 country = "Cuba",
@@ -718,12 +718,99 @@ class CheapestInternetFakeData : CostOfLivingDataSource {
         )
 
     }
+    private val _invalidData by lazy {
+        listOf(
+            CityEntity(
+                cityName = " ",
+                country = " ",
+                mealsPrices = MealsPrices(
+                    mealInexpensiveRestaurant = null,
+                    mealFor2PeopleMidRangeRestaurant = null,
+                    mealAtMcDonaldSOrEquivalent = null
+                ),
+                drinksPrices = DrinksPrices(
+                    cappuccinoRegularInRestaurants = null,
+                    cokePepsiAThirdOfLiterBottleInRestaurants = null,
+                    waterAThirdOfLiterBottleInRestaurants = null,
+                    milkRegularOneLiter = null,
+                    waterOneAndHalfLiterBottleAtTheMarket = null
+                ),
+                fruitAndVegetablesPrices = FruitAndVegetablesPrices(
+                    apples1kg = null,
+                    banana1kg = null,
+                    oranges1kg = null,
+                    tomato1kg = null,
+                    potato1kg = null,
+                    onion1kg = null,
+                    lettuceOneHead = null
+                ),
+                foodPrices = FoodPrices(
+                    loafOfFreshWhiteBread500g = null,
+                    riceWhite1kg = null,
+                    eggsRegular12 = null,
+                    localCheese1kg = null,
+                    chickenFillets1kg = null,
+                    beefRound1kgOrEquivalentBackLegRedMeat = null
+                ),
+                servicesPrices = ServicesPrices(
+                    basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment = null,
+                    oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans = null,
+                    internet60MbpsOrMoreUnlimitedDataCableAdsl = null,
+                    fitnessClubMonthlyFeeForOneAdult = null,
+                    tennisCourtRentOneHourOnWeekend = null,
+                    cinemaInternationalReleaseOneSeat = null,
+                    preschoolOrKindergartenFullDayPrivateMonthlyForOneChild = null,
+                    internationalPrimarySchoolYearlyForOneChild = null
+                ),
+                clothesPrices = ClothesPrices(
+                    onePairOfJeansLevis50oneOrSimilar = null,
+                    oneSummerDressInAChainStoreZaraHAndM = null,
+                    onePairOfNikeRunningShoesMidRange = null,
+                    onePairOfMenLeatherBusinessShoes = null
+                ),
+                transportationsPrices = TransportationsPrices(
+                    oneWayTicketLocalTransport = null,
+                    monthlyPassRegularPrice = null,
+                    taxiStartNormalTariff = null,
+                    taxi1kmNormalTariff = null,
+                    taxi1hourWaitingNormalTariff = null,
+                    gasolineOneLiter = null
+                ),
+                carsPrices = CarsPrices(
+                    volkswagenGolf_1_4_90kwTrendLineOrEquivalentNewCar = null,
+                    toyotaCorollaSedan_1_6l_97kwComfortOrEquivalentNewCar = 21105.0f
+                ),
+                realEstatesPrices = RealEstatesPrices(
+                    apartmentOneBedroomInCityCentre = null,
+                    apartmentOneBedroomOutsideOfCentre = null,
+                    apartment3BedroomsInCityCentre = null,
+                    apartment3BedroomsOutsideOfCentre = null,
+                    pricePerSquareMeterToBuyApartmentInCityCentre = null,
+                    pricePerSquareMeterToBuyApartmentOutsideOfCentre = null
+                ),
+                averageMonthlyNetSalaryAfterTax = null,
+                dataQuality = false
+            )
+        )
+    }
+
+    val validSalaryAndInternetPrices by lazy {
+        object : CostOfLivingDataSource {
+            override fun getAllCitiesData() = validData
+        }
+    }
+
+    val invalidData by lazy {
+        object : CostOfLivingDataSource {
+            override fun getAllCitiesData() = _invalidData
+        }
+    }
+
+     val emptyData by lazy {
+        object : CostOfLivingDataSource {
+            override fun getAllCitiesData() = emptyList<CityEntity>()
+
+        }
+    }
+
 }
-
-
-
-
-
-
-
-
