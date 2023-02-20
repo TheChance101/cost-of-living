8537015f6a8bd9233496aaeed7a56cf528cfb01e
TarekIdrees <tarekdrias321@gmail.com>
2023-02-20 14:29:40 +0200

    add a function for meals prices data class

--- src/main/kotlin/model/MealsPrices.kt
+++ src/main/kotlin/model/MealsPrices.kt
@@ -4,4 +4,10 @@ data class MealsPrices(
     val mealInexpensiveRestaurant: Float?,
     val mealFor2PeopleMidRangeRestaurant: Float?,
     val mealAtMcDonaldSOrEquivalent: Float?,
-)
+) {
+    fun getSummationPricesOfMeals(): Float {
+        return mealFor2PeopleMidRangeRestaurant!!.div(2) +
+                mealInexpensiveRestaurant!! +
+                mealAtMcDonaldSOrEquivalent!!
+    }
+}
